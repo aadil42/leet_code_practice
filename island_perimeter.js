30302a4e31fd176407c3862cf983701e8a441ad1
Aadil Kelawala <adilkelawala@gmail.com>
2022-01-10 02:54:52 +0530

    solved island perimeter problem.

---
+++ island_perimeter.js
@@ -0,0 +1,30 @@
+var islandPerimeter = function(grid) {
+
+    const row = grid.length;
+    const column = grid[0].length;
+    let perimeter = 0;
+    for(let i = 0; i < row; i++) {
+        for(let j = 0; j< column; j++) {
+            if(grid[i][j] == 1) {
+                perimeter += 4;
+
+                if(i > 0 && grid[i - 1][j] == 1) {
+                    perimeter -= 2;
+                }
+                if(j > 0 && grid[i][j - 1] == 1) {
+                    perimeter -= 2;
+                }
+            }
+        }
+    }
+
+    return perimeter
+};
+
+const map = [
+    [0,1,0,0],
+    [1,1,1,0],
+    [0,1,0,0],
+    [1,1,0,0]
+];
+console.log(islandPerimeter(map));
