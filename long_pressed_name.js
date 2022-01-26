d0698c78b439b63b2112b1c6612a97d454c57f63
aadil42 <adilkelawala@gmail.com>
2022-01-26 17:38:40 +0530

    solved long_pressed_name.

---
+++ long_pressed_name.js
@@ -0,0 +1,22 @@
+var isLongPressedName = function(name, typed) {
+    
+    let i = 0;
+    let j = 0;
+    while ( i < name.length || j < typed.length) {
+        if(name[i] === typed[j]) {
+            i++;
+            j++;
+        } else if(typed[j] === typed[j - 1]) {
+            j++;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+
+const myName = 'aadil';
+const typed = 'aaaaadddillllk';
+console.log(isLongPressedName(myName, typed));
