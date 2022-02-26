50a4717f442c698842e7ed91ebd56d8a786799a6
aadil42 <adilkelawala@gmail.com>
2022-02-27 00:02:50 +0530

    solved detect-cycle

---
+++ detect-cycle.js
@@ -0,0 +1,20 @@
+// REMEMBER 
+// you need legit linked list for input to test this solution, which you don't have but you have checked it on leet code
+// it's workin fine. just push it on github as it is. it's legit solution and it's working.
+
+var detectCycle = function(head) {
+    
+    const myMap = new Map();
+    
+    let count = 0;
+    
+    while(head) {
+        if(myMap.has(head)) {
+            return head;
+        }
+        myMap.set(head, count)
+        count++;
+        head = head.next;
+    }
+    return head;
+};
