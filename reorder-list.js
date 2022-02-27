24f4f3f25f09abd7bb94999106a719704b7465a4
aadil42 <adilkelawala@gmail.com>
2022-02-27 14:19:11 +0530

    solved reorder list

---
+++ reorder-list.js
@@ -0,0 +1,43 @@
+// REMEMBER 
+// you need legit linked list for input to test this solution, which you don't have but you have checked it on leet code
+// it's workin fine. just push it on github as it is. it's legit solution and it's working.
+
+var reorderList = function(head) {
+  
+    if(!head || !head.next) return head;
+    let slowPointer = head;
+    let fastPointer = head;
+    
+    while(fastPointer && fastPointer.next) {
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next.next;
+    }
+    reverseHead  = reverseLinkList(slowPointer);
+    ref = head;
+    console.log(head);
+    while(reverseHead && reverseHead.next) {
+        const reverseNext  = reverseHead.next;
+        const headNext  = head.next;
+        
+        head.next = reverseHead;
+        reverseHead.next = headNext;
+        
+        head = headNext;
+        reverseHead = reverseNext;
+    }
+    // head.next = null;
+    
+    return ref;
+};
+
+function reverseLinkList(head) {
+    let pre = null;
+    while(head) {
+        const temp = head.next;
+        head.next = pre;
+        pre = head;
+        head = temp;        
+    } 
+    
+    return pre;
+}
