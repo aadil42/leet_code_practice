8127a5ae131373e1b9f1017ef2bb0639d40156a8
aadil42 <adilkelawala@gmail.com>
2022-03-01 16:24:47 +0530

    solved maximum subarray

---
+++ maximum-subarray.js
@@ -0,0 +1,11 @@
+var maxSubArray = function(nums) {
+    
+    let total = Number.NEGATIVE_INFINITY;
+    let currunt = 0;
+        for(let i = 0; i < nums.length; i++) {
+            currunt = Math.max(nums[i], currunt + nums[i]);
+            total = Math.max(currunt, total);
+        }
+        
+        return total;
+    };
