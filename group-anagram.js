4d970a8c006fe770fafe586cad94cc8fd74d293b
aadil42 <adilkelawala@gmail.com>
2022-04-13 19:40:33 +0530

    solved group anagram

---
+++ group-anagram.js
@@ -0,0 +1,38 @@
+var groupAnagrams = function(strs) {
+    
+
+    const result = [];
+    const myHash = new Map();
+
+    strs.forEach((word) => {
+
+        let newHash = new Array(26).fill(0);
+        [...word].forEach((char) => {
+            // console.log(char.charCodeAt(0) - 'a'.charCodeAt(0));
+            newHash[char.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+            console.log(newHash);
+        });
+        
+        // making an array into string
+        newHash = newHash.join('-');
+        // console.log(newHash);
+        if(!myHash.has(newHash)) {
+            // console.log(newHash, 'if');
+            myHash.set(newHash,[word]);
+        } else {
+            // console.log(newHash);
+            myHash.set(newHash, [...myHash.get(newHash), word]);
+        }
+    });
+
+    console.log(myHash);
+    for(let [key, value] of myHash) {
+        result.push(myHash.get(key));
+    }
+    
+    return result;
+};
+
+const strs = ["bdddddddddd","bbbbbbbbbbc"];
+console.log(groupAnagrams(strs));
+
