f6226fb6639676ad06ef4f5a79243899ea412fb1
aadil42 <adilkelawala@gmail.com>
2022-04-29 16:52:53 +0530

    solved valid-paliandrome

---
+++ valid-paliandrome.js
@@ -0,0 +1,21 @@
+var isPalindrome = function(s) {
+    
+    s  = s.replace(/[^a-z0-9]/gi, '');
+    s = s.toLowerCase();
+
+    let front = 0;
+    let back = s.length - 1;
+    while(front < back) {
+        // console.log(s[front], s[back]);
+        if(s[front] !==  s[back]) return false;
+        front++;
+        back--;
+    }
+
+    return true;
+};
+
+
+
+const s = ' ';
+console.log(isPalindrome(s));
