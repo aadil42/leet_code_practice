5e204b02ec31b8b8f122340a5b86597098dcfb87
aadil42 <adilkelawala@gmail.com>
2022-05-10 15:40:21 +0530

    solved isSameTree

---
+++ same-tree.js
@@ -0,0 +1,19 @@
+// you need appropriate data structure to run this code. If you don't have any paste this code in this editor https://leetcode.com/problems/same-tree/submissions/
+
+var isSameTree = function(p, q) {
+    
+    
+    function goRecursive(p, q) {
+        
+        if((!p && q) || (p && !q)) return false;
+        if(!p && !q) return true;
+        
+        if(p.val !== q.val) {
+            return false;
+        }
+        
+       return  (goRecursive(p.left, q.left) &&  goRecursive(p.right, q.right));
+    }
+    
+    return goRecursive(p, q);
+};
