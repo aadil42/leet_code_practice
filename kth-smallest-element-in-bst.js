2bea13fdde7901822fd7e57a33e752d68bf2a9a6
aadil42 <adilkelawala@gmail.com>
2022-06-08 17:18:10 +0530

    solved kth-smallest-element

---
+++ kth-smallest-element-in-bst.js
@@ -0,0 +1,23 @@
+function makeInOrderArr(root) {
+    const inOrderArr = [];
+    
+    goRecursive(root);
+    function goRecursive(root) {
+        if(!root) return;
+        goRecursive(root.left);
+        inOrderArr.push(root.val);
+        goRecursive(root.right);
+    }
+    
+    return inOrderArr;
+}
+var kthSmallest = function(root, k) {
+    
+    if(!root) return;
+    const inOrderArr = makeInOrderArr(root);
+    
+    if(k <= inOrderArr.length) {
+    return inOrderArr[k-1];
+    }
+    
+};
