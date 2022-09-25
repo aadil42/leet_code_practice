70e31b94f82196394441386aeac285f53ab553b3
aadil42 <adilkelawala@gmail.com>
2022-09-25 20:17:11 +0530

    the code is exactly the same as the needCode guy's. I don't know why it's not  working for me

--- minimum-windows-substring.js
+++ minimum-windows-substring.js
@@ -1,64 +1,97 @@
 var minWindow = function(s, t) {
 
-const haveHash = new Map();
-const needHash = new Map();
-let i = 0;
-let j = 0;
-let min = Number.MAX_VALUE;
+const haveHash = {};
+const needHash = {};
+
+
+// filling the frequency of t strings chars
+for(let k = 0; k < t.length;  k++) {
+    let value = haveHash[t[k]];
+    if(value) {
+        haveHash[t[k] + 1];
+    } else {
+        haveHash[1];
+    }
+}
+
 let have = 0;
 let need = t.length;
-let left = 0;
-let right = 0;
 
-t.split('').forEach((char) => {
+let result = [-1, -1];
+let resultLen = Number.MAX_VALUE;
 
-    setHash(needHash, char);
-    
-    if(needHash.has(char)) {
-        needHash.set(char, needHash.get(char) + 1);
+left = 0;
+right = 0;
+
+while(right < s.length) {
+
+    const c = s[right];
+    if(needHash[c]) {
+        needHash[c] = needHash[c] + 1;
     } else {
-        needHash.set(char, 1);
+        needHash[c] = 1;
     }
-});
 
-while(i < s.length) {
+    if(haveHash[c] && haveHash[c] === needHash[c]) {
+        have++;
+    }
 
-
-
-    if(have == need) {
-        // slide i untill have !== need;
-        if(j - i + 1 < min) {
-            left = i;
-            right = j;
+    while(have === need) {
+        if(right - left + 1 < resultLen) {
+            result[0] = left;
+            result[1] = right;
+            resultLen = right - left + 1;
         }
-        min = Math.min(min, j - i + 1);
 
-
+        needHash[c] -= 1 
+        if(haveHash[s[left]] && needHash[s[left]] < haveHash[s[left]]) {
+            have--;
+        }
+        left++;
     }
-
-    if(have !== need) {
-        j++;
-    }
+    
+    right++;
 }
-
-return s.slice(left, right);
+    console.log(result[0], result[1]);
+    return s.substring(result[0], result[1] + 1);
 };
 
-function setHash(needHash, value) {
-    if(needHash.has(value)) {
-        if(haveHash.has(value)) {
-            haveHash.set(value, haveHash.get(value) + 1);
-        } else {
-            haveHash.set(value, 1);
-        }
 
-        // taking care of incementing the have count;
-        if(haveHash.get(value) == needHash.get(value)) {
-            have += 1;
-        }
-    }
-}
 const s = 'ADOBECODEBANC';
 const t = 'ABC';
 
 console.log(minWindow(s, t));
+
+
+// bruteforce
+var minWindowBrute = function(s, t) {
+    
+
+    const myHash = new Map();
+
+
+    for(let i = 0; i < t.length; i++) {
+        let value =  myHash.get(t[i]);
+        if(value) {
+            myHash.set(t[i], value+1);
+        } else {
+            myHash.set(t[i], 1);
+        }
+    }
+
+    console.log(myHash);
+
+    let minStr = '';
+    let i = 0;
+    let j = 0;
+    // k will determin if the  t string has ended.
+    let k = 0;
+
+    while(j < s.length) {
+        
+    }
+};
+
+const s1 = "ADOBECODEBANC";
+const t1 = "ABCCCC";
+// console.log(minWindowBrute(s1,t1));
