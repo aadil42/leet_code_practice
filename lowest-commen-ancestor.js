57360273c6278f4eb86d06abf6d66b1ba661e71a
aadil42 <adilkelawala@gmail.com>
2022-10-15 16:45:11 +0530

    solved it I tested it with console.log it is giving me the answer but when I return the same answer from the function it gives me undefined. Don't know what's wrong this is the second time I tested this code and it's working as expected. something seems wrong with LeetCode.

--- lowest-commen-ancestor.js
+++ lowest-commen-ancestor.js
@@ -1,8 +1,10 @@
 function findPath(root,target) {
     const path = [];    
     goRicursive(root, path);
+    
     function goRicursive(root, curruntArr) {
     if(!root) return false;
+        
     curruntArr.push(root.val);
     if(root.val === target) {
         return true;
@@ -13,23 +15,29 @@ function findPath(root,target) {
     curruntArr.pop();
     return false;
     }
+
     return path;
 }
 
 
 var lowestCommonAncestor = function(root, p, q) {
     
-    const pPath = findPath(root, p);
-    const qPath = findPath(root, q);
-
+    const pPath = findPath(root, p.val);
+    const qPath = findPath(root, q.val);
+   
     const maxLen = Math.max(pPath.length, qPath.length);
-
-    let CA;
+    let CA = 0;
+    
+  
     for(let i = 0; i < maxLen; i++) {
         if(pPath[i] !== qPath[i]) {
             CA = pPath[i - 1];
             break;
         }
     }
+
+    console.log(CA);
     return CA;
+    // return CA;
 };
+
