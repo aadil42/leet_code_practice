4c98f9b4f29701b1d8e9b4343b0b779020fdd4d6
aadil42 <adilkelawala@gmail.com>
2022-10-18 14:03:14 +0530

    solved contains duplicate second time.

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -12,3 +12,18 @@ var containsDuplicate = function(nums) {
     
     return false;
 };
+
+// solved the same problam second time for revision purpose. 
+
+var containsDuplicateR = function(nums) {
+    
+    const numberSet = new Set();
+    
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(numberSet.has(nums[i])) return true;
+        numberSet.add(nums[i]);
+    }
+    
+    return false;
+};
