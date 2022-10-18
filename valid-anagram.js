95a7732512d9b1a01c6af5d2525531aa4d3dd54b
aadil42 <adilkelawala@gmail.com>
2022-10-18 13:54:22 +0530

    solved valid-anagram second time.

--- valid-anagram.js
+++ valid-anagram.js
@@ -23,3 +23,35 @@ var isAnagram = function(s, t) {
     
     return true;
 };
+
+// this was solved second time for revison purpose
+var isAnagramR = function(s, t) {
+    
+    if( s.length !== t.length ) return false;
+     
+     const charMap = new Map();
+     
+     // creating word frequencies.
+     for(let i =  0; i < s.length; i++) {
+         const char = s[i];
+         if(charMap.has(char)) {
+             charMap.set(char, charMap.get(char) + 1);
+         } else {
+             charMap.set(char, 1);
+         }
+     }
+     
+     // checking if anagram.
+     for(let i = 0; i < t.length; i++) {
+         const char = t[i];
+         if(charMap.get(char)) {
+             charMap.set(char, charMap.get(char) - 1);
+         } else {
+             return false;
+         }
+     }
+     
+     
+     return true;
+     
+ };
