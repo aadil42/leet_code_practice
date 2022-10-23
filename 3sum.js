afbe95fac3bd98c7f799ed7cf4ae3bbac2fba853
aadil42 <adilkelawala@gmail.com>
2022-10-23 19:01:58 +0530

    solved 3sum again

--- 3sum.js
+++ 3sum.js
@@ -6,7 +6,7 @@ sortedNums = nums.sort((a,b) => {
 
 let outputArr = [];
 for(let i = 0; i < sortedNums.length - 2; i++) {
-    if(sortedNums[i] !== sortedNums[i-1] || i === 0) {
+    if(sortedNums[i] !== sortedNums[i-1]) {
         let low = i + 1;
         let heigh = sortedNums.length - 1;
         // let targetSum = 0 - sortedNums[i];
@@ -27,6 +27,44 @@ return outputArr;
 
 };
 
-const nums = [0, 0, 0];
-console.log(threeSum(nums));
-// 
+const nums = [-1,0,1,2,-1,-4];
+// console.log(threeSum(nums));
+
+// burte force
+var threeSumR = function(nums) {
+    nums.sort((a, b) => {
+        return a - b;
+    });
+
+    const result = [];
+    for(let i = 0; i < nums.length - 2; i++) {
+
+        // this condition is pretry important it keeps the duplicate out of our result array.
+        if(nums[i] !== nums[i - 1]) {
+
+            let low = i + 1;
+            let heigh = nums.length - 1;
+
+            while(low < heigh) {
+
+                if(nums[low] + nums[heigh] + nums[i] === 0) {
+
+                    result.push([nums[i],nums[low],nums[heigh]]);
+                    
+                    // below while loop is pretty important it keeps the duplicate out of our result array.
+                    while(nums[low] === nums[low+1]) low++;
+                    low++;
+                } else if(nums[low] + nums[heigh] + nums[i] > 0) {
+                    heigh--;
+                } else {
+                    low++;
+                }
+            }
+        }
+    }
+
+    return result;
+};
+
+console.log(threeSumR(nums));
+
