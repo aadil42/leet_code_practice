7db959426d6ab999aba947b9e9c681cc317db96c
aadil42 <adilkelawala@gmail.com>
2022-10-23 20:29:22 +0530

    solved valid-paliandrome second time

--- valid-paliandrome.js
+++ valid-paliandrome.js
@@ -19,3 +19,22 @@ var isPalindrome = function(s) {
 
 const s = ' ';
 console.log(isPalindrome(s));
+
+
+// solved it second time
+var isPalindromeR = function(s) {
+    s  = s.replace(/[^a-z0-9]/gi, '');
+    s = s.toLowerCase();
+
+    let front = 0;
+    let back = s.length - 1;
+
+    while(front < back) {
+        if(s[front] !== s[back]) return false;
+
+        front++;
+        back--;
+    }
+
+    return true;
+};
