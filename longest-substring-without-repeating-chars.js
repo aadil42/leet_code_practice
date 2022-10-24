b45f7bd8b9b635b5d5cf7cf6dd44574b1de94a6b
aadil42 <adilkelawala@gmail.com>
2022-10-24 15:38:56 +0530

    solved longest-substring-without-repeating-chars 2nd and 3rd time

--- longest-substring-without-repeating-chars.js
+++ longest-substring-without-repeating-chars.js
@@ -23,4 +23,68 @@ var lengthOfLongestSubstring = function(s) {
 };
 
 const s = "bbbbb";
-console.log(lengthOfLongestSubstring(s));
+// console.log(lengthOfLongestSubstring(s));
+
+
+
+// attempted 2nd time. with set
+var lengthOfLongestSubstringRR = function(s) {
+    const wordSet = new Set();
+ let maxLen = 0;
+ let curruntLen = 0;
+
+let left = 0;
+let right = 0;
+
+ while(right < s.length){
+     console.log(right,s.length);
+     if(wordSet.has(s[right])) {
+         
+         while(s[left] !== s[right] && left < right) {
+             wordSet.delete(s[left]);
+             left++;
+             curruntLen--;
+         }
+         curruntLen--;
+         left++;
+         right++;
+         curruntLen++;
+         maxLen = Math.max(curruntLen, maxLen);
+     } else {
+
+         wordSet.add(s[right]);
+         right++;
+         curruntLen++;
+         maxLen = Math.max(curruntLen, maxLen);
+     }
+
+     // console.log(left, right);
+ }
+
+ return maxLen;
+};
+
+// attempted 3rd time. with set
+var lengthOfLongestSubstringR = function(s) { 
+    const wordSet = new Set();
+    let maxLen = 0;
+    let curruntLen = 0;
+
+    let left = 0;
+    let right = 0;
+
+    while(right < s.length){
+     if(wordSet.has(s[right])) {
+        wordSet.delete(s[left]);
+        left++;
+     } else {
+        wordSet.add(s[right]);
+        right++;
+     }
+     maxLen = Math.max(wordSet.size, maxLen);
+    }
+
+    return maxLen;
+};
+
+console.log(lengthOfLongestSubstringR(s));
