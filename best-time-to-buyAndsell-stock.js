716e82f645cbcce728f9fb3558746f8fdc54907d
aadil42 <adilkelawala@gmail.com>
2022-10-29 14:48:01 +0530

    solved best-time-to-buyAndSell-stock second time.

--- best-time-to-buyAndsell-stock.js
+++ best-time-to-buyAndsell-stock.js
@@ -24,3 +24,42 @@ var maxProfit = function(prices) {
 
 const prices = [7,1,5,3,6,4];
 console.log(maxProfit(prices));
+
+
+// solved second time
+var maxProfitR = function(prices) {
+    
+    
+    let left = 0;
+    let right = 1;
+    let max = 0;
+    
+    
+    while(right < prices.length) {
+        
+        const profit = prices[right] - prices[left];
+        
+        max = Math.max(profit, max);
+        
+        if(prices[right] < prices[left]) {
+            left = right;
+        }
+        right++;
+    }
+    
+    return max;
+};
+
+// brute force;
+var maxProfitBrute = function(prices) {
+    
+    
+    let maxProfit = 0;
+    for(let i = 0; i < prices.length; i++) {
+        for(let j = i + 1; j < prices.length; j++) {
+            maxProfit = Math.max(prices[j] - prices[i], maxProfit);
+        }
+    }
+    
+    return maxProfit;
+};
