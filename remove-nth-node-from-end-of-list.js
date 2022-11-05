155840b089d6c27f98c8c2cf87b667cc3fd385f9
aadil42 <adilkelawala@gmail.com>
2022-11-05 17:12:15 +0530

    solved remove-nth-node-from-end-fo-list second time

--- remove-nth-node-from-end-of-list.js
+++ remove-nth-node-from-end-of-list.js
@@ -47,3 +47,37 @@ var findLen = function(node) {
     
     return count;
 }
+
+// solving it again. 
+var removeNthFromEndR = function(head, n) {
+    
+    const reverseList = reversLinkList(head);
+    let reversehead = new ListNode(null, reverseList);
+    let temp = reversehead;
+    let i = 1;
+    while(i !== n) {
+        i++;
+        temp = temp.next;
+    }
+
+    temp.next = temp.next.next;
+
+    const unreverseList = reversLinkList(reversehead.next);
+
+    return unreverseList;
+};
+
+var reversLinkList = function(head) {
+
+    let prev = null;
+
+    while(head) {
+        let temp = head;
+        head = head.next;
+        temp.next = prev;
+        prev = temp;
+        temp = head;
+    }
+
+    return prev;    
+}
