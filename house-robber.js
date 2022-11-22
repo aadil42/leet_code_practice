18a64334bc7c82d7d6bb8543595a4ed7f838f87a
aadil42 <adilkelawala@gmail.com>
2022-11-22 16:47:57 +0530

    solved  house robber second time

--- house-robber.js
+++ house-robber.js
@@ -11,3 +11,21 @@ var rob = function(nums) {
     
     return rob2;
 };
+
+
+/// second time
+var robR = function(nums) {
+    
+   
+    if(nums.length == 1) return nums[0];
+    nums[1] = Math.max(nums[0], nums[1]);
+
+    for(let i = 2; i < nums.length; i++) {
+        console.log(nums[i - 1], nums[i] + nums[i-2]);
+
+        nums[i] = Math.max(nums[i-1], nums[i] + nums[i-2]);
+    }
+
+    console.log(nums);
+    return nums[nums.length - 1];
+};
