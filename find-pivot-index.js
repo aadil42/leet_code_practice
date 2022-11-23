47197814725f572bd67017c007eba1cb93959de7
aadil42 <adilkelawala@gmail.com>
2022-11-23 15:50:55 +0530

    solved find-pivot-ined

---
+++ find-pivot-index.js
@@ -0,0 +1,18 @@
+var pivotIndex = function(nums) {
+    
+    const sum = nums.reduce((preSum, currentVal) => {
+        return preSum + currentVal;
+    }, 0);
+    
+    
+    let left = 0;
+    for(let i = 0; i < nums.length; i++) {
+        let right = sum - left - nums[i];
+        if(right === left) {
+            return i;
+        }
+        left += nums[i];
+    }
+    
+    return -1;
+};
