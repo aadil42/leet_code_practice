447ef664ef3c58e25ffe245fc9de7bb41094b572
aadil42 <adilkelawala@gmail.com>
2022-11-23 16:42:34 +0530

    solved house-robber-2

--- house-robber-2.js
+++ house-robber-2.js
@@ -27,3 +27,27 @@ var preRob = function(nums) {
 
 const nums =  [1,2,3];
 console.log(rob(nums));
+
+
+// second time.
+var robR = function(nums) {
+    
+    if(nums.length === 1) return nums[0];
+    const preSum = subSrob(nums.slice(0, nums.length - 1));
+    const postSum = subSrob(nums.slice(1));
+  
+    return Math.max(preSum, postSum);
+  };
+  
+  function subSrob(nums) {
+      console.log(nums);
+     
+      if(nums.length === 1) return nums[0];
+      if(nums.length === 2) return Math.max(nums[0], nums[1]);
+      nums[1] = Math.max(nums[0], nums[1]);
+      for(let i = 2; i < nums.length; i++) {
+       nums[i] = Math.max(nums[i - 1], nums[i] + nums[i - 2]);
+      }
+  
+      return nums[nums.length - 1];
+  }
