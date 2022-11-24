ded6501d4036a2a6cb114093cbeec652e2e428b9
aadil42 <adilkelawala@gmail.com>
2022-11-24 15:04:27 +0530

    solved find-all-numbers-disappeared-in-an-array

---
+++ find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,18 @@
+
+// O(n).
+var findDisappearedNumbers = function(nums) {
+    
+    const numberSet = new Set();
+
+    for(let i = 1; i < nums.length + 1; i++) {
+        numberSet.add(i);
+    }
+
+    nums.forEach((element) => {
+        if(numberSet.has(element)) {
+            numberSet.delete(element);
+        }
+    });
+
+    return Array.from(numberSet);
+};
