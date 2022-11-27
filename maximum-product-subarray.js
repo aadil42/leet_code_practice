ac8b153493641b4223422804d0dea3c732b7d400
aadil42 <adilkelawala@gmail.com>
2022-11-28 00:01:20 +0530

    solved maximum-product-subarra

---
+++ maximum-product-subarray.js
@@ -0,0 +1,18 @@
+var maxProduct = function(nums) {
+
+    let maxCurrent = 1;
+    let minCurrent = 1;
+    let Max = nums[0];
+    
+    for(let i = 0; i < nums.length; i++) {
+        const temp = nums[i] * maxCurrent;
+    
+        maxCurrent = Math.max(temp, nums[i] * minCurrent, nums[i]);
+        minCurrent = Math.min(temp, minCurrent * nums[i], nums[i]);
+    
+        Max = Math.max(Max, maxCurrent);
+    }
+    
+    return Max;
+};
+
