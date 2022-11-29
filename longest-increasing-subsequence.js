e531bca5fbfa50662fa55d6dc42cd4c7036c1120
aadil42 <adilkelawala@gmail.com>
2022-11-29 15:13:26 +0530

    solved longest-increasing-subsequence second time

--- longest-increasing-subsequence.js
+++ longest-increasing-subsequence.js
@@ -15,3 +15,19 @@ return Math.max(...dpArray);
 
 const nums = [10,9,2,5,3,7,101,18];
 console.log(lengthOfLIS(nums));
+
+// solved second time
+var lengthOfLISR = function(nums) {
+    
+    let DpArra = new Array(nums.length).fill(1);
+    
+    for(let i = nums.length - 1; i > -1; i--) {
+        for(let j = i + 1; j < nums.length; j++) {
+            if(nums[i] < nums[j]) {
+                DpArra[i] = Math.max(DpArra[i], 1 + DpArra[j]);
+            }
+        }
+    }
+    
+    return Math.max(...DpArra);
+    };
