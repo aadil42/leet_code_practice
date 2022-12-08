4f9f7793b4d1348c627d6dd83986726f6f7390e7
aadil42 <adilkelawala@gmail.com>
2022-12-08 16:22:39 +0530

    solved unique-length-3-palindromic-subsequences.js

---
+++ unique-length-3-palindromic-subsequences.js
@@ -0,0 +1,56 @@
+// brute force approche O(n^3);
+
+var countPalindromicSubsequenceBrute = function(s) {
+    const palindromSet = new Set();
+
+    for(let i = 0; i < s.length; i++) {
+        for(let j = i + 1; j < s.length; j++) {
+            for(let k = j  + 1;  k < s.length; k++) {
+                if(s[i] === s[k]) {
+                    palindromSet.add(s[i] + s[j] + s[k]);
+                    break;
+                }
+            }
+        }
+    }
+
+    return palindromSet.size;
+};
+
+// optimized approche O(n); 
+var countPalindromicSubsequence = function(s) {
+ 
+    const resSet = new Set();
+    const leftHash = new Set();
+    const rightHash = new Map();
+    
+    s.split('').forEach((element) => {
+        const current = rightHash.get(element); 
+        if(!current) {
+            rightHash.set(element, 1);
+        } else {
+            rightHash.set(element, current+1);
+        }
+    });
+    
+    
+    for(let i = 0; i < s.length; i++) {
+    
+        const rightCurrent = rightHash.get(s[i]);
+        rightHash.set(s[i], rightCurrent - 1);
+        if(!rightHash.get(s[i])) {
+            rightHash.delete(s[i]);
+        }
+    
+        for(let  j = 0; j < 26; j++) {
+            const char  = String.fromCharCode('a'.charCodeAt(0) + j);
+            if(leftHash.has(char) && rightHash.has(char)) {
+                resSet.add(s[i]+char);
+            }
+        }
+        leftHash.add(s[i]);
+    }
+    
+    return resSet.size;
+    
+    };
