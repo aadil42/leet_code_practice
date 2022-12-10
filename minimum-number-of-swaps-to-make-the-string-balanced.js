f3af2cc2133a132aac4f6de1f5d7dd9075db7873
aadil42 <adilkelawala@gmail.com>
2022-12-10 15:45:33 +0530

    solved minimum-number-of-swaps-to-make-the-string-balanced.js

---
+++ minimum-number-of-swaps-to-make-the-string-balanced.js
@@ -0,0 +1,11 @@
+var minSwaps = function(s) {
+
+    let extraClosing = 0;
+    let maxClosing = 0;
+    for(let i = 0; i < s.length; i++) {
+        s[i] === ']' ? extraClosing++ : extraClosing--;
+        maxClosing = Math.max(maxClosing, extraClosing);
+    }
+
+    return Math.ceil(maxClosing/2);
+};
