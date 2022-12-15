658aa261916b202ba637dba2c0bb836e165464fe
aadil42 <adilkelawala@gmail.com>
2022-12-15 21:54:59 +0530

    solved find-the-index-of-the-first-occurrence-in-a-string in O(n+m) one of the most complicated algorithm I've ever seen. Don't feel bad if it takes you several tries to get the algorithm

--- find-the-index-of-the-first-occurrence-in-a-string.js
+++ find-the-index-of-the-first-occurrence-in-a-string.js
@@ -1,6 +1,6 @@
 
 // brute force O(haystack*needle);
-var strStr = function(haystack, needle) {
+var strStrBrute = function(haystack, needle) {
     
     for(let i = 0; i < haystack.length; i++) {
         if(checkStr(i, needle, haystack)) {
@@ -19,3 +19,68 @@ var strStr = function(haystack, needle) {
   
   return true;
   }
+
+//   efficient with KMP algorithm. O(m+n);
+var strStr = function(haystack, needle) {
+    
+    const pattern = Array(needle.length).fill(0);
+
+    let i = 1;
+    let j = 0;
+
+    while(i < needle.length) {
+        if(needle[i] === needle[j]) {
+            pattern[i] = j + 1;
+            i++;
+            j++;
+        } else if(needle[i] !== needle[j]) {
+            if(j === 0) {
+                pattern[i] = 0;
+                i++;
+            } else if(j !== 0 && needle[j] === needle[i]) {
+                pattern[i] = 1 + pattern[j - 1];
+                i++;
+            } else {
+                j = pattern[j - 1];
+            }
+        }
+    }
+    
+ 
+j = 0;
+i = 0;
+
+while(i < haystack.length) {
+
+    if(haystack[i] === needle[j]) {
+        i++;
+        j++;
+    } else if(haystack[i] !== needle[j]) {
+        if(j === 0) {
+            i++;
+        } else {
+        j = pattern[j - 1];            
+        }
+    }
+
+    if(j === needle.length) return i - needle.length;
+}
+
+return -1;
+
+}
+
+// abcdabca
+
+// [0,1,2,0,1,2,3,3]
+// AAACAAAA
+//    j   i
+
+// aaaxaaax
+// aaaa
+const haystack = "abcxabcdabxabcdabcdabcy", needle = "abcdabcy";
+
+console.log(strStr(haystack, needle));
+  
+  
+
