31f1b1f5de49b9a054d70b06a8a481f6c78dace7
aadil42 <adilkelawala@gmail.com>
2022-12-29 04:22:55 +0530

    solved trapping-rain-water in O(1) space

--- trapping-rain-water.js
+++ trapping-rain-water.js
@@ -39,3 +39,38 @@ var trap = function(height) {
 
     return water;
 };
+
+/**
+ * Linear 
+ * Time O(n) | Space O(1) 
+ * @param {number[]} height
+ * @return {number}
+ */
+ var trap = function(height) {
+    
+    let left = 0;
+    let right = height.length - 1;
+    let current = left;
+    let maxLeft = 0;
+    let maxRight = 0;
+    let water = 0;
+  
+    while(left < right) {
+        const waterBlock = Math.min(maxLeft, maxRight) - height[current];
+        if(waterBlock > 0) {
+            water += waterBlock;
+        }
+        maxLeft = Math.max(maxLeft, height[left]);
+        maxRight = Math.max(maxRight, height[right]);
+  
+        if(maxLeft <= maxRight) {
+            left++;
+            current = left;
+        } else {
+            right--;
+            current = right;
+        }
+    }
+  
+    return water;
+  };
