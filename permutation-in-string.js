5d573046adeebc59013885b20e99f751e0df2075
aadil42 <adilkelawala@gmail.com>
2022-12-30 21:31:38 +0530

    solved permutation-in-string in js

--- permutation-in-string.js
+++ permutation-in-string.js
@@ -44,3 +44,77 @@ var isPermutation = function(s, t) {
     
     return true;
 };
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+ var checkInclusion = function(s1, s2) {
+    const isValid = s1.length <= s2.length; 
+ if(!isValid) return false;
+
+ const s1Hash = new Map();
+ const s2Hash = new Map();
+
+ // populating both hash with 0's
+ for(let i = 0; i < 26; i++) {
+     s1Hash.set(String.fromCharCode(97+i),0);
+     s2Hash.set(String.fromCharCode(97+i),0);
+ }
+
+ // populating s1Hash and s2Hash upto s1Hash length.
+ for(let i = 0; i < s1.length; i++) {
+     if(s1Hash.has(s1[i])) {
+         s1Hash.set(s1[i], s1Hash.get(s1[i]) + 1);
+     } else {
+         s1Hash.set(s1[i], 1);
+     }
+
+     if(s2Hash.has(s2[i])) {
+         s2Hash.set(s2[i], s2Hash.get(s2[i]) + 1);
+     } else {
+         s2Hash.set(s2[i], 1);
+     }
+ }    
+
+ let left = 0;
+ let right = s1.length;
+ let matches = 0;
+
+ for(let i = 0; i <26; i++) {
+     s2Char = s2Hash.get(String.fromCharCode(i+97));
+     s1Char = s1Hash.get(String.fromCharCode(i+97));
+
+     if(s2Char === s1Char) matches++;
+ }
+ 
+
+ while(right < s2.length) {
+
+     if(matches === 26) return true;
+
+     const leftChar = s2[left];
+     const rightChar = s2[right];
+
+
+
+     s2Hash.set(rightChar, s2Hash.get(rightChar) + 1);
+     if(s2Hash.get(rightChar) === s1Hash.get(rightChar)) {
+         matches++;
+     } else if(s2Hash.get(rightChar) === s1Hash.get(rightChar) + 1) {
+         matches--;
+     }
+
+     s2Hash.set(leftChar, s2Hash.get(leftChar) - 1);
+     if(s2Hash.get(leftChar) === s1Hash.get(leftChar)) {
+         matches++;
+     } else if(s2Hash.get(leftChar) === s1Hash.get(leftChar) - 1) {
+         matches--;
+     }
+
+     left++;
+     right++;
+ }
+ return matches === 26;
+};
