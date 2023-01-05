df8cc6892c26af45d60f364c99ace590712d9cdf
aadil42 <adilkelawala@gmail.com>
2023-01-05 18:17:24 +0530

    solved unique-binary-search-trees

---
+++ unique-binary-search-trees.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+    
+
+    function countRecursion(n,map) {
+        if(n in map) {
+            return map[n];
+        }
+        if(n <= 1) return 1;
+        let total = 0;
+        for(let i = 1; i <= n; i++) {
+            total += countRecursion(i-1,map) * countRecursion(n-i,map);
+        }
+        map[n] = total;
+        return total;
+    }
+
+    return countRecursion(n,{});
+};
