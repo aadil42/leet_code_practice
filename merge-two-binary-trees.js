11e3708858eab3c9a4ba97097f2d2bb21d60b03e
aadil42 <adilkelawala@gmail.com>
2023-01-14 15:20:35 +0530

    solved merge-two-binary-trees

---
+++ merge-two-binary-trees.js
@@ -0,0 +1,31 @@
+/**
+ * Pre order Traversal
+ * Time O(n) | Space O(h). h = height of the tree.
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+const mergeTrees = (root1, root2) => {
+
+    if(!root1 && !root2) return null;
+    if(root1 === null) {
+        return root2;
+    }
+    if(root2 === null) {
+        return root1;
+    }
+    
+  root1.val = root1.val + root2.val;
+  root1.left = mergeTrees(root1.left, root2.left);
+  root1.right =  mergeTrees(root1.right, root2.right);
+    
+    return root1;
+};
