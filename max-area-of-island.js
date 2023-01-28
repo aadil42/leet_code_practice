547747532b7c3045fb3922e04622fa89172122d0
aadil42 <adilkelawala@gmail.com>
2023-01-28 16:46:14 +0530

    solved max-area-of-island

---
+++ max-area-of-island.js
@@ -0,0 +1,37 @@
+/** DFS
+ * Time O(n*m) || Space O(1);
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    const row = grid.length;
+    const column = grid[0].length;
+    let maxArea = 0;
+    let currentArea = 0;
+    for(let i = 0; i < row;  i++) {
+        for(let j = 0; j < column; j++) {
+            dfs(i,j);
+            maxArea = Math.max(maxArea,currentArea);
+            currentArea = 0;
+        }
+    }
+
+    function dfs(r,c) {
+        
+        if(
+            r === row ||
+            r < 0 || 
+            c === column ||
+            c < 0 ||
+            grid[r][c] === 0
+        ) return 0;
+
+        currentArea++;
+        grid[r][c] = 0;
+        dfs(r+1, c);
+        dfs(r-1, c);
+        dfs(r,c+1);
+        dfs(r,c-1);
+    }
+    return maxArea;
+};
