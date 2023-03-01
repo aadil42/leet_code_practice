db7e28f93730b3a4ecfbdac4c600d0faf7f511d0
aadil42 <adilkelawala@gmail.com>
2023-03-01 15:59:50 +0530

    solved coin-change-ii

---
+++ coin-change-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+    
+    // when doing the caching with just {} it gives TLE error.
+    // but when doing with 2d array it passes.
+    const cache = new Array(coins.length).fill(-1).map(() => new Array(amount + 1).fill(-1));
+    function dfs(index, left) {
+        if(left < 0) return 0;
+        if(left === 0) return 1;
+
+        const key = index + '-' + left;
+        if(cache[index][left] !== -1) return cache[index][left];
+
+        let currentMax = 0;        
+        for(let i = index; i < coins.length; i++) {
+            currentMax += dfs(i, left - coins[i]);
+        }
+        cache[index][left] = currentMax;
+        return cache[index][left];
+    }
+
+    return dfs(0,amount);
+    
+};
