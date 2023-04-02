3dd7184508bfca0a241de5e40a958e4311e87b5f
aadil42 <adilkelawala@gmail.com>
2023-04-02 16:33:23 +0530

    sovled maximum-points-you-can-obtain-from-cards optimally

--- maximum-points-you-can-obtain-from-cards.js
+++ maximum-points-you-can-obtain-from-cards.js
@@ -16,3 +16,35 @@ var maxScore = function(cardPoints, k) {
 
     return dfs(0, cardPoints.length - 1, 0);
 };
+
+
+/**
+ * Greedy
+ * Time O(n) | Space O(1);
+ * @param {number[]} cardPoints
+ * @param {number} k
+ * @return {number}
+ */
+var maxScore = function(cardPoints, k) {
+    
+    let currentSum = 0;
+    let maxSum = 0;
+  
+    windowStart = 0;
+    windowEnd = cardPoints.length - k - 1;
+  
+    // calculating initial sum
+    for(let i = windowEnd + 1; i < cardPoints.length; i++) {
+        currentSum += cardPoints[i];
+    }
+  
+  
+    while(windowEnd < cardPoints.length) {     
+        maxSum = Math.max(maxSum, currentSum);
+        currentSum = currentSum + cardPoints[windowStart] - cardPoints[windowEnd + 1];
+        windowStart++;
+        windowEnd++;
+    }
+  
+    return maxSum;
+  };
