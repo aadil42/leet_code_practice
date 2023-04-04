ba60abfc69d0070b206c4011830c6248e3102521
aadil42 <adilkelawala@gmail.com>
2023-04-04 17:32:52 +0530

    solved two-city-scheduling in n*log(n)

--- two-city-scheduling.js
+++ two-city-scheduling.js
@@ -24,3 +24,38 @@ var twoCitySchedCost = function(costs) {
 
     return dfs(0, costs.length/2,costs.length/2);
 };
+
+/**
+ * 
+ * Greedy 
+ * Time O(n) | Space O(n)
+ * @param {number[][]} costs
+ * @return {number}
+ */
+var twoCitySchedCost = function(costs) {
+  
+    const diffs = [];
+  
+    costs.forEach((cost) => {
+        diffs.push([cost[0], cost[1], cost[1] - cost[0]]);
+    });
+  //   console.log(diffs);
+  
+    diffs.sort((diff1, diff2)  => {
+        return diff1[2] - diff2[2];
+    });
+  
+      console.log(diffs);
+      let minCost = 0;
+  
+      for(let i = 0; i < diffs.length; i++) {
+          if(i < diffs.length/2) {
+              minCost += diffs[i][1];
+          }
+          if(i >= diffs.length/2) {
+              minCost += diffs[i][0];
+          }
+      }
+  
+      return minCost;
+  };
