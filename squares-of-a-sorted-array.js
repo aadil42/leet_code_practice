6ee64075062e364d24bddf6f99f9c470e3fa7c68
aadil42 <adilkelawala@gmail.com>
2023-04-05 12:33:40 +0530

    solved squares-of-a-sorted-array in O(n) time

--- squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array.js
@@ -1,8 +1,35 @@
 /**
+ * Time O(n*log(n)) | SpaceO(n);
  * @param {number[]} nums
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
     
     return nums.map((num) => Math.abs(num)).sort((a,b) => a-b).map((num) => num*num);
+};
+
+/**
+ * Linear Time
+ * Time O(n) | Space O(n)
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    
+    let left = 0;
+    let right = nums.length - 1;
+    const squareArr = [];
+    while(left <= right) {
+        const leftS = nums[left] * nums[left];
+        const rightS = nums[right] * nums[right];
+        if(leftS > rightS) {
+            squareArr.push(leftS);
+            left++;
+        } else {
+            squareArr.push(rightS);
+            right--;
+        }
+    }
+
+    return squareArr.reverse();
 };
