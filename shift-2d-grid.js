a66c86d747a98663f8ac6c11e93195fa10d83ae7
aadil42 <adilkelawala@gmail.com>
2023-04-27 10:44:49 +0530

    solved shift-2d-grid (different solution)

--- shift-2d-grid.js
+++ shift-2d-grid.js
@@ -36,3 +36,46 @@ var shiftGrid = function(grid, k) {
 
     return result;
 };
+
+
+/**
+ * efficient solution without declaring bunch of arrays.
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
+ */
+var shiftGrid = function(grid, k) {
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+
+    k = k % (ROWS * COLS);
+    const result = [];
+    for(let i = 0; i < ROWS; i++) {
+        const subArr = [];
+        for(let j = 0; j < COLS; j++) {
+            subArr.push(0);
+        }
+        result.push(subArr);
+    };
+
+    const posToVal = (r,c) =>  {
+        return r*COLS + (c % COLS);
+    }
+
+    const valToPos = (val) => {
+        
+        val = val % (ROWS*COLS); // if val goes out of bound
+        return [Math.floor(val/COLS), val % COLS];
+    }
+
+    for(let i = 0; i < ROWS; i++) {
+        for(let j = 0; j < COLS; j++) {
+            const targetVal = posToVal(i,j) + k;
+            const targetPos = valToPos(targetVal);
+            console.log(targetPos);
+            result[targetPos[0]][targetPos[1]] = grid[i][j];
+        }
+    }
+
+    return result;
+};
