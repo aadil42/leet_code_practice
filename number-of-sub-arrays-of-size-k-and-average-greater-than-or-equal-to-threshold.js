7b3e5db6a2a1cc73203aa679a2134ca4d70858f5
aadil42 <adilkelawala@gmail.com>
2023-05-11 07:31:02 +0530

    solved number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold in n time

--- number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
+++ number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
@@ -7,7 +7,7 @@
  * @param {number} threshold
  * @return {number}
  */
-var numOfSubarrays = function(arr, k, threshold) {
+var numOfSubarraysBrute = function(arr, k, threshold) {
     
     let left = 0;
     let right = k - 1;
@@ -26,5 +26,31 @@ var numOfSubarrays = function(arr, k, threshold) {
         right++;
         left++;
     }
+    return result;
+};
+
+/**
+ * Linear Time
+ * 
+ * Time O(n) | Space O(1)
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} threshold
+ * @return {number}
+ */
+var numOfSubarrays = function(arr, k, threshold) {
+
+    let left = 0;
+    let right = k - 1;
+    let currentTotal = arr.slice(left, right + 1).reduce((acc, curr) => acc + curr);
+    let result = 0;
+    while(right < arr.length) {
+        if(currentTotal/k >= threshold) result++;
+        currentTotal = currentTotal - arr[left]
+        left++;
+        right++;
+        currentTotal = currentTotal + arr[right];
+    }
+
     return result;
 };
