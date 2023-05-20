f826b8c7e21d36d52f7594314bc6a81a8b7d666e
aadil42 <adilkelawala@gmail.com>
2023-05-20 18:38:23 +0530

    solved longest-palindromic-subsequence

--- last-stone-weight-ii.js
+++ last-stone-weight-ii.js
@@ -24,3 +24,35 @@ var lastStoneWeightII = function(stones) {
 
     return dfs(0);
 };
+
+
+/**
+ * t = total of the input array. In the worst case we might be given an array with a bunch of one 
+ * Time O(n*t) | Space O(n*t)
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeightII = function(stones) {
+    
+    const cache = {};
+    const stonesTotal = stones.reduce((acc, curr) => {
+        return acc + curr;
+    },0);
+
+    const target = Math.ceil(stonesTotal / 2);
+    // console.log(target);
+
+    const dfs = (i, total) => {
+        const hash = `${i}#${total}`;
+        // console.log(hash);
+        // don't check cach[hash] because if the value is 0 then it won't run and keep calling recursively. And It will exeed time limit.
+        if(cache[hash] !== undefined) return cache[hash];
+
+        if(total  >= target || i === stones.length) return Math.abs(total - (stonesTotal - total));
+
+        cache[hash] = Math.min(dfs(i+1, total), dfs(i+1, total+stones[i]));
+        return cache[hash];
+    }
+
+    return dfs(0, 0);
+};
