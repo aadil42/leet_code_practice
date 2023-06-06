25162089f8e5257453ff28ca5d37dd82749f5350
aadil42 <adilkelawala@gmail.com>
2023-06-06 08:25:23 +0530

    solved can-make-arithmetic-progression-from-sequence

---
+++ can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,16 @@
+/**
+ * Time n*log(n) | Space (n)
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function(arr) {
+    
+    arr.sort((a,b) => a-b);
+
+    const diff = arr[1] - arr[0];
+
+    for(let i = 2; i < arr.length; i++) {
+        if(arr[i] - arr[i-1] !== diff) return false;
+    }
+    return true;
+};
