a577b9bb6106a979ff5c27abfe54bbfdf7fb67db
aadil42 <adilkelawala@gmail.com>
2023-06-07 09:08:13 +0530

    solved minimum-flips-to-make-a-or-b-equal-to-c by fliping bits

--- minimum-flips-to-make-a-or-b-equal-to-c.js
+++ minimum-flips-to-make-a-or-b-equal-to-c.js
@@ -7,6 +7,36 @@
  * @return {number}
  */
 var minFlips = function(a, b, c) {
+    
+    let min = 0;
+    while(a || b || c) {
+        const aBit = a & 1;
+        const bBit = b & 1;
+        const cBit = c & 1;
+
+        if((aBit && bBit) && !cBit) {
+            min += 2;
+        } else if( (!aBit && !bBit && cBit) || ((aBit || bBit) && !cBit)) {
+            min++;
+        }
+
+        a >>= 1;
+        b >>= 1;
+        c >>= 1;    
+    }
+
+    return min;
+};
+
+/**
+ * 
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+
+var minFlips1 = function(a, b, c) {
     a = a.toString(2).padStart(32,'0')
     b = b.toString(2).padStart(32,'0')
     c = c.toString(2).padStart(32,'0')
