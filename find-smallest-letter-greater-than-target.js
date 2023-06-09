bd030d01ca115574f83d25723261f38b5d948835
aadil42 <adilkelawala@gmail.com>
2023-06-09 08:48:17 +0530

    solved find-smallest-letter-greater-than-target with binary search in log(n) time

--- find-smallest-letter-greater-than-target.js
+++ find-smallest-letter-greater-than-target.js
@@ -1,12 +1,40 @@
 /**
  * 
+ * Binary Search
+ * Time O(log(n)) | Space O(1)
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    
+    let left = 0;
+    let right = letters.length - 1;
+    const str = letters.join('');
+    let result = letters[0];
+    while(left <= right) {
+        const mid = left + Math.floor((right - left)/2);
+        if(str.charCodeAt(mid) > target.charCodeAt(0)) {
+            result = str[mid];
+            right = mid - 1;
+        }
+        if(str.charCodeAt(mid) <= target.charCodeAt(0)) {
+            left = mid + 1;
+        }
+    }
+
+    return result;
+};
+
+/**
+ * 
  * Linear search
  * Time O(n) | Space O(1)
  * @param {character[]} letters
  * @param {character} target
  * @return {character}
  */
-var nextGreatestLetter = function(letters, target) {
+var nextGreatestLetter1 = function(letters, target) {
     
     const str = letters.join('');
     for(let i = 0; i < letters.length; i++) {
@@ -14,3 +42,4 @@ var nextGreatestLetter = function(letters, target) {
     }
     return str[0];
 };
+
