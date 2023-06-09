7464f73554a4d160dea8ae021fc3da62ce4ebdd6
aadil42 <adilkelawala@gmail.com>
2023-06-09 10:31:35 +0530

    added time and space complexity

--- check-if-a-string-contains-all-binary-codes-of-size-k.js
+++ check-if-a-string-contains-all-binary-codes-of-size-k.js
@@ -1,3 +1,12 @@
+/**
+ * https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+ * 
+ * Hashing
+ * Time O(n*k) | Space O(2^k) (it can't get any bigger than 2^k in the worst case)
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
 var hasAllCodes = function(s, k) {
     
     const bitSet = new Set();
