c2f4322098dfec7507467b619850f6b60437c971
aadil42 <adilkelawala@gmail.com>
2023-06-25 16:56:30 +0530

    solved first-bad-version with linear and binary search

---
+++ first-bad-version.js
@@ -0,0 +1,72 @@
+/**
+ * Binary Search
+ * Definition for isBadVersion()
+ * https://leetcode.com/problems/first-bad-version/
+ * 
+ * Time O(log(n)) | Space O(1)
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 1;
+        let right = n;
+
+        let firstBad = 0;
+        while(left <= right) {
+            const mid = left + Math.floor((right-left)/2);
+            const isBad = isBadVersion(mid);
+            if(isBad) {
+                firstBad = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return firstBad;
+    };
+};
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * Linear Search 
+ * 
+ * Time O(n) | Space O(1)
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution1 = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        while(n) {
+            if(!isBadVersion(n)) return n+1;
+            n--;
+        }
+        return 1;
+    };
+};
