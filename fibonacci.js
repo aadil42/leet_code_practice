7c4caa618d006bd69fce206abd3234a5cc8069ca
aadil42 <adilkelawala@gmail.com>
2023-07-05 08:22:03 +0530

    solved fibonacci with brute force

---
+++ fibonacci.js
@@ -0,0 +1,20 @@
+/**
+ * Brute force
+ * Time O(2^n) | Space O(n)
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    
+    const dfs = (n) => {
+        if(n <= 0) {
+            return 0;
+        }
+        if(n === 1) {
+            return 1;
+        }
+        return dfs(n-1) + dfs(n-2);
+    }
+
+    return dfs(n);
+};
