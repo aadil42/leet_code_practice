21a92902ecfef5ec173611dd1bcbd392a3bb1e55
aadil42 <adilkelawala@gmail.com>
2023-07-05 09:21:25 +0530

    solved longest-subarray-of-1s-after-deleting-one-element with sliding window

--- longest-subarray-of-1s-after-deleting-one-element.js
+++ longest-subarray-of-1s-after-deleting-one-element.js
@@ -1,4 +1,51 @@
 /**
+ * Sliding Window
+ * 
+ * Time O(n) | Space O(1)
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const checkIfAll1 = (nums) => {
+    for(let i = 0; i < nums.length; i++) {
+        if(!nums[i]) return false;
+    }
+    return true;
+}
+
+const longestSubarray = function(nums) {
+
+    if(checkIfAll1(nums)) return nums.length - 1;
+    
+    let max = 0;
+    let current = 0;
+    let zeroCount = 0;
+
+    let left = 0;
+    let right = 0
+
+    while(right < nums.length) {
+        if(nums[right] === 0) zeroCount += 1;
+
+        while(zeroCount > 1 && left < right) {
+            if(nums[left] === 0) zeroCount -= 1;
+            if(nums[left] === 1) current -= 1;
+            left++;
+        }
+
+        if(nums[right] === 1) {
+            current += 1;
+            max = Math.max(current, max);
+        }
+        right++;
+    }
+
+    return max;
+};
+
+
+
+/**
  * 
  * Brute force O(n^2) | Space O(1)
  * @param {number[]} nums
@@ -20,7 +67,7 @@ const getSubArray = (ignoreEl, nums) => {
     return max;
 }
 
-const longestSubarray = function(nums) {
+const longestSubarray1 = function(nums) {
 
     let max = 0;
     for(let i = 0; i < nums.length; i++) {
