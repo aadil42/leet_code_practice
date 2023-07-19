0272f1d45b2aa43310f422e1fad2cc27c3b3870d
aadil42 <adilkelawala@gmail.com>
2023-07-19 08:55:09 +0530

    solved non-overlapping-intervals

--- non-overlapping-intervals.js
+++ non-overlapping-intervals.js
@@ -1,26 +1,28 @@
+/**
+ * Greedy | Sorting
+ * https://leetcode.com/problems/non-overlapping-intervals/
+ * 
+ * Time O(n) | Space O(1)
+ * @param {number[][]} intervals
+ * @return {number}
+ */
 var eraseOverlapIntervals = function(intervals) {
-
-    // sort the arrray
-    intervals = intervals.sort((a,b) => {
-        return a[0] -b[0];
-    });
     
+    intervals = intervals.sort((a, b) => a[0] - b[0]);
+
     let preEnd = intervals[0][1];
     let result = 0;
-    for(let i = 1; i < intervals.length; i++) {
-        if(intervals[i][0] >= preEnd) {
-            // this is non overlapping so just update the preEndjj
-            preEnd = intervals[i][1];
-        } else {
-            result += 1;
-            // this is overlapping so now update the preEnd with the minimum value so we can get rid of the longer overlapping line// draw it out if you don't understand it.
-            preEnd = Math.min(preEnd, intervals[i][1]);
-        }
-    }
 
+    for(let i = 1; i < intervals.length; i++) {
+        if(intervals[i][0] < preEnd) {
+            result++;
+            preEnd = Math.min(preEnd, intervals[i][1]);
+            continue;
+        }
+        preEnd = intervals[i][1];
+    }
     return result;
 };
-
 
 intervals = [[1,2],[1,2],[1,2]];
 console.log(eraseOverlapIntervals(intervals));
