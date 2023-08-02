71e3a8c7a7ce236f440299b29d3fef587c469f0f
aadil42 <adilkelawala@gmail.com>
2023-08-02 09:33:21 +0530

    added another solution for permutation.

--- permutations.js
+++ permutations.js
@@ -1,11 +1,38 @@
 /**
+ * Recursion | Backtracking
+ * 
+ * https://leetcode.com/problems/permutations/
+ * Time O(n^n) | Space O(n!)
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+
+    const result = [];
+    const dfs = (numSet) => {
+        if(numSet.size === nums.length) {
+            result.push([...numSet]);
+            return;
+        }
+        for(let i = 0; i < nums.length; i++) {
+            if(numSet.has(nums[i])) continue;
+            numSet.add(nums[i]);
+            dfs(numSet);
+            numSet.delete(nums[i]);
+        }
+    }
+
+    dfs(new Set());
+    return result;
+};
+/**
  * 
  * Recursion 
  * Time  O(n!) | Space O(n!) 
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
+var permute1 = function(nums) {
     function dfs(nums) {
         if(nums.length === 1) return [nums.slice(0)];
 
