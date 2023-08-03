756766427b018212004018c0d9f60c96b84e81e2
aadil42 <adilkelawala@gmail.com>
2023-08-03 09:05:43 +0530

    added another solution for letter-combinations-of-a-phone-number it's kind of simmiler to the old one.

--- letter_combination_of_phone_number.js
+++ letter_combination_of_phone_number.js
@@ -1,7 +1,49 @@
+/**
+ * Recursion | Backtracking
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ *
+ * Time O(n^n) | Space O(26) 
+ * @param {string} digits
+ * @return {string[]}
+ */
 var letterCombinations = function(digits) {
+
+    if(!digits) return [];
+    const numberToLetter = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+
+    const result = [];
+    const dfs = (currStrArr, index) => {
+        if(currStrArr.length === digits.length) {
+            result.push(currStrArr.join(''));
+            return;
+        }
+
+        const currentDigit = digits[index];
+        const corrospondingDigits = numberToLetter[currentDigit].split('');
+
+        for(let i = 0; i < corrospondingDigits.length; i++) {
+            currStrArr.push(corrospondingDigits[i]);
+            dfs(currStrArr, index+1);
+            currStrArr.pop();
+        }        
+    }
+    dfs([], 0);
+    return result;
+};
+
+var letterCombinations2 = function(digits) {
   
     const result = [];
-    digitsMap = new Map();
+    const digitsMap = new Map();
 
     digitsMap.set('2', 'abc');
     digitsMap.set('3', 'def');
