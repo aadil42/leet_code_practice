32a5583510ad277b8a2c4372a4faa509b3a75d34
aadil42 <adilkelawala@gmail.com>
2023-08-11 10:04:32 +0530

    adding additional helpful comment

--- coin-change-ii.js
+++ coin-change-ii.js
@@ -6,7 +6,7 @@
 var change = function(amount, coins) {
     
     // when doing the caching with just {} it gives TLE error.
-    // but when doing with 2d array it passes.
+    // but when doing with 2d array it passes. Yes and also if you do it with Map it also passes. 
     const cache = new Array(coins.length).fill(-1).map(() => new Array(amount + 1).fill(-1));
     function dfs(index, left) {
         if(left < 0) return 0;
