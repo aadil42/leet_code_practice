81297538320eca577aaebc577b045f813b9345d2
aadil42 <adilkelawala@gmail.com>
2023-08-11 10:06:22 +0530

    adding alternative solution

--- coin-change-ii.js
+++ coin-change-ii.js
@@ -1,9 +1,36 @@
 /**
+ * Recursion | DP
+ * https://leetcode.com/problems/coin-change-ii/
+ * m = number of coins | n = amount
+ * Time O(m*n) | Space O(m*n);
+ * Neater way
  * @param {number} amount
  * @param {number[]} coins
  * @return {number}
  */
 var change = function(amount, coins) {
+
+    const cache = new Map();
+    const dfs = (i, sum) => {
+        const hash = `${i}-${sum}`;
+        if(cache.has(hash)) return cache.get(hash);
+        if(sum === amount) return 1;
+        if(i >= coins.length || sum > amount) return 0;
+        
+        const result = dfs(i, sum + coins[i]) + dfs(i + 1, sum);
+        cache.set(hash, result);
+        return result;
+    }
+
+    return dfs(0, 0);
+};
+
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change1 = function(amount, coins) {
     
     // when doing the caching with just {} it gives TLE error.
     // but when doing with 2d array it passes. Yes and also if you do it with Map it also passes. 
