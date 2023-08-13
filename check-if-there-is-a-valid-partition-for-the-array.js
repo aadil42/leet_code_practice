776a4c123375bef89497ddb447ac1e7b9a0f58a6
aadil42 <adilkelawala@gmail.com>
2023-08-13 13:37:41 +0530

    solved check-if-there-is-a-valid-partition-for-the-array

---
+++ check-if-there-is-a-valid-partition-for-the-array.js
@@ -0,0 +1,37 @@
+/**
+ * Recursion | DP
+ * https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
+ * 
+ * Time O(n) | Space O(n)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var validPartition = function(nums) {
+    
+    const isValid = (start, end) => {
+        if(end - start === 1) return nums[start] === nums[end];
+        if(nums[start] + 1 === nums[start+1] && nums[start+1] + 1 === nums[start+2]) return true;
+        if(nums[start] === nums[start+1] && nums[start+1] === nums[start+2]) return true;
+        return false;
+    }
+
+    const cache = new Map();
+    const dfs = (i) => {
+        if(cache.has(i)) return cache.get(i);
+        if(i >= nums.length) return true;
+
+        let choice1 = false;
+        let choice2 = false;
+        if(isValid(i, i+1)) {
+            choice1 = dfs(i+2);
+        }
+        if(isValid(i, i+2))  {
+            choice2 = dfs(i+3);
+        }
+
+        cache.set(i, choice1 || choice2);
+        return choice1 || choice2;
+    }
+
+    return dfs(0);
+};
