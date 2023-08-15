c20e9db44c59b1f65e45ebc28c00687f0feee5d3
aadil42 <adilkelawala@gmail.com>
2023-08-15 09:54:06 +0530

    added time and space complexity

--- partition-list.js
+++ partition-list.js
@@ -2,6 +2,23 @@
 // you need legit linked list for input to test this solution, which you don't have but you have checked it on leet code
 // it's workin fine. just push it on github as it is. it's legit solution and it's working.
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * List traversal.
+ * https://leetcode.com/problems/partition-list/
+ * 
+ * Time O(n) | Space  O(n)
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+
 
 var partition = function(head, x) {
 
