c985f79e2228a5496d0094ffa03d4d5920164f14
aadil42 <adilkelawala@gmail.com>
2023-08-22 14:01:23 +0530

    solved excel-sheet-column-title

--- excel-sheet-column-title.js
+++ excel-sheet-column-title.js
@@ -1,4 +1,24 @@
 /**
+ * 
+ * Time O(log(n)) | Space O(log(n))
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+
+    const result = [];
+    while(columnNumber > 0) {
+      const remainder = (columnNumber - 1) % 26;
+      const char = String.fromCharCode('A'.charCodeAt(0) + remainder);
+      result.push(char);
+      columnNumber = Math.floor((columnNumber - 1)/26);
+    }
+  
+    return result.reverse().join('');
+  };
+
+
+/**
  * https://leetcode.com/problems/excel-sheet-column-title/
  * 
  * Brute  Force 
@@ -6,7 +26,7 @@
  * @param {number} columnNumber
  * @return {string}
  */
-var convertToTitle = function(columnNumber) {
+var convertToTitle1 = function(columnNumber) {
     const alphabetObj = {
       '1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E', '6': 'F', '7': 'G', '8': 'H', '9': 'I', '10': 'J',
       '11': 'K', '12': 'L', '13': 'M', '14': 'N', '15': 'O', '16': 'P', '17': 'Q', '18': 'R', '19': 'S', '20': 'T',
