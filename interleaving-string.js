5cba6cd288f6e97960ef98404fa692a681d6aa60
aadil42 <adilkelawala@gmail.com>
2023-08-25 12:33:56 +0530

    added second solution to interleaving-string

--- interleaving-string.js
+++ interleaving-string.js
@@ -31,3 +31,30 @@ var isInterleave = function(s1, s2, s3) {
 
     return dfs(0,0,0,'');
 };
+
+/**
+ * Another approch
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave1 = function(s1, s2, s3) {
+    
+  if(s1.length + s2.length !== s3.length) return false;
+  
+  const cache = new Map();
+  function dfs(i, j) {
+      const key = i+'-'+j;
+      if(cache.has(key)) return cache.get(key);
+      if(i === s1.length && j === s2.length) return true;
+
+      if(s1[i] === s3[i+j] && dfs(i+1, j)) return true;
+      if(s2[j] === s3[i+j] && dfs(i, j+1)) return true;
+
+      cache.set(key, false);
+      return cache.get(key);
+  }
+
+  return dfs(0,0);
+};
