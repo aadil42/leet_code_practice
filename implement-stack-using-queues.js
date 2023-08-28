c715def7c997baab4137515315438179f05c5127
aadil42 <adilkelawala@gmail.com>
2023-08-28 10:43:51 +0530

    solved implement-stack-using-queues using queue operations

--- implement-stack-using-queues.js
+++ implement-stack-using-queues.js
@@ -1,4 +1,60 @@
+/** 
+ * Queue
+ * https://leetcode.com/problems/implement-stack-using-queues/
+ * 
+ * @param {number} x
+ * @return {void}
+ */
+
 var MyStack = function() {
+    this.queue = new Queue();
+};
+
+/**
+ * Time O(n) | Space O(n)
+ * @return {null/undefined}
+ */
+MyStack.prototype.push = function(x) {
+    const arr = [x, ...this.queue.toArray()]; // O(n)
+    this.queue = new Queue(arr); 
+};
+
+/**
+ * Time O(1) | Space O(1)
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.queue.dequeue() ?? null;
+};
+
+/**
+ * Time O(1) | Space O(1)
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue.front() ?? null;
+};
+
+/**
+ * Time O(1) | Space O(1)
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue.isEmpty();
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+
+//You have used stack here. You werer told to use only Queue and it operations.
+// But, no worries. It still got accepted. The queue solution is above.
+var MyStack1 = function() {
     this.stack = [];
 };
 
@@ -7,7 +63,7 @@ var MyStack = function() {
  * @param {number} x
  * @return {void}
  */
-MyStack.prototype.push = function(x) {
+MyStack1.prototype.push = function(x) {
     this.stack.push(x);
 };
 
@@ -15,7 +71,7 @@ MyStack.prototype.push = function(x) {
  * Time O(1) | Space O(1)
  * @return {number}
  */
-MyStack.prototype.pop = function() {
+MyStack1.prototype.pop = function() {
     return this.stack.pop();
 };
 
@@ -23,7 +79,7 @@ MyStack.prototype.pop = function() {
  * Time O(1) | Space O(1)
  * @return {number}
  */
-MyStack.prototype.top = function() {
+MyStack1.prototype.top = function() {
     return this.stack[this.stack.length - 1];
 };
 
@@ -31,7 +87,7 @@ MyStack.prototype.top = function() {
  * Time O(1) | Space O(1)
  * @return {boolean}
  */
-MyStack.prototype.empty = function() {
+MyStack1.prototype.empty = function() {
     return this.stack.length === 0;
 };
 
@@ -43,3 +99,4 @@ MyStack.prototype.empty = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.empty()
  */
+
