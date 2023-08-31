5e023ed45894933ffb6f335444bfc60e34cf11fa
aadil42 <adilkelawala@gmail.com>
2023-09-01 04:55:24 +0530

    added time/space complexity and added bruteforce approch

--- counting-bits.js
+++ counting-bits.js
@@ -1,3 +1,11 @@
+/**
+ * 
+ * https://leetcode.com/problems/counting-bits/
+ * Time O(n*log(n)) | Space O(1)
+ * @param {number} n
+ * @return {number[]}
+ */
+
 var countBits = function(n) {
     
     if(!n) return [0];
@@ -23,3 +31,30 @@ function countBit(number) {
     }
     return count;
 }
+
+/**
+ * https://leetcode.com/problems/counting-bits/
+ * Brute Force
+ * 
+ * Time O(n*17) | Space O(n) | 17 because (n) won't exceed 10^5 and we don't require more than 17 bit for 10^5
+ * @param {number} n
+ * @return {number[]}
+ */
+
+// constant time O(1)
+const countBitInNumber = (number) => {
+    return number.toString(2).split('').filter((bit) => bit === '1').length;
+}
+
+// linear time O(n)
+var countBits = function(n) {
+    if(!n) return [0];
+    const bits = [0];
+
+    for(let i = 1;  i < n+1; i++) {
+        bits.push(countBitInNumber(i));
+    }
+
+    return bits;
+};
+
