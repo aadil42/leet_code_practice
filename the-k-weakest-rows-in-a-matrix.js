30998212138950974c0cff29ab09488dffab3566
aadil42 <adilkelawala@gmail.com>
2023-09-18 08:00:23 +0530

    added sorting solution which is n*log(n)

--- the-k-weakest-rows-in-a-matrix.js
+++ the-k-weakest-rows-in-a-matrix.js
@@ -1,4 +1,30 @@
 /**
+ * 
+ * Sorting
+ * Time O(n*log(n)) | Space O(n)
+ * https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+var kWeakestRows = function(mat, k) {
+    
+    const result = [];
+    mat = mat.map((arr,i) => [arr.filter((item) => item === 1).length, i]);
+    mat = mat.sort((a,b) => {
+        if(a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0];
+    });
+    mat = mat.reverse();
+    while(k) {
+        result.push(mat.pop()[1]);
+        k--;
+    }
+    return result;
+};
+
+
+/**
  * Brute Force
  * Time O(n*k) | Space O(k)
  * https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix
@@ -6,7 +32,7 @@
  * @param {number} k
  * @return {number[]}
  */
-var kWeakestRows = function(mat, k) {
+var kWeakestRows1 = function(mat, k) {
     
     const result = [];
     mat = mat.map((arr) => arr.filter((item) => item === 1).length);
