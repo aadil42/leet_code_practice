ddc8319f41dc9ff972ca890884d2db74c1dfaa7b
aadil42 <adilkelawala@gmail.com>
2023-09-21 13:09:16 +0530

    added better solution for log(N+M)

--- median-of-two-sorted-arrays.js
+++ median-of-two-sorted-arrays.js
@@ -51,67 +51,37 @@ var findMedianSortedArraysBF = function(nums1, nums2) {
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-
-    let A = nums1;
-    let B = nums2;
-    if(nums1.length < nums2.length) {
-        A = nums1;
-        B = nums2;
-    } else {
-        B = nums1;
-        A = nums2;
-    }
-
-    const total = nums1.length + nums2.length;
-    const half = Math.floor(total/2);
-
-    let left = 0;
-    let right = A.length - 1;
-
-    while(true) {
-        let i = Math.floor(((right + left)/2));
-        let j = half - i - 2;    
-
-        let Aleft;
-        let Bleft;  
-        let Aright;
-        let Bright;
-
-        if(i >= 0) {
-            Aleft = A[i];
+    
+    if(nums1.length > nums2.length) {
+        [nums1, nums2] = [nums2, nums1];
+      }
+  
+      const total = nums1.length + nums2.length;
+      const half = Math.floor((nums1.length + nums2.length)/2);
+  
+      let left = 0;
+      let right = nums1.length;
+      while(true) {
+  
+        const mid = Math.floor((left+right)/2);
+        const leftFromHalf = half - mid - 2;
+        
+        let left1 = mid >= 0 ? nums1[mid] : -Infinity;
+        let left2 = leftFromHalf >= 0 ? nums2[leftFromHalf] : -Infinity;
+        let right1 = mid+1 < nums1.length ? nums1[mid+1] : Infinity;
+        let right2 = leftFromHalf+1 < nums2.length ? nums2[leftFromHalf+1] : Infinity;
+  
+        if(left1 <= right2 && left2  <= right1) {
+          if(total%2) {
+            return Math.min(right1, right2);
+          }
+          return (Math.max(left1, left2) + Math.min(right1, right2))/2;
+        } 
+  
+        if(left1 < left2) {
+          left = mid + 1; 
         } else {
-            Aleft = -Infinity
+          right = mid - 1;
         }
-
-        if(j >= 0) {
-            Bleft = B[j];
-        } else {
-            Bleft = -Infinity
-        }
-
-        if(i+1 < A.length) {
-            Aright = A[i+1];
-        } else {
-            Aright = Infinity;
-        }
-
-        if(j+1 < B.length) {
-            Bright = B[j+1];
-        } else {
-            Bright = Infinity;
-        }
-
-        if(Aleft <= Bright && Bleft <= Aright) {
-            if(total % 2) {
-                return Math.min(Aright,Bright);
-            }
-            return (Math.max(Aleft, Bleft) + Math.min(Aright, Bright)) / 2;
-        }
-
-        if(Aleft > Bleft) {
-            right = i-1;
-        } else {
-            left = i+1;
-        }
-    }
+      }
 };
