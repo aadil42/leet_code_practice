9950a91c2edb9133f2c04b3e6fd1c1af97d1aca2
aadil42 <adilkelawala@gmail.com>
2023-09-26 14:09:20 +0530

    add stack solution to remove-duplicaet-letters

--- remove-duplicate-letters.js
+++ remove-duplicate-letters.js
@@ -1,9 +1,38 @@
 /**
- * 
+ * Stack 
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/remove-duplicate-letters
  * @param {string} s
  * @return {string}
  */
 var removeDuplicateLetters = function(s) {
+
+    const lastSeen = {};
+    const seen = new Set();
+    const result = [];
+    // add lastSeen chars
+    for(let i = 0; i < s.length; i++) {
+        lastSeen[s[i]] = i;
+    }
+    for(let i = 0; i < s.length; i++) {
+
+        if(seen.has(s[i])) continue; // it is important to check this at the start
+
+        while(result.length && result[result.length - 1] > s[i] && lastSeen[result[result.length - 1]] > i) {
+            seen.delete(result.pop());
+        }
+        result.push(s[i]);
+        seen.add(s[i]);
+    }
+
+    return result.join('');
+};
+/**
+ * DP (Mike, Inspect this code. The above code is done using Stack.)
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicateLetters1 = function(s) {
     
     let targetLen = 0;
     const charSet = new Set();
