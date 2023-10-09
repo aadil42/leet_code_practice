7b52d20c27cccfbd9e3a9ac0b96a3b13281ffe31
aadil42 <adilkelawala@gmail.com>
2023-10-09 08:27:53 +0530

    solved find-first-and-last-position-of-element-in-sorted-array second time

--- find-first-and-last-position-of-element-in-sorted-array.js
+++ find-first-and-last-position-of-element-in-sorted-array.js
@@ -1,12 +1,62 @@
 /**
  * Binary Search
+ * Time O(n*log(n)) | Space O(1)
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    
+  const binarySearch = (searchD) => {
+
+      let left = 0;
+      let right = nums.length - 1;
+      let index = searchD ? -Infinity : Infinity;
+
+      while(left <= right) {
+          const mid = left + Math.floor((right - left)/2);
+
+          if(nums[mid] === target) {
+              if(searchD) {
+                  index = Math.max(index, mid);
+                  left = mid + 1;
+              }
+              if(!searchD) {
+                  index = Math.min(index, mid);
+                  right = mid - 1;
+              }
+          }
+
+          if(target > nums[mid]) {
+              left = mid + 1;
+          }
+
+          if(target < nums[mid]) {
+              right = mid - 1;
+          }
+      }
+
+      return index;
+  }
+
+  const startIdx = binarySearch(0);
+  const endIdx = binarySearch(1);
+  // console.log(startIdx, endIdx)
+  if(startIdx === Infinity || endIdx === -Infinity) return [-1,-1];
+  return [startIdx, endIdx];
+};
+
+/**
+ * Binary Search
  * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
  * Time O(log(n)) | Space O(1)
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
+var searchRange1 = function(nums, target) {
     
     const result = [];
 
