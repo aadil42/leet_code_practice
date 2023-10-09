fd1cfafcf374cf394bdc864155d24d7ccc6d1f78
aadil42 <adilkelawala@gmail.com>
2023-10-09 12:53:52 +0530

    added another solution that brnach in only three directions.

--- max-dot-product-of-two-subsequences.js
+++ max-dot-product-of-two-subsequences.js
@@ -22,6 +22,40 @@ var maxDotProduct = function(nums1, nums2) {
 
         const result = Math.max(nums1[idx1] * nums2[idx2] + dfs(idx1+1, idx2+1, true),
                                 dfs(idx1+1, idx2, taken),
+                                dfs(idx1, idx2+1, taken)
+                              ); // you actually don't need to branch out in forth direction. But that would work too. Dry run and you'll see.
+
+        cache.set(hash, result);
+        return result;                                
+    }
+
+    return dfs(0,0,0);
+};
+
+/**
+ * DP | memoization
+ * Time O(n^2) | Space O(n^2)
+ * https://leetcode.com/problems/max-dot-product-of-two-subsequences
+ * 
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDotProduct1 = function(nums1, nums2) {
+    
+    const cache = new Map();
+    const dfs = (idx1, idx2, taken) => {
+
+        if(idx1 === nums1.length && taken) return 0;
+        if(idx2 === nums2.length && taken) return 0;
+        if(idx1 === nums1.length) return -Infinity;
+        if(idx2 === nums2.length) return -Infinity;
+
+        const hash = `${idx1}-${idx2}-${taken}`;
+        if(cache.has(hash)) return cache.get(hash);
+
+        const result = Math.max(nums1[idx1] * nums2[idx2] + dfs(idx1+1, idx2+1, true),
+                                dfs(idx1+1, idx2, taken),
                                 dfs(idx1, idx2+1, taken),
                                 dfs(idx1+1, idx2+1, taken));
 
@@ -31,3 +65,4 @@ var maxDotProduct = function(nums1, nums2) {
 
     return dfs(0,0,0);
 };
+
