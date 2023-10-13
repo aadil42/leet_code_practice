989346e31e38cc7e650b81474f5fae2d9301d8c3
aadil42 <adilkelawala@gmail.com>
2023-10-13 09:01:26 +0530

    solved min-cost-climbing-stairs

--- min-cost-climbing-stairs.js
+++ min-cost-climbing-stairs.js
@@ -1,10 +1,33 @@
 /**
+ * 
+ * DP | Recursion
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/min-cost-climbing-stairs
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    
+    const cache = new Map();
+    const dfs = (index) => {
+        if(cache.has(index)) return cache.get(index);
+        if(index >= cost.length) return 0;
+
+        const result = Math.min((cost[index] || 0) + dfs(index+1), (cost[index] || 0) + dfs(index+2));
+        cache.set(index, result);
+        return result;
+    }
+
+    return dfs(-1);
+};
+
+/**
  * Recursion
  * Time O(n) | Space O(n)
  * @param {number[]} cost
  * @return {number}
  */
-var minCostClimbingStairs = function(cost) {
+var minCostClimbingStairs1 = function(cost) {
     
     const memo = {
         [cost.length - 1]: cost[cost.length - 1]
