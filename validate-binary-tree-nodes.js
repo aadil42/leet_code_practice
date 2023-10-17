c26161c2cb3e8ebe06c86f846e4311566af7dba1
aadil42 <adilkelawala@gmail.com>
2023-10-17 12:43:18 +0530

    add another solution to validate-binary-tree-nodes

--- validate-binary-tree-nodes.js
+++ validate-binary-tree-nodes.js
@@ -8,7 +8,43 @@
  * @param {number[]} rightChild
  * @return {boolean}
  */
+/**
+ * @param {number} n
+ * @param {number[]} leftChild
+ * @param {number[]} rightChild
+ * @return {boolean}
+ */
 var validateBinaryTreeNodes = function(n, leftChild, rightChild) {
+    
+  const visited = new Set();
+
+  const findRoot = () => {
+    let root = 0;
+    for(let i = 0; i < leftChild.length; i++) {
+      const left = leftChild[i];
+      const right = rightChild[i];
+      if(left ===  root || right === root) root = i;
+    }
+    return root;
+  }
+
+  const dfs = (i) => {
+
+    if(i === -1) return true;
+    if(visited.has(i)) return false;
+
+    const left = leftChild[i];
+    const right = rightChild[i];
+    visited.add(i);
+
+    return dfs(left) && dfs(right);
+  }
+
+  const root = findRoot();
+  return dfs(root) && visited.size === n;
+};
+
+var validateBinaryTreeNodes1 = function(n, leftChild, rightChild) {
     
     const visited = new Set();
 
