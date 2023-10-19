cc9157ed6e7ccfd800b002e725a63f1faba91509
aadil42 <adilkelawala@gmail.com>
2023-10-19 10:21:59 +0530

    add new solution. add time&space complexity

--- backspace-string-compare.js
+++ backspace-string-compare.js
@@ -1,25 +1,30 @@
+/**
+ * Stack 
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/backspace-string-compare/
+ * 
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
 var backspaceCompare = function(s, t) {
     
-    const sStack = [];
+    const sStack  = [];
     const tStack = [];
-    
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] == '#') {
-            sStack.pop();
-        } else {
-            sStack.push(s[i]);
-        }
-    }
-    
-    for(let i = 0; i < t.length; i++) {
-        if(t[i] == '#') {
-            tStack.pop();
-        } else {
-            tStack.push(t[i]);
-        }
-    }
-    console.log(sStack, tStack);
-    return sStack.join('') == tStack.join('');
-};
 
-console.log(backspaceCompare('ab#c', 'ad#c'));
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === "#") {
+            sStack.pop();
+            continue;
+        };
+        sStack.push(s[i]);
+    }
+    for(let i = 0; i < t.length; i++) {
+        if(t[i] === "#") {
+            tStack.pop();
+            continue;
+        };
+        tStack.push(t[i]);
+    }
+    return tStack.join('') === sStack.join('');
+};
