0f6f6ddb522d0df42b01747397e9e079c133abe3
aadil42 <adilkelawala@gmail.com>
2023-10-29 17:20:39 +0530

    added another solution for minimum-equal-sum-of-two-arrays-after-replacing-zeros

--- minimum-equal-sum-of-two-arrays-after-replacing-zeros.js
+++ minimum-equal-sum-of-two-arrays-after-replacing-zeros.js
@@ -7,7 +7,25 @@
  * @param {number[]} nums2
  * @return {number}
  */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
 var minSum = function(nums1, nums2) {
+    let s1 = nums1.reduce((acc, curr) => acc+curr, 0) + nums1.filter((num) => num === 0).length;
+    let s2 = nums2.reduce((acc, curr) => acc+curr, 0) + nums2.filter((num) => num === 0).length;
+    let z1 = nums1.filter((num) => num === 0).length;
+    let z2 = nums2.filter((num) => num === 0).length;
+
+    if(s1 === s2) return s1;
+
+    if(s1 < s2 && z1 > 0) return s2;
+    if(s2 < s1 && z2 > 0) return s1;
+    return -1;
+};
+
+var minSum1 = function(nums1, nums2) {
     let s1 = 0;
     let s2 = 0;
     let z1 = 0;
