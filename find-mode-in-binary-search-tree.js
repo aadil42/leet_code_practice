75d1d15fbcae621f1c2f7c155296befdf56b16d5
aadil42 <adilkelawala@gmail.com>
2023-11-01 09:34:47 +0530

    added another solution

--- find-mode-in-binary-search-tree.js
+++ find-mode-in-binary-search-tree.js
@@ -1,4 +1,54 @@
 /**
+ * Tree traversal
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/find-mode-in-binary-search-tree
+ * 
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var findMode = function(root) {
+
+    const occuranceMap = new Map();
+
+    const dfs = (node) => {
+        if(!node) return 0; 
+
+        if(occuranceMap.has(node.val)) occuranceMap.set(node.val, occuranceMap.get(node.val) + 1);
+        if(!occuranceMap.has(node.val)) occuranceMap.set(node.val, 1);
+
+        dfs(node.left);
+        dfs(node.right); 
+    }
+
+    dfs(root);
+
+    let resultStack = [];
+    for(const [key, val] of occuranceMap) {
+        if(val > (resultStack[resultStack.length - 1] && resultStack[resultStack.length - 1][1])) {
+            resultStack = [[key, val]];
+            continue;
+        } 
+        if(val === (resultStack[resultStack.length - 1] && resultStack[resultStack.length - 1][1]) ) {
+            resultStack.push([key, val]);
+            continue;
+        }
+        if(resultStack.length === 0) resultStack.push([key, val]);
+
+    }
+
+    return resultStack.map((node) => node[0]);
+};
+
+/**
  * Binary Search
  * Time O(n*log(n)) | Space O(n)
  * https://leetcode.com/problems/find-mode-in-binary-search-tree/
@@ -15,7 +65,7 @@
  * @return {number[]}
  */
 
-var findMode = function(root) {
+var findMode1 = function(root) {
     
     const countOccurance = (target) => {
         const dfs = (node) => {
