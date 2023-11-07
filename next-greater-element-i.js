f29870b1fb087ec4749a9476b00d434069c567e3
aadil42 <adilkelawala@gmail.com>
2023-11-07 09:39:58 +0530

    solved  next-greater-element-i

--- next-greater-element-i.js
+++ next-greater-element-i.js
@@ -1,4 +1,37 @@
+/**
+ * Brute force
+ * Time O(n^2) | Space O(1) // if you don't count the ans array which includes the ans then it's O(1) space. We're not using hashmap like the below solution.
+ * https://leetcode.com/problems/next-greater-element-i/
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
 var nextGreaterElement = function(nums1, nums2) {
+    
+    const ans = [];
+
+    for(let i = 0; i < nums1.length; i++) {
+
+        for(let j = 0; j < nums2.length; j++) {
+            if(nums1[i] === nums2[j]) {
+                for(let k = j+1; k < nums2.length; k++){
+                    if(nums1[i] < nums2[k]) {
+                        ans.push(nums2[k]);
+                        break;
+                    };
+                }
+                break;
+            }
+        }
+
+        if(ans.length === i) ans.push(-1);
+    }
+
+    return ans;
+};
+
+
+var nextGreaterElement1 = function(nums1, nums2) {
     
     const AnsArr = [];
     // burte force
