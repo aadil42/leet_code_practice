5481f47179db7a2ce6e55e6f0e6782659aa41991
aadil42 <adilkelawala@gmail.com>
2023-11-28 22:03:26 +0530

    add another way to solve the problem

--- number-of-pairs-of-interchangeable-rectangles.js
+++ number-of-pairs-of-interchangeable-rectangles.js
@@ -1,5 +1,38 @@
-// brute force O(n^2)
+/**
+ * Array | Math
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/
+ * 
+ * @param {number[][]} rectangles
+ * @return {number}
+ */
 var interchangeableRectangles = function(rectangles) {
+    
+    const ratios = rectangles.map((ract) => (ract[0]/ract[1]));
+    ratios.sort((a,b) => a-b);
+    
+    const gaussTotal = (n) => {
+        return (1+n)*(n/2);
+    }
+
+    
+    let left = 0;
+    let right = 0;
+    let total  = 0;
+    console.log(ratios);
+    while(right < ratios.length) {
+        while(right < ratios.length && ratios[right] === ratios[left]) {
+            right++;
+        }
+        total += gaussTotal(right-left-1);
+        left = right;
+    }
+
+    return total;
+};
+
+// brute force O(n^2)
+var interchangeableRectangles1 = function(rectangles) {
     
     let totalPair = 0;
     for(let i = 0; i < rectangles.length; i++) {
@@ -14,7 +47,7 @@ var interchangeableRectangles = function(rectangles) {
 
 
 // O(n) doing some fancy math.
-var interchangeableRectangles = function(rectangles) {
+var interchangeableRectangles2 = function(rectangles) {
     
     const ratioFrequency = {};
 
