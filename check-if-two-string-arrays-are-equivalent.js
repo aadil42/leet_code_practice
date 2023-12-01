e1e5c912d66b85a7293947b51071869a32147585
aadil42 <adilkelawala@gmail.com>
2023-12-01 10:44:24 +0530

    add another concatination solution

--- check-if-two-string-arrays-are-equivalent.js
+++ check-if-two-string-arrays-are-equivalent.js
@@ -37,3 +37,15 @@ var arrayStringsAreEqual = function(word1, word2) {
 
     return true;
 };
+
+/**
+ * Concatination
+ * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual1 = function(word1, word2) {
+    return word1.join('') === word2.join('');
+};
