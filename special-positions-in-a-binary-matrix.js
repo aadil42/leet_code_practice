90a3c0f0032ef9d5b9e42703836ef19b06bbbff2
aadil42 <adilkelawala@gmail.com>
2023-12-13 09:52:12 +0530

    add optimal solution with hashmap

--- special-positions-in-a-binary-matrix.js
+++ special-positions-in-a-binary-matrix.js
@@ -1,4 +1,39 @@
 /**
+ * Hashing
+ * Time O(n^2) | Space O(n+m)
+ * https://leetcode.com/problems/special-positions-in-a-binary-matrix/
+ * 
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var numSpecial = function(mat) {
+    
+    const rowOnes = {};
+    const colOnes = {};
+
+    const ROW = mat.length;
+    const COL = mat[0].length;
+
+    for(let  i = 0; i < ROW; i++) {
+        for(let j = 0; j < COL; j++) {
+            if(mat[i][j]) {
+                rowOnes[i] = rowOnes[i] + 1 || 1;
+                colOnes[j] = colOnes[j] + 1 || 1;
+            }
+        }
+    }
+
+    let specialPos = 0;
+    for(let i = 0; i < ROW; i++) {
+        for(let j = 0; j < COL; j++) {
+            if(mat[i][j] && rowOnes[i] === 1 && colOnes[j] === 1) specialPos++;
+        }
+    }
+
+    return specialPos;
+};
+
+/**
  * Brute Force  
  * Time O(n^3) | Space O(1)
  * https://leetcode.com/problems/special-positions-in-a-binary-matrix
@@ -6,7 +41,7 @@
  * @param {number[][]} mat
  * @return {number}
  */
-var numSpecial = function(mat) {
+var numSpecial1 = function(mat) {
     
     const ROW = mat[0].length;
     const COL = mat.length;
