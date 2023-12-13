837e591364cf13cc606438a976c5f7ad852c6f18
aadil42 <adilkelawala@gmail.com>
2023-12-13 08:49:41 +0530

    solved special-positions-in-a-binary-matrix

---
+++ special-positions-in-a-binary-matrix.js
@@ -0,0 +1,38 @@
+/**
+ * Brute Force  
+ * Time O(n^3) | Space O(1)
+ * https://leetcode.com/problems/special-positions-in-a-binary-matrix
+ * 
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var numSpecial = function(mat) {
+    
+    const ROW = mat[0].length;
+    const COL = mat.length;
+
+    const checkRow = (r, c) => {
+        for(let i = 0; i < ROW; i++) {
+            if(i === c) continue;
+            if(mat[r][i] === 1) return false;
+        }
+        return true;
+    }
+
+    const checkCol = (r, c) => {
+        for(let i = 0; i < COL; i++) {
+            if(i === r) continue;
+            if(mat[i][c] === 1) return false;
+        }
+        return true;
+    }
+
+    let specialPos = 0;
+    for(let i = 0; i < ROW; i++) {
+        for(let j = 0; j < COL; j++) {
+            if(mat[j][i] && checkCol(j,i) && checkRow(j,i)) specialPos++; 
+        }
+    }
+
+    return specialPos;
+};
