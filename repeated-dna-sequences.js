21b7136527c77d26029c7054774458a21c998bfe
aadil42 <adilkelawala@gmail.com>
2023-12-18 12:10:12 +0530

    add alternate solution

--- repeated-dna-sequences.js
+++ repeated-dna-sequences.js
@@ -1,3 +1,25 @@
+
+/**
+ * Hashing
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/repeated-dna-sequences/
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function(s) {
+    
+    const sequenceSet = new Set();
+    const resultSet = new Set();
+    s = s.split('');
+    for(let i = 0; i < s.length; i++) {
+        let dnaSequence = ((i+9 < s.length) && s.slice(i, i+10).join("")) || "";
+        if(sequenceSet.has(dnaSequence) && dnaSequence) resultSet.add(dnaSequence);
+        if(dnaSequence) sequenceSet.add(dnaSequence);
+    }
+
+    return [...resultSet];
+};
+
 /**
  * https://leetcode.com/problems/repeated-dna-sequences/
  * Hashing
@@ -6,7 +28,7 @@
  * @return {string[]}
  */
 
-var findRepeatedDnaSequences = function(s) {
+var findRepeatedDnaSequences1 = function(s) {
 
     const sequanceStack = new Set();
     let resultStack = new Set();
