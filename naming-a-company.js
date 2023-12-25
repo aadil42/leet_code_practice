cb45f8ca08af2e40ffce7b982eb8d85af9cb1b74
aadil42 <adilkelawala@gmail.com>
2023-12-25 22:39:56 +0530

    solved naming-a-company

--- naming-a-company.js
+++ naming-a-company.js
@@ -1,11 +1,53 @@
 /**
+ * Hashing 
+ * Time O(n*26^2) | Space O(n);
+ * @param {string[]} ideas
+ * @return {number}
+ */
+var distinctNames = function(ideas) {
+
+    const sets = [];
+
+    for(let i = 0; i < 26; i++) {
+        sets.push(new Set());
+    }
+
+    for(let i = 0; i < ideas.length; i++) {
+        const firstLatter = ideas[i][0];
+        sets[firstLatter.charCodeAt(0) - 97].add(ideas[i].slice(1));
+    }
+
+    const same = [];
+    for(let i = 0; i < 26; i++) {
+        same.push(Array(26).fill(0));
+    }
+
+    for(let i = 0; i < 26; i++) {
+        for(let suffix of sets[i]) {
+            for(let j = i+1; j < 26; j++) {
+                if(sets[j].has(suffix)) same[i][j]++;
+            }
+        }
+    }
+
+    let total = 0;
+    for(let i = 0; i < 26; i++) {
+        for(let j = i+1; j < 26; j++) {
+            total += (sets[i].size - same[i][j]) * (sets[j].size - same[i][j]) * 2;
+        }
+    }
+
+    return total;
+};
+
+/**
  * Brute Force | Set
  * Time O(n^2) | Space O(n)
  * https://leetcode.com/problems/naming-a-company/
  * @param {string[]} ideas
  * @return {number}
  */
-var distinctNames = function(ideas) {
+var distinctNames1 = function(ideas) {
     
     const uniqueNameSet = new Set(ideas);
 
