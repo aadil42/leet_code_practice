fad75693c524c621dc7efec12269a76dcd4e09ec
aadil42 <adilkelawala@gmail.com>
2023-12-26 13:54:23 +0530

    add another solution

--- minimum-penalty-for-a-shop.js
+++ minimum-penalty-for-a-shop.js
@@ -1,12 +1,42 @@
 /**
- * Linear
+ * Array
+ * Time O(n) | Space O(1);
+ * https://leetcode.com/problems/minimum-penalty-for-a-shop/
+ * @param {string} customers
+ * @return {number}
+ */
+var bestClosingTime = function(customers) {
+    
+  let totalY = customers.split("").filter((char) => char === "Y").length;
+  let yTaken = 0;
+  let nTaken = 0;
+  let minPanelty = Infinity;
+  let minHour = 0;
+  
+  let i = 0; 
+  while(i < customers.length + 1) {
+      const currPanelty = (totalY - yTaken) + nTaken;
+      if(currPanelty < minPanelty) {
+          minPanelty = currPanelty;
+          minHour = i;
+      }
+      if(customers[i] === "Y") yTaken++;
+      if(customers[i] === "N") nTaken++;
+      i++; 
+  }
+  return minHour;
+  
+};
+
+/**
+ * Array
  * https://leetcode.com/problems/minimum-penalty-for-a-shop/
  * 
  * Time O(n) | Space O(1)
  * @param {string} customers
  * @return {number}
  */
-var bestClosingTime = function(customers) {
+var bestClosingTime1 = function(customers) {
 
     // count open penelty
     let op = 0;
