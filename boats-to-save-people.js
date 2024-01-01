e34e526f2dc19a14eec2c6a110d4a7a645630220
aadil42 <adilkelawala@gmail.com>
2024-01-01 16:39:39 +0530

    add alternate solution

--- boats-to-save-people.js
+++ boats-to-save-people.js
@@ -1,9 +1,39 @@
 /**
+ * Time O(n*log(n)) | Space O(n);
+ * Sorting | Two Pointer
+ * https://leetcode.com/problems/boats-to-save-people/
  * @param {number[]} people
  * @param {number} limit
  * @return {number}
  */
- var numRescueBoats = function(people, limit) {
+var numRescueBoats = function(people, limit) {
+    
+    let left = 0;
+    let right = people.length - 1;
+    people = people.sort((a,b) => a-b);
+
+    let boats = 0;
+    while(left <= right) {
+
+        if(people[left] + people[right] <= limit) {
+            boats++;
+            left++;
+            right--;
+            continue;
+        }
+
+        boats++;
+        right--;
+    }
+    return boats;
+};
+
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+ var numRescueBoats1 = function(people, limit) {
     people.sort((a,b) => a-b);
 
     let left = 0;
