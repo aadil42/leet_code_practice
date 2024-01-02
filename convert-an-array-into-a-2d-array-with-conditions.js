18fefb6b501c1cebd92dfa863e9118bc98cc3df4
aadil42 <adilkelawala@gmail.com>
2024-01-02 11:49:13 +0530

    added another solution

--- convert-an-array-into-a-2d-array-with-conditions.js
+++ convert-an-array-into-a-2d-array-with-conditions.js
@@ -32,6 +32,46 @@ var findMatrix = function(nums) {
 };
 
 /**
+ * Hashing 
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions
+ * 
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var findMatrix2 = function(nums) {
+    
+    const twoDArray = [];
+    const numFrequency = {};
+    let DistinctIntegerSize = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if(!numFrequency[num]) {
+            DistinctIntegerSize++;
+            numFrequency[num] = 1;
+        } else {
+            numFrequency[num] += 1;
+        }
+    }
+
+    while(DistinctIntegerSize) {
+        const currArr = [];
+        for(const key in numFrequency) {
+            currArr.push(key);
+            numFrequency[key] = numFrequency[key] - 1;
+            if(!numFrequency[key]) {
+                delete numFrequency[key];
+                DistinctIntegerSize--;
+            }
+        }
+        twoDArray.push(currArr);
+    }
+
+    return twoDArray;
+};
+
+/**
  * Brute Force | Hash
  * Time O(n^2) | Space O(n)
  * https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/
