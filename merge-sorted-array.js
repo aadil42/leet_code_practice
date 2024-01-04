0a30ea144c2d912c58447a3e85aa09150ba00bf7
aadil42 <adilkelawala@gmail.com>
2024-01-04 15:23:08 +0530

    add alternet solution

--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -1,4 +1,39 @@
 /**
+ * Array | Two Pointer
+ * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/merge-sorted-array/
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+
+    let k = m + n - 1;
+    m =  m - 1;
+    n =  n - 1;
+
+    while(m > -1 && n > -1) {
+        if(nums1[m] >= nums2[n]) {
+            nums1[k] = nums1[m];
+            m--;
+        } else {
+            nums1[k] = nums2[n];
+            n--;
+        }
+        k--;
+    }
+
+    while(n > -1) {
+        nums1[k] = nums2[n];
+        n--;
+        k--;
+    }
+};
+
+
+/**
  * Linear 
  * Time O(N) | Space O(1)
  * https://leetcode.com/problems/merge-sorted-array/
@@ -8,7 +43,7 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- var merge = function(nums1, m, nums2, n) {
+ var merge1 = function(nums1, m, nums2, n) {
     
     let k = m + n - 1;
     m = m - 1;
