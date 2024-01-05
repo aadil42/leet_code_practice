07b4306a3cb8afd095b870427ff77717f220f22c
aadil42 <adilkelawala@gmail.com>
2024-01-05 12:48:10 +0530

    add recursive solution with time:n^2 and space:n^2

--- longest-increasing-subsequence.js
+++ longest-increasing-subsequence.js
@@ -1,4 +1,38 @@
+/**
+ * DP | Recursion
+ * Time O(n^2) | Space O(n^2) 
+ * https://leetcode.com/problems/longest-increasing-subsequence/
+ * Note: The Time complexity is fine so is the space complexity. But for some reason this solution is not getting accepted on leetcode
+ * // Convert this code to C++ with the help of chatGPT(make sure to use 2d array for caching) and it will get submitted.
+ * // The logic of the code is fine. So, don't worry. You did good.
+ * @param {number[]} nums
+ * @return {number}
+ */
 var lengthOfLIS = function(nums) {
+
+
+    const cache = new Map();
+
+    const dfs = (cur, pre) => {
+        const hash = `${cur}-${pre}`;
+        
+        if(cache.has(hash)) return cache.get(hash);
+        if(cur === nums.length) return 0;
+
+        let max = 0;
+        if(nums[cur] > nums[pre] || nums[pre] === undefined) {
+            max = 1+dfs(cur+1, cur);
+        } 
+        
+        const result = Math.max(max, dfs(cur+1, pre));
+        cache.set(hash, result);
+        return result;
+    }
+
+    return dfs(0, -1);
+};
+
+var lengthOfLIS1 = function(nums) {
     
 const dpArray = new Array(nums.length).fill(1);
 
@@ -13,11 +47,8 @@ for(let i = nums.length - 1; i > -1; i--) {
 return Math.max(...dpArray);
 };
 
-const nums = [10,9,2,5,3,7,101,18];
-console.log(lengthOfLIS(nums));
-
 // solved second time
-var lengthOfLISR = function(nums) {
+var lengthOfLISR2 = function(nums) {
     
     let DpArra = new Array(nums.length).fill(1);
     
@@ -30,4 +61,4 @@ var lengthOfLISR = function(nums) {
     }
     
     return Math.max(...DpArra);
-    };
+};
