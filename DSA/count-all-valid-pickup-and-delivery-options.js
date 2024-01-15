be0f92fe0d604a75cb6b49f74e2f8530b1049203
aadil42 <adilkelawala@gmail.com>
2024-01-15 16:38:10 +0530

    add recursive solution

--- DSA/count-all-valid-pickup-and-delivery-options.js
+++ DSA/count-all-valid-pickup-and-delivery-options.js
@@ -19,3 +19,26 @@ var countOrders = function(n) {
 
     return result % mod;
 };
+
+/**
+ * Recursion | Math | Combination.
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
+ * @param {number} n
+ * @return {number}
+ */
+var countOrders = function(n) {
+    
+    const mod = 10**9 + 7;
+    const slots = 2*n;
+
+    const calcWays = (n) => {
+        return (n)*(n-1)/2;
+    }
+    const dfs = (slots) => {
+        if(slots === 2) return 1; 
+        return (calcWays(slots)*dfs(slots-2))%mod;
+    }
+
+    return dfs(slots);
+};
