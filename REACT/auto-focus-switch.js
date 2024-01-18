c9a5e79678ad46dc31a2af09527220aee7c62f1e
aadil42 <adilkelawala@gmail.com>
2024-01-18 10:25:03 +0530

    add auto-focus-switch functionality in react

--- REACT/auto-focus-switch.js
+++ REACT/auto-focus-switch.js
@@ -1,7 +1,7 @@
 // React doesn't exist in this repo so it's just  the code.
 // if you want this code to run, you have to create React environment.
 
-import React, { useRef } from 'react';
+import React, {useState, useRef, useEffect } from 'react';
 
 /**
  * @typedef {Object} Element
@@ -11,78 +11,81 @@ import React, { useRef } from 'react';
  */
 
 /**
- * This should work. 
- * The reason it's not passing because of the test cases.
- * In this code, we're only moving to the next element if we try to type a char and then we exceed.
- * for example. say you have type 3 char so far and the limit is 3 char also. We  won't move to the next element unless we try to type the 4th char in it.
- * 
  * @param {Object} props
  * @param {Element[]} props.elements
  * @return {JSX.Element}
  */
 export const AutoFocusSwitch = ({ elements }) => {
+    
+    const refs = useRef(elements.map((el) => React.createRef()));
 
-    const refs = useRef(elements.map((element) => React.createRef()));
-    const onKeyDownHandler = (e, maxLen, currIndex) => {
-        if(!/[0-9]*/.test(e.key) || e.key === "e" || e.key === "E") {
-            e.preventDefault();
-            return;
-        }
-        const currentInput = refs.current[currIndex].current;
-
-        if(/[0-9]/.test(e.key)) {
-            console.log(currentInput.value.length, maxLen);
-            if(currentInput.value.length === maxLen) {
-                const nextInput = getNextEl(currIndex);
-                console.log(nextInput, 'next', currentInput.value.length);
-                if(nextInput) {
-                    nextInput.focus();
-                    return;
+    const [hyphens, setHypen] = useState(elements.map((el) => "-"));
+    
+    useEffect(() => {
+        setHypen((hyphens) => {
+            return hyphens.map((hyphen, idx) => {
+                if(idx === hyphens.length - 1) {
+                    return "";
                 }
-                e.preventDefault();
-            }
-        }
+                return hyphen;
+            });
 
-        if(e.key === "Backspace") {
-            if(currentInput.value.length === 0) {
-                const preInput = getPreEl(currIndex);
-                if(preInput) {
-                    preInput.focus();
-                }
-            }
-        }   
-    }
-
-    const getNextEl = (currIndex) => {
-        let i = currIndex + 1;
-        while(refs.current[i] && refs.current[i].current.type !== "number") {
-            i++;
-        }
-        return refs.current[i] && refs.current[i].current;
-    }
-
-    const getPreEl = (currIndex) => {
-        let i = currIndex - 1;
-        while(refs.current[i] && refs.current[i].current.type !== "number") {
-            i--;
-        }
-
-        return refs.current[i] && refs.current[i].current;
-    }
-
-    return (
-        elements.map((element, index) => {
-            return <>
-                    <input 
-                        {...element}
-                        key={index}
-                        ref={refs.current[index]}
-                        onKeyDown={(e) => {
-                            onKeyDownHandler(e, +element.maxLength, index);
-                        }}
-                    />
-                    {(index !== elements.length - 1) && <span>-</span>}   
-                   </>
         })
-    )
+    }, []);
+    
+    const onChangeHandler = (e, el, idx) => {
+        // change the value
+        // check if it exeeds
+            // if it does then move to the next el.
+        const typedDigit = e.target.value.replace(/\D/g, '');
+
+        refs.current[idx].current.value = typedDigit.slice(0, +el.maxLength);
+
+        if(refs.current[idx].current.value.length === +el.maxLength) {
+            // console.log('hehe from inside');
+            let nextEl = idx+1;
+            while(refs.current[nextEl] && refs.current[nextEl].current.type !== "number") {
+                   nextEl++;
+            }
+
+            refs.current[nextEl]?.current.focus();
+        }    
+    }
+
+    const onKeyDownHandler = (e, el, idx) => {
+
+        if('.-'.includes(e.key)) e.preventDefault();
+
+        if(e.key === "Backspace" && e.target.value === "") {
+            let preIdx = idx-1;
+            while(refs.current[preIdx] && refs.current[preIdx].current.type !== "number") {
+                preIdx--;
+            }
+
+            if(refs.current[preIdx]) {
+                e.preventDefault();
+                refs.current[preIdx].current.value = refs.current[preIdx].current.value.slice(0, -1);
+                refs.current[preIdx].current.focus();
+
+            }
+        }
+    }
+
+    
+     
+    return (
+        elements.map((el, idx) => {
+            return <> 
+                        <input 
+                            ref={refs.current[idx]}
+                            key={idx}
+                            onChange={(e) => {onChangeHandler(e, el, idx)}}
+                            onKeyDown={(e) => {onKeyDownHandler(e, el, idx)}}
+                            type={el.type}
+                            id={el.id}
+                        />
+                        {hyphens[idx]}
+                    </>
+        })
+    );
 }
