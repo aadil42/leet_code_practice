85232749ad91df313039a553a00ffefc6227511f
aadil42 <adilkelawala@gmail.com>
2024-01-25 10:41:09 +0530

    add recursive solution

--- DSA/longest-common-subsequence.js
+++ DSA/longest-common-subsequence.js
@@ -1,4 +1,45 @@
+/**
+ * DP | Recursion | Caching
+ * Time O(n^2) | Space O(n^2)
+ * https://leetcode.com/problems/longest-common-subsequence/
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
 var longestCommonSubsequence = function(text1, text2) {
+    
+    const cache = new Map();
+
+    const dfs = (i1, i2) => {
+
+        const hash = `${i1}-${i2}`;
+        
+        if(cache.has(hash)) return cache.get(hash);
+        
+        if(i1  === text1.length || i2 === text2.length) return 0;
+
+        let result = 0;
+
+        if(text1[i1] === text2[i2]) {
+            result = 1 + dfs(i1+1, i2+1);
+        }
+        result = Math.max(result, dfs(i1+1, i2+1));
+        result = Math.max(result, dfs(i1+1, i2));
+        result = Math.max(result, dfs(i1, i2+1));
+
+        cache.set(hash, result);
+        return result;
+    }
+
+    return dfs(0,0);
+};
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence1 = function(text1, text2) {
     const row = text1.length + 1;
     const column = text2.length + 1;
 
