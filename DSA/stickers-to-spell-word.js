6f3fc2b8039aedab9600028c648bfbed8c78e668
aadil42 <adilkelawala@gmail.com>
2024-02-06 17:00:17 +0530

    add brute force solution for stickers-to-spell-word

--- DSA/stickers-to-spell-word.js
+++ DSA/stickers-to-spell-word.js
@@ -52,12 +52,69 @@ var minStickers = function(stickers, target) {
     return result !== Infinity ? result : -1;
 };
 
-const stickers = ["notice","possible"], target = "basicbasic";
+/**
+ * Brute Force | Backtracking | Recursion
+ * Time O(3^n) | Space O(n)
+ * https://leetcode.com/problems/stickers-to-spell-word/
+ * @param {string[]} stickers
+ * @param {string} target
+ * @return {number}
+ */
+var minStickers1 = function(stickers, target) {
 
-console.log(minStickers(stickers, target));
+    const targetFrequency = {};
 
-// if(Object.keys({}).length) {
-//     console.log(true);
-// } else {
-//     console.log(false);
-// }
+    for(let i = 0; i < target.length; i++) {
+        targetFrequency[target[i]] = (targetFrequency[target[i]] &&  targetFrequency[target[i]] + 1) || 1;
+    }
+
+    const backTrack = (frequency, subtractedChars) => {
+        for(let i = 0; i < subtractedChars.length; i++) {
+            const char = String.fromCharCode(i + 97);
+            if(subtractedChars[i]) {
+                frequency[char] += subtractedChars[i]; 
+            }
+        }
+    }
+
+    const isTargetFrequencyEmpty = (frequency) => {
+        for(let key in frequency) {
+            if(frequency[key]) return false;
+        }
+        return true;
+    }
+
+    const dfs = (i, frequency) => {
+        
+        if(isTargetFrequencyEmpty(frequency)) return 0;
+        if(i === stickers.length) return Infinity;
+
+        let atLeastOneCharExists = false;
+
+        // this will keep track of how many chars we have deleted from targetCharFreqeuncy
+        //  so that we can add them back when backtracking.
+        
+        const subtractedChars = new Array(26).fill(0);
+
+        for(let j = 0;  j < stickers[i].length; j++) {
+            if(frequency[stickers[i][j]]) {
+                atLeastOneCharExists = true;
+                subtractedChars[stickers[i][j].charCodeAt(0) - 97] += 1;
+                frequency[stickers[i][j]] -= 1;
+            }
+        }
+
+        if(atLeastOneCharExists) {
+            let currentMin = Infinity;
+            currentMin = Math.min(1+dfs(i, frequency), 1+dfs(i+1, frequency));
+            backTrack(frequency, subtractedChars);
+            currentMin = Math.min(currentMin, dfs(i+1, frequency));
+            return currentMin;
+        }
+        
+        return dfs(i+1, frequency);
+    }
+
+    const result = dfs(0, targetFrequency);
+    return ((result === Infinity) && -1) | result;
+};
