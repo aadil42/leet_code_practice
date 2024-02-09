1cef1740b2b6433420ccfce5eda137a5c9059ea6
aadil42 <adilkelawala@gmail.com>
2024-02-09 12:49:29 +0530

    add better solution

--- DSA/integer-break.js
+++ DSA/integer-break.js
@@ -1,8 +1,42 @@
 /**
+ * DP | Memoization | Recursion
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/integer-break/
  * @param {number} n
  * @return {number}
  */
 var integerBreak = function(n) {
+
+    // excepiton cases.
+    if(n === 3) return 2;
+    if(n === 2)  return 1;
+
+    const cache = new Map();
+    const dfs = (leftNum) => {
+
+        if(cache.has(leftNum)) return cache.get(leftNum);
+        if(leftNum === 0) return 1;
+
+        let i = 1;
+        let max = 0;
+        while(i <= leftNum) {
+            if(leftNum-i < 0) break;
+            max = Math.max(max, i * dfs(leftNum-i));
+            i++;
+        }
+
+        cache.set(leftNum, max);
+        return max;
+    }
+
+    return dfs(n);
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak1 = function(n) {
     
     const dp = {};
     dp[1] = 1;
