38632a3850e2ea4c97d572a1dde53db86803e57d
aadil42 <adilkelawala@gmail.com>
2024-02-10 10:32:10 +0530

    add simpler code for check-if-there-is-a-valid-partition-for-the-array

--- DSA/check-if-there-is-a-valid-partition-for-the-array.js
+++ DSA/check-if-there-is-a-valid-partition-for-the-array.js
@@ -1,12 +1,63 @@
 /**
  * Recursion | DP
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
+ * 
+ * easy to understand and readable solution. 
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var validPartition = function(nums) {
+    
+    const cache = new Map();
+    const dfs = (i) => {
+
+        if(cache.has(i)) return cache.get(i);
+
+        if(i === nums.length) return true;
+        if(i > nums.length) return false;
+
+        let isValid = false;
+        if(nums[i] === nums[i+1]) {
+            isValid = dfs(i+2);
+            if(isValid) {
+                cache.set(i, true);
+                return true;
+            }
+        }
+
+        if(nums[i] === nums[i+1] && nums[i+1] === nums[i+2]) {
+            isValid = dfs(i+3);
+            if(isValid) {
+                cache.set(i, true);
+                return true;
+            }
+        }
+
+        if(nums[i] + 1 === nums[i+1] && nums[i+1] + 1 === nums[i+2]) {
+            isValid = dfs(i+3);
+            if(isValid) {
+                cache.set(i, true);
+                return true;
+            }
+        }
+
+        cache.set(i, false);
+        return false;
+    }
+
+    return dfs(0);
+};
+
+/**
+ * Recursion | DP
  * https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
  * 
  * Time O(n) | Space O(n)
  * @param {number[]} nums
  * @return {boolean}
  */
-var validPartition = function(nums) {
+var validPartition1 = function(nums) {
     
     const isValid = (start, end) => {
         if(end - start === 1) return nums[start] === nums[end];
