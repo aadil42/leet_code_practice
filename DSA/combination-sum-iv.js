6d99779799c0137ad28be05daf8ad310d49271bd
aadil42 <adilkelawala@gmail.com>
2024-02-10 15:28:33 +0530

    solved combination-sum-iv again

--- DSA/combination-sum-iv.js
+++ DSA/combination-sum-iv.js
@@ -1,12 +1,43 @@
 /**
- * DP | Recursion
- * Time O(n*m) | Space O(n)
- * https://leetcode.com/problems/combination-sum-iv
+ * DP | Recursion | Memoization
+ * Time O(n*m) | Space O(n*m)
+ * https://leetcode.com/problems/combination-sum-iv/
+ * 
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
 var combinationSum4 = function(nums, target) {
+    
+    const cache = new Map();
+
+    const dfs = (leftNum) => {
+
+        if(cache.has(leftNum)) return cache.get(leftNum);
+        if(leftNum === 0) return 1;
+
+        let total = 0;
+        for(let i = 0; i < nums.length; i++) {
+            if(leftNum - nums[i] < 0) continue;
+            total += dfs(leftNum - nums[i]);
+        }
+
+        cache.set(leftNum, total);
+        return total;
+    }
+
+    return dfs(target);
+};
+
+/**
+ * DP | Recursion
+ * Time O(n*m) | Space O(n*m)
+ * https://leetcode.com/problems/combination-sum-iv
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4Again = function(nums, target) {
     
     const cache = new Map();
     const dfs = (remaining) => {
