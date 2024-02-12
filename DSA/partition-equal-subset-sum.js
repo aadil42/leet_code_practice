b73dd2db2665f605625ebd55f77617da19c9958f
aadil42 <adilkelawala@gmail.com>
2024-02-12 20:54:06 +0530

    add recursive solution to partition-equal-subset-sum with n^2 time complexity

--- DSA/partition-equal-subset-sum.js
+++ DSA/partition-equal-subset-sum.js
@@ -1,8 +1,41 @@
 /**
+ * DP | Recursion | Memoization
+ * Time O(n^2) | Space O(n^2)
+ * https://leetcode.com/problems/partition-equal-subset-sum/
+ * 
  * @param {number[]} nums
  * @return {boolean}
  */
 var canPartition = function(nums) {
+
+    const total = nums.reduce((acc, num) => acc+num, 0);
+
+    if(total%2) return false;
+
+    const cache = new Map();
+
+    const dfs = (i, leftNum) => {
+        const hash = `${i}-${leftNum}`;
+
+        if(cache.has(hash)) return cache.get(hash);
+
+        if(leftNum === 0) return true;
+        if(i === nums.length || leftNum < 0) return false;
+
+        const result = dfs(i+1, leftNum - nums[i]) || dfs(i+1, leftNum);
+        cache.set(hash, result);
+
+        return result;
+    }
+
+    return dfs(0, total/2);
+};
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition1 = function(nums) {
     
     if(sum(nums) % 2) return false;
     const target = sum(nums) / 2;
