a961d16e681184a3f9fd76927ccae6ca4e744139
aadil42 <adilkelawala@gmail.com>
2024-02-12 13:18:56 +0530

    add recursive solution with n^2 solution

--- DSA/delete-and-earn.js
+++ DSA/delete-and-earn.js
@@ -46,3 +46,35 @@ var deleteAndEarn = function(nums) {
     return earn2;
     
     };
+
+/**
+ * Recursion | DP | Memoization
+ * Time O(n^2) | Space O(n^2)
+ * https://leetcode.com/problems/delete-and-earn/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var deleteAndEarn1 = function(nums) {
+    
+    nums.sort((a,b) => a-b);
+
+    const cache = new Map();
+
+    const dfs = (i, pre) => {
+        const hash = `${i}-${pre}`;
+
+        if(cache.has(hash)) return cache.get(hash);
+
+        if(i >= nums.length) return 0;
+
+        while(pre !== -1 && (nums[pre] + 1 === nums[i]) ) {
+            i++;
+        }
+
+        const result = Math.max( (nums[i] || 0) + dfs(i+1, i), dfs(i+1, pre));
+        cache.set(hash, result); 
+        return result
+    }
+
+    return dfs(0,-1);
+};
