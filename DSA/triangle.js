b66bfc4e0d078b426193b3f8fd3cf2e4b0008ff2
aadil42 <adilkelawala@gmail.com>
2024-02-12 16:38:42 +0530

    solve triangle with recursion

--- DSA/triangle.js
+++ DSA/triangle.js
@@ -1,8 +1,39 @@
 /**
+ * Recursion | DP | Caching
+ * Time O(n^2) | Space O(n^2)
+ * https://leetcode.com/problems/triangle/
  * @param {number[][]} triangle
  * @return {number}
  */
 var minimumTotal = function(triangle) {
+
+    const outOfBound = (r,c) => {
+        if(r === triangle.length || c === triangle[r].length) return true;
+        return false;
+    }
+
+    const cache = new Map();
+
+    const dfs = (r,c) => {
+
+        const hash = `${r}-${c}`;
+
+        if(cache.has(hash)) return cache.get(hash);
+
+        if(outOfBound(r,c)) return 0;
+        const result = Math.min(triangle[r][c] + dfs(r+1, c), triangle[r][c] + dfs(r+1, c+1));
+        cache.set(hash, result);
+        return result;
+    }
+
+    return dfs(0,0);
+};
+
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal1 = function(triangle) {
     
     const dp = new Array(triangle[triangle.length - 1].length + 1).fill(0);
     
