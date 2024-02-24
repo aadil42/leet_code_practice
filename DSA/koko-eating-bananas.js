95f02ec30114658552eb88b17bcf1a756a02f788
aadil42 <adilkelawala@gmail.com>
2024-02-24 15:42:30 +0530

    add alternative code  to solve koko-eating-bananas

--- DSA/koko-eating-bananas.js
+++ DSA/koko-eating-bananas.js
@@ -1,11 +1,52 @@
 /**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    
+    
+    const canEatInTime = (speed) => {
+        
+        let timeTaken = 0;
+        let i = 0;
+
+        while(i < piles.length) {
+            timeTaken += Math.ceil(piles[i]/speed);
+            i++;
+        }
+
+        return timeTaken <= h;
+    }
+
+    let minSpeed = 1;
+    let maxSpeed = Math.max(...piles);
+
+    let slowestSpeedK;
+
+    while(minSpeed <= maxSpeed) {
+
+        const mid = minSpeed + Math.floor((maxSpeed-minSpeed)/2);
+        
+        if(canEatInTime(mid)) {
+            slowestSpeedK = mid;
+            maxSpeed = mid - 1;
+        } else {
+            minSpeed = mid + 1;
+        }
+    }
+
+    return slowestSpeedK;
+};
+
+/**
  * Binary Search
  * Time O(log(n)*n) | Space O(1)
  * @param {number[]} piles
  * @param {number} h
  * @return {number}
  */
-var minEatingSpeed = function(piles, h) {
+var minEatingSpeed1 = function(piles, h) {
     
     let minSpeed = Math.max(...piles);
     let left = 1;
