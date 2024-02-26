fcc1da6d40b635fbe29cbf4be4e55dea7d7a6adc
aadil42 <adilkelawala@gmail.com>
2024-02-26 13:50:54 +0530

    solved second time

--- DSA/valid-perfect-square.js
+++ DSA/valid-perfect-square.js
@@ -1,9 +1,37 @@
 /**
- * Naive way.
+ * Binary Search
+ * Time O(log(n)) | Space O(1)
+ * https://leetcode.com/problems/valid-perfect-square/
  * @param {number} num
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
+    
+    let left = 1;
+    let right = num;
+
+    while(left <= right) {
+
+        const mid = left + Math.floor((right-left)/2);
+
+        if(mid*mid === num) return true;
+
+        if(mid*mid > num) {
+            right = mid-1;
+        } else {
+            left = mid+1;
+        }
+    }
+
+    return false;
+};
+
+/**
+ * Naive way.
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare2 = function(num) {
     if(Math.sqrt(num).toString().includes('.')) return false;
     return true;
 };
@@ -16,7 +44,7 @@ var isPerfectSquare = function(num) {
  * @param {number} num
  * @return {boolean}
  */
-var isPerfectSquare = function(num) {
+var isPerfectSquare1 = function(num) {
     let left = 1;
     let right = num;
 
