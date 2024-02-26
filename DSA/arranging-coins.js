e11eee706617359983d89305128aa7a0fe89b4e9
aadil42 <adilkelawala@gmail.com>
2024-02-26 15:58:52 +0530

    add solution for arranging-coins

--- DSA/arranging-coins.js
+++ DSA/arranging-coins.js
@@ -1,11 +1,43 @@
 /**
+ * Math | BinarySearch
+ * Time O(log(n)) | Space O(1)
+ * https://leetcode.com/problems/arranging-coins/
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    
+    let maxRows;
+    let left = 1;
+    let right = n;
+
+    const getTotal = (n) => {
+        return (1+n)*(n/2);
+    }
+
+    while(left <= right) {
+
+        const mid = left + Math.floor((right-left)/2);
+
+        if(getTotal(mid) <= n) {
+            maxRows = mid;
+            left = mid +1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return maxRows;
+};
+
+/**
  * https://leetcode.com/problems/arranging-coins/
  * Linear time
  * Time O(n) | Space O(1)
  * @param {number} n
  * @return {number}
  */
-var arrangeCoins = function(n) {
+var arrangeCoins1 = function(n) {
     
     let steps = 1;
     let canBuild = 0;
@@ -26,7 +58,7 @@ var arrangeCoins = function(n) {
  * @param {number} n
  * @return {number}
  */
-var arrangeCoins = function(n) {
+var arrangeCoins2 = function(n) {
     
     let left = 1;
     let right = n;
@@ -53,7 +85,7 @@ var arrangeCoins = function(n) {
  * @param {number} n
  * @return {number}
  */
-var arrangeCoins = function(n) {
+var arrangeCoins3 = function(n) {
     
     let result1 = Math.floor((-1 + Math.sqrt(1+(8*n)))/2);
     let result2 = Math.floor((-1 - Math.sqrt(1+(8*n)))/2);
