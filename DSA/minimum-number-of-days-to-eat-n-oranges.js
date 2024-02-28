afc8657ef5cb3aa137c81d259f7ebd4c648e5797
aadil42 <adilkelawala@gmail.com>
2024-02-28 12:30:06 +0530

    add brute force solution with time complexity of O(n) and space compexity of O(n)

--- DSA/minimum-number-of-days-to-eat-n-oranges.js
+++ DSA/minimum-number-of-days-to-eat-n-oranges.js
@@ -1,9 +1,55 @@
+/**
+ * Brute Force
+ * DP | Recursion | Memoization
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/minimum-number-of-days-to-eat-n-oranges/
+ * 
+ * @param {number} n
+ * @return {number}
+ */
+var minDays = function(n) {
+
+    const cache = new Map();
+
+    const dfs = (n) => {
+
+        if(cache.has(n)) return cache.get(n);
+
+        if(n <= 0) return 0;
+
+        if(n%2 === 0 && n%3 === 0) {
+            const result = Math.min(1 + dfs(n - (n/2)), 1 + dfs(n - (2*(n/3))), 1 + dfs(n-1)); 
+            cache.set(n, result); 
+            return result;
+        }
+
+        if(n%2 === 0) {
+            const result = Math.min(1 + dfs(n - (n/2)), 1 + dfs(n-1));
+            cache.set(n, result);
+            return result;
+        }  
+
+        if(n%3 === 0) {
+            const result =  Math.min(1+dfs(n - (2*(n/3))), 1 + dfs(n-1));
+            cache.set(n, result);
+            return result;
+        }
+
+        const result = 1 + dfs(n-1);
+        cache.set(n, result);
+        return result;
+    }
+
+
+    return dfs(n);
+};
+
 // Don't quite understand the question yet. But I do understand the code and answer. check it out again after some time.
 /**
  * @param {number} n
  * @return {number}
  */
-var minDays = function(n) {
+var minDays1 = function(n) {
     
     const hash = new Map();
     hash.set(0,0);
