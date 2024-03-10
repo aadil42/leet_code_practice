9a1eab3de1a5338fd6db82d0f14c585ec51fec8f
aadil42 <adilkelawala@gmail.com>
2024-03-10 12:41:29 +0530

    add alternate solution

--- DSA/intersection-of-two-arrays.js
+++ DSA/intersection-of-two-arrays.js
@@ -15,3 +15,32 @@ var intersection = function(nums1, nums2) {
 
     return [...result];
 };  
+
+/**
+ * Hashing
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/intersection-of-two-arrays
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection1 = function(nums1, nums2) {
+    
+    const nums1Set = new Set(nums1);
+    const nums2Set = new Set(nums2);
+
+    const ansSet = new Set();
+
+    let i = 0;
+    while(i < nums1.length) {
+        if(nums1Set.has(nums1[i]) && nums2Set.has(nums1[i])) ansSet.add(nums1[i]);
+        i++;
+    }
+    i = 0;
+    while(i < nums2.length) {
+        if(nums1Set.has(nums2[i]) && nums2Set.has(nums2[i])) ansSet.add(nums2[i]);
+        i++;
+    }
+
+    return [...ansSet];
+};
