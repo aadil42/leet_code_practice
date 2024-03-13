66c613f691531ed86457ea643142562c4ace395b
aadil42 <adilkelawala@gmail.com>
2024-03-13 15:19:24 +0530

    solved remove-zero-sum-consecutive-nodes-from-linked-list

--- DSA/remove-zero-sum-consecutive-nodes-from-linked-list.js
+++ DSA/remove-zero-sum-consecutive-nodes-from-linked-list.js
@@ -1,4 +1,48 @@
 /**
+ * Hashing | LinkedList
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeZeroSumSublists = function(head) {
+
+    const dummyNode = new ListNode(0, head);
+    const preFixHash = {
+        0: dummyNode
+    };
+    
+    let current = head;
+    let totalSoFar = 0;
+
+    while(current) {
+        totalSoFar += current.val;
+        if(totalSoFar in preFixHash) {
+            let toBeDeleted = preFixHash[totalSoFar].next;
+            let tempSum = totalSoFar + toBeDeleted.val;
+            while(toBeDeleted !== current) {
+                delete preFixHash[tempSum];
+                toBeDeleted = toBeDeleted.next;
+                tempSum += toBeDeleted.val;
+            }
+            preFixHash[totalSoFar].next = current.next;
+        } else {
+            preFixHash[totalSoFar] = current;
+        }
+        current = current.next;
+    }
+    
+    return dummyNode.next;
+};
+
+/**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
@@ -12,7 +56,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var removeZeroSumSublists = function(head) {
+var removeZeroSumSublists1 = function(head) {
         
     const listArr = [];
 
