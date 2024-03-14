f60c9e0644312310108885ed2adae51b72cdbae4
aadil42 <adilkelawala@gmail.com>
2024-03-14 14:31:48 +0530

    add alternate code for course-schedule-ii

--- DSA/course-schedule-ii.js
+++ DSA/course-schedule-ii.js
@@ -1,11 +1,78 @@
 /**
+ * DFS | ToplogicalSort
+ * Time O(n) | Space O(n^2)
+ * https://leetcode.com/problems/course-schedule-ii/   
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    
+    const createGraph = (edges) => {
+
+        const graph = {};
+
+        for(let i = 0; i < edges.length; i++) {
+            const from = edges[i][0];
+            const to = edges[i][1];
+
+            if(!graph[from]) {
+                graph[from] = [];
+            }
+
+            graph[from].push(to);
+        }
+
+        return graph;
+    }
+
+    const graph = createGraph(prerequisites);
+
+    // set converts to array in the order the values were added.
+    const topologicalOrder = new Set();
+    
+    for(let i = 0; i < numCourses.length; i++) {
+        nodes.add(numCourses[i][0]);
+        nodes.add(numCourses[i][1]);
+    }
+
+    const dfs = (node, visited) => {
+        
+        if(topologicalOrder.has(node)) return true;
+        if(visited.has(node)) return false;
+
+        visited.add(node);
+        
+        const neighbors = graph[node] || [];
+
+        for(let i = 0; i < neighbors.length; i++) {
+            const nextNode = neighbors[i];
+            if(!dfs(nextNode, visited)) return false;
+        }
+
+        if(!topologicalOrder.has(node)) {
+            topologicalOrder.add(node);
+        }
+
+        visited.delete(node);
+        return true;
+    }
+
+    for(let i = 0; i < numCourses; i++) {
+        if(!dfs(i, new Set())) return [];
+    }
+
+    return [...topologicalOrder];
+};
+
+/**
  * Topological Sort
  * Time O(n+m) | Space O(n)
  * @param {number} numCourses
  * @param {number[][]} prerequisites
  * @return {number[]}
  */
-var findOrder = function(numCourses, prerequisites) {
+var findOrder1 = function(numCourses, prerequisites) {
     
     const visited = new Set();
     const cycle = new Set();
