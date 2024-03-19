e62cf961aa6c10bf1e0cea1045d3e5307f37bc03
aadil42 <adilkelawala@gmail.com>
2024-03-19 10:12:50 +0530

    add alternative/better code with same approch

--- DSA/max-area-of-island.js
+++ DSA/max-area-of-island.js
@@ -1,9 +1,61 @@
+/**
+ * Graph | DFS
+ * Time O(n*m) | Space O(1)
+ * https://leetcode.com/problems/max-area-of-island/
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+
+    const directions = [[1,0],[-1,0],[0,1],[0,-1]];
+
+    const ROW = grid.length;
+    const COL = grid[0].length;
+
+    const isOutOfBound = (row, col) => {
+        if(row === ROW) return true;
+        if(col === COL) return true;
+        if(row < 0) return true;
+        if(col < 0) return true;
+        return false;
+    }  
+
+    const dfs = (row, col) => {
+        if(isOutOfBound(row, col)) return 0;
+        if(grid[row][col] === 0) return 0;
+        grid[row][col] = 0;
+
+        let currTotal = 1; // the cell is one.
+        for(let i = 0; i < directions.length; i++) {
+            
+            const nextRow = row + directions[i][0];
+            const nextCol = col + directions[i][1];
+
+            currTotal += dfs(nextRow, nextCol);
+        }
+
+        return currTotal;
+    }
+
+    let maxIslandArea = 0;
+
+    for(let i = 0; i < ROW; i++) {
+        for(let j = 0; j < COL; j++) {
+            if(grid[i][j]) {
+                maxIslandArea = Math.max(maxIslandArea, dfs(i,j));
+            }
+        }
+    }
+
+    return maxIslandArea;
+};
+
 /** DFS
  * Time O(n*m) || Space O(1);
  * @param {number[][]} grid
  * @return {number}
  */
-var maxAreaOfIsland = function(grid) {
+var maxAreaOfIsland1 = function(grid) {
     const row = grid.length;
     const column = grid[0].length;
     let maxArea = 0;
