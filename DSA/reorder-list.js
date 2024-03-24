f4997b4f0ad2d5a87aaa6448d4ee5300ce49f5a4
aadil42 <adilkelawala@gmail.com>
2024-03-24 10:53:02 +0530

    solved reorder-lsit

--- DSA/reorder-list.js
+++ DSA/reorder-list.js
@@ -1,8 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Time O(n) | Space O(1)
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    
+    // edge case. it is not neccecery the code will work out to be fine even without it.
+    if(!head.next) return head;
+    
+    // helper function.
+    const reverse = (head) => {
+
+        let curr = head;
+        let pre = null;
+
+        while(curr) {
+            const temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;    
+        }
+
+        return pre;
+    }
+
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    let remainingHalf = slow.next;
+    slow.next = null;
+
+    // reverse remainingHalf;
+    let remainingHalfReverse = reverse(remainingHalf);
+    let curr = head;
+    
+    while(remainingHalfReverse) {
+        const temp1 = curr;
+        const temp2 = remainingHalfReverse;
+        curr = curr.next;
+        remainingHalfReverse = remainingHalfReverse.next;
+
+        temp1.next = temp2;
+        temp2.next = curr;
+    }
+};
+
 // REMEMBER 
 // you need legit linked list for input to test this solution, which you don't have but you have checked it on leet code
 // it's workin fine. just push it on github as it is. it's legit solution and it's working.
 
-var reorderList = function(head) {
+var reorderList1 = function(head) {
   
     if(!head || !head.next) return head;
     let slowPointer = head;
@@ -43,7 +102,7 @@ function reverseLinkList(head) {
 
 // solving reorder-list second time.
 
-var reorderListR = function(head) {
+var reorderListR2 = function(head) {
 
     let slowPointer = head;
     let fastPointer = head;
