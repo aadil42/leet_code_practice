97b9b29961f9f663f51cf6d1a5eeb20ad9d93aac
aadil42 <adilkelawala@gmail.com>
2024-03-26 14:25:00 +0530

    add code with helpful comment with time complexity of O(n) and space complexity of O(1)

--- DSA/first-missing-positive.js
+++ DSA/first-missing-positive.js
@@ -1,11 +1,53 @@
 /**
  * Basically, we're going to store the numbers in the array index by converting
  * the number to negetive. negetive doesn't matter. that's why we can do it.
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    
+    // make negetive values 0 because they don't matter.
+    nums = nums.map((num) => {
+        if(num < 0) return 0;
+        return num;
+    });
+
+    // the meat of the solution
+    for(let i = 0; i < nums.length; i++) {
+        const idx = Math.abs(nums[i]);
+        
+        // mark the value to be -1 so we know it was in the array
+        if(nums[idx-1] > 0) {
+            nums[idx-1] *= -1;
+        }
+
+        // for the negetive values that were present in the array.
+        if(nums[idx-1] === 0) {
+            nums[idx-1] = -1 * (nums.length + 1);
+        }
+    }
+
+    // loop from 1 to n. if the value at i-1 is negetive that means that val existed in the array.
+    // so once you encounter the value at i-1 that is not negetive that means that value was not present in the array 
+    // so just return i at that point.
+    let i = 1;
+    while(i < nums.length + 1) {
+        if(nums[i-1] >= 0) return i;
+        i++;
+    }
+
+    // all values from 1 to n must have been in the array.
+    return i;
+};
+
+/**
+ * Basically, we're going to store the numbers in the array index by converting
+ * the number to negetive. negetive doesn't matter. that's why we can do it.
  * Time O(n) | Space O(1)
  * @param {number[]} nums
  * @return {number}
  */
-  var firstMissingPositive = function(nums) {
+  var firstMissingPositive0 = function(nums) {
     //-1 1 3 4
     const swap = (a,b) => {
         const temp = nums[a];
@@ -37,7 +79,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive0 = function(nums) {
+var firstMissingPositive1 = function(nums) {
     
     // convert every negative to 0;
     nums = nums.map((num) => {
@@ -73,7 +115,7 @@ var firstMissingPositive0 = function(nums) {
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive1 = function(nums) {
+var firstMissingPositive2 = function(nums) {
     const numberSet = new Set(nums);
     let i = 1;
     while(true) {
@@ -88,7 +130,7 @@ var firstMissingPositive1 = function(nums) {
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive2 = function(nums) {
+var firstMissingPositive3 = function(nums) {
 
     const numberSet = new Set(nums);
       
