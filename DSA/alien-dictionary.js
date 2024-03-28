6cd2a7293a00d912ab810de78c6581b2571e22e4
aadil42 <adilkelawala@gmail.com>
2024-03-28 16:23:58 +0530

    add another solution. but it's  not getting accepted on lintcode. there are some condition that are not accounted for. But I think the code  will work  on leetcode as they don't have these extra condition

--- DSA/alien-dictionary.js
+++ DSA/alien-dictionary.js
@@ -13,7 +13,7 @@ var alienOrder = function(words) {
 
        visited.set(node, true);
        const iteratebleNodes = graph[node];
-       for(iteratebleNode of iteratebleNodes) {
+       for(const iteratebleNode of iteratebleNodes) {
          if(dfs(iteratebleNode)) return true;
        }
 
@@ -58,3 +58,109 @@ function makeGraph(words) {
 
 const words = ['wrt','wrf','er','ett','rftt'];
 console.log(alienOrder(words));
+
+export class Solution {
+    /**
+     * @param words: a list of words
+     * @return: a string which is correct order
+     */
+    alienOrder(words) {
+      // Write your code here
+  
+      let edges = new Set();
+  
+      for(let i = 0; i < words.length - 1; i++) {
+          // iterete over chars
+          for(let j = 0; j < Math.min(words[i].length, words[i+1].length); j++) {
+            if(words[i][j] !== words[i+1][j]) {
+              // const edge = [words[i][j], words[i+1][j]];
+              // edges.push(edge);
+              edges.add(`${words[i][j]}-${words[i+1][j]}`);
+            }
+          }
+      }
+  
+      edges = [...edges].map((edge) => edge.split('-'));
+  
+      // const graph = {};
+  
+      // for(let i = 0; i < edges.length; i++) {
+        
+      //   const from = edges[i][0];
+      //   const to = edges[i][1];
+  
+      //   if(!graph[from]) {
+      //     graph[from] = [];
+      //   }
+  
+      //   graph[from].push(to);
+      // }
+  
+      const makeGraph = (words) => {
+        const directedGraph = {};
+  
+        words.forEach((word) => {
+            word.split('').forEach((char) => {
+                directedGraph[char] = [];
+            });
+        });
+        for(let i = 0; i < words.length - 1; i++) {
+            const word1 = words[i];
+            const word2 = words[i + 1];
+  
+          
+            const minLen = Math.min(word1.length, word2.length);
+  
+            if(word1.length > word2.length && word1.slice(0, minLen) === word2.slice(0, minLen)) return false;
+  
+            for(let j = 0; j < minLen; j++) {
+                if(word1[j] !== word2[j]) {
+                    directedGraph[word1[j]].push(word2[j]);
+                }
+            }
+        }
+  
+        return directedGraph;
+      }
+  
+      const graph = makeGraph(words);
+      console.log(graph);
+      const globleVisited = new Set();
+  
+      const dfs = (node, visited, order) => {
+        
+        // this means we have a cycle.
+        if(visited.has(node)) return false;
+        visited.add(node);
+  
+        // this means the node is already visited. But, it's not a cycle.
+        if(globleVisited.has(node)) return order;
+        globleVisited.add(node);
+  
+        const neighbors = graph[node] || [];
+  
+        for(let i = 0; i < neighbors.length; i++) {
+  
+          const nextNode = neighbors[i];
+          if(!dfs(nextNode, visited, order)) return false;
+  
+        }
+  
+        visited.delete(node);
+        order.push(node);
+        return order;
+      }
+  
+      let alianOrderedDict = [];
+  
+      for(const node in graph) {
+        const order = dfs(node, new Set(), []);
+        if(order) {
+          alianOrderedDict = [...alianOrderedDict, ...order];
+        }
+        if(!order) return "";
+      }
+  
+      return alianOrderedDict.reverse().join("");
+    }
+  }
