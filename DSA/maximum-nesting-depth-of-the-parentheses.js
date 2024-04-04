6ebcd096c3f95dbedac4a5c0aa435a33de0a1a0b
aadil42 <adilkelawala@gmail.com>
2024-04-04 11:48:13 +0530

    add alternate code with same approch for maximum-nesting-depth-of-the-parentheses

--- DSA/maximum-nesting-depth-of-the-parentheses.js
+++ DSA/maximum-nesting-depth-of-the-parentheses.js
@@ -1,4 +1,22 @@
+/**
+ * Stack | Since you just need the length of the stack you can use a variable instead of the stack DS.
+ * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+ * @param {string} s
+ * @return {number}
+ */
 var maxDepth = function(s) {
+    let currDepth = 0;
+    let maxDepth = 0;
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === "(") currDepth++;
+        maxDepth = Math.max(currDepth, maxDepth);
+        if(s[i] === ")")  currDepth--;
+    }
+    return maxDepth;
+};
+
+var maxDepth1 = function(s) {
     let currunt = 0;
     let max = 0;
     
