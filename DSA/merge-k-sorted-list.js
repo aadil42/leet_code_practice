f58eb1814d4b48a7b6a85a8e52d43ecd86f1f5ae
aadil42 <adilkelawala@gmail.com>
2024-04-08 12:54:31 +0530

    solved merge-k-sorted-lists again

--- DSA/merge-k-sorted-list.js
+++ DSA/merge-k-sorted-list.js
@@ -1,4 +1,51 @@
-var mergeTwoLists = function(list1, list2) {
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * LinkedList
+ * Time O(n^2) | Space O(1)
+ * https://leetcode.com/problems/merge-k-sorted-lists/
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    
+    if(lists.length === 0) return null;  
+    if(lists.length === 1) return lists[0];
+
+    const merge2Lists = (h1, h2) => {
+        const dummyNode = new ListNode();
+        let curr = dummyNode;
+        while(h1 && h2) {
+            if(h1.val < h2.val) {
+                curr.next = h1;
+                h1 = h1.next;
+            } else {
+                curr.next = h2;
+                h2 = h2.next;
+            }
+            curr = curr.next;
+        }
+        
+        if(h1) curr.next = h1;
+        if(h2) curr.next = h2;
+
+        while(curr.next) curr = curr.next;
+        return dummyNode.next;
+    }
+
+    let sorted = null;
+    for(let i = 0; i < lists.length ; i++) {
+        sorted = merge2Lists(sorted, lists[i]);
+    }
+    return sorted;
+};
+
+var mergeTwoLists0 = function(list1, list2) {
     let head = tail = new ListNode(0);
 
     while (list1 && list2) {
