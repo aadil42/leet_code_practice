1635b8b8ee0413851d00c6305c63e59b9af221a7
aadil42 <adilkelawala@gmail.com>
2024-04-10 10:13:49 +0530

    add simpler/concise code. corrected  space and time complexity

--- DSA/split-linked-list-in-parts.js
+++ DSA/split-linked-list-in-parts.js
@@ -5,16 +5,115 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-
 /**
- * Linked List Traversal
- * Time O(n) | Space O(n)
- * https://leetcode.com/problems/split-linked-list-in-parts
+ * LinkedList
+ * Time O(n*(n/k)) | Space O(k)
+ * https://leetcode.com/problems/split-linked-list-in-parts/
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode[]}
  */
 var splitListToParts = function(head, k) {
+    // helper methods
+
+    const getLLlength = (head) => {
+        let len = 0;
+        while(head) {
+            head = head.next;
+            len++;
+        }
+        return len;
+    }
+    
+    const splitArr = (head, k) => {
+
+        const llArr = [];
+
+        while(k) {
+            if(!head) {
+                llArr.push(null);
+                k--;
+                continue;
+            }
+            llArr.push(head);
+            const temp = head.next;
+            head.next = null;
+            head = temp;
+            k--;
+        }
+
+        return llArr;
+    }
+
+    const splitLL = (head, portion) => {
+        const dummyNode = head;
+
+        while(portion > 1) {
+            head = head.next;
+            portion--;
+        }
+
+        const nextHead = head.next;
+        head.next = null;
+        return [dummyNode, nextHead];
+    }
+
+    const len = getLLlength(head);
+
+    // edge case.
+    if(len < k) {
+        return splitArr(head, k);
+    }
+
+    let remainder = len%k;
+    const equalPartsLen = Math.floor(len/k);
+    const result = [];
+    let counter = 1;
+
+    while(k) {
+
+        if(remainder && counter%equalPartsLen === 0) {
+            const [firstNode, nextHead] = splitLL(head, equalPartsLen + 1);
+            result.push(firstNode);
+            remainder--;
+            k--;
+            head = nextHead;
+            counter++;
+            continue;
+        }
+
+        if(counter%equalPartsLen === 0) {
+            const [firstNode, nextHead] = splitLL(head, equalPartsLen);
+            result.push(firstNode);
+            k--;
+            head = nextHead;
+            counter++;
+            continue;
+        }
+
+        counter++;
+    }
+
+    return result;
+};
+/////////////////////////////////////////////////////////////////////////
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * Linked List Traversal
+ * Time O(n*(n/k)) | Space O(k)
+ * https://leetcode.com/problems/split-linked-list-in-parts
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode[]}
+ */
+var splitListToParts0 = function(head, k) {
     
     const llLen = (head) => {
         let current = head;
