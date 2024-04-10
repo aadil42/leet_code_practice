6c77be0c6447ea0614e6fd33ad1416bfb10844f4
aadil42 <adilkelawala@gmail.com>
2024-04-10 12:17:48 +0530

    add more simpler and readable code.

--- DSA/insertion-sort-list.js
+++ DSA/insertion-sort-list.js
@@ -1,4 +1,48 @@
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Time O(n^2) | Space O(1)
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function(head) {
+    
+    const dummyNode = new ListNode(-Infinity, head);
+    head = dummyNode;
+
+    const insert = (node) => {
+        let head = dummyNode;
+
+        while(head.next.val < node.val) {
+            head = head.next;
+        }
+
+        const temp = head.next;
+        head.next = node;
+        node.next = temp;
+    }
+
+    while(head) {
+
+        if(head.next && (head.next.val < head.val)) {
+            temp = head.next.next;
+            insert(head.next);
+            head.next = temp;
+            continue;
+        }
+
+        head = head.next;
+    }
+
+    return dummyNode.next;
+};
+
+/**
  * Time O(n^2) | Space O(1)
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -10,7 +54,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var insertionSortList = function(head) {
+var insertionSortList0 = function(head) {
     
     let current = head;
     let dummy = new ListNode();
