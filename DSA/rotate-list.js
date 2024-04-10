17feab7a35340688cec0d1357ba144e07b8eb873
aadil42 <adilkelawala@gmail.com>
2024-04-10 20:47:36 +0530

    add simpler/more-readable code. the time and space complexity is the same

--- DSA/rotate-list.js
+++ DSA/rotate-list.js
@@ -1,8 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * LinkedList
+ * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/rotate-list/
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    
+    // edge cases.
+    if(!head) return null;
+    if(!head.next) return head;
+
+
+    const getLength = (head) =>  {
+        let i = 0;
+        while(head) {
+            head = head.next;
+            i++;
+        }
+        return i;
+    }
+
+    const len = getLength(head);
+    k = k%len;
+    const targetLen = len - k;
+    let idx = 1;
+    let remainingHead = null;
+
+    if(targetLen === len) return head;
+
+    let curr = head;
+    while(curr) {
+        if(idx === targetLen) {
+            remainingHead = curr.next;
+            curr.next = null;
+        }
+        curr = curr.next;
+        idx++;
+    }
+
+    const newHead = remainingHead;
+    while(remainingHead.next) {
+        remainingHead = remainingHead.next;
+    }
+
+    remainingHead.next = head;
+
+    return newHead;
+};
+
+
 // REMEMBER 
 // you need legit linked list for input to test this solution, which you don't have but you have checked it on leet code
 // it's workin fine. just push it on github as it is. it's legit solution and it's working.
 
-var rotateRight = function(head, k) {
+var rotateRight0 = function(head, k) {
     
     if(!head) return null;   
 
