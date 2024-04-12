1903c97711718ff37a5fb7803b2be9d27150b183
aadil42 <adilkelawala@gmail.com>
2024-04-12 10:38:15 +0530

    solved partition-list again

--- DSA/partition-list.js
+++ DSA/partition-list.js
@@ -1,6 +1,48 @@
+
 // REMEMBER 
 // you need legit linked list for input to test this solution, which you don't have but you have checked it on leet code
 // it's workin fine. just push it on github as it is. it's legit solution and it's working.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * LinkedList
+ * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/partition-list/
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    
+    const dummyNode = new ListNode(null, head);
+
+    let partitionLocation = null;
+    let curr = dummyNode;
+
+    while(curr && curr.next) {
+        if(curr.next.val >= x && !partitionLocation) partitionLocation = curr;
+
+        while(curr.next && curr.next.val < x && partitionLocation) {
+            const temp = partitionLocation.next;
+            const temp1 = curr.next.next;
+
+            partitionLocation.next = curr.next;
+            curr.next.next = temp;
+            partitionLocation = partitionLocation.next;
+            curr.next = temp1;
+        }
+
+        curr = curr.next;
+    }
+
+    return dummyNode.next;
+};
 
 /**
  * Definition for singly-linked list.
@@ -20,7 +62,7 @@
  */
 
 
-var partition = function(head, x) {
+var partition0 = function(head, x) {
 
     if(!head) return null;
        
