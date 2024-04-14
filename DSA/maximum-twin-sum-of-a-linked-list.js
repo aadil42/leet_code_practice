f20f32ee7f95a4729c66cb9e86d16bbd07df8d25
aadil42 <adilkelawala@gmail.com>
2024-04-14 18:49:42 +0530

    solved maximum-twin-sum-of-a-linked-list again

--- DSA/maximum-twin-sum-of-a-linked-list.js
+++ DSA/maximum-twin-sum-of-a-linked-list.js
@@ -1,4 +1,65 @@
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * LinkedList
+ * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+    
+    const getLen = (head) => {
+        let i = 0;
+        while(head) {
+            head = head.next;
+            i++;
+        }
+        return i;
+    }
+
+    const reverseLL = (head) => {
+        let pre = null;
+        while(head) {
+            const temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+    
+    const dummyNode = new ListNode(null, head);
+    const len = getLen(head);
+    let half = len/2;
+    
+    let curr = dummyNode;
+    while(half) {
+        curr = curr.next;
+        half--;
+    }
+
+    let head2 = curr.next;
+    head2 = reverseLL(head2);
+
+    curr.next = null; // break the link
+
+    let max = 0;
+    while(head) {
+        max = Math.max(head.val + head2.val, max);
+        head = head.next;
+        head2 = head2.next;
+    }
+
+    return max;
+};
+/////////////////////////////////////////////////////
+/**
  * Linear Time 
  * 
  * Time O(n) | Space O(1)
@@ -12,7 +73,7 @@
  * @param {ListNode} head
  * @return {number}
  */
-var pairSum = function(head) {
+var pairSum0 = function(head) {
     
     //helper functions
     const llLength = (head) => {
