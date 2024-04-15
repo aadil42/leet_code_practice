ab255b077d932972cb13ccd89a0ecb1352ef5ab2
aadil42 <adilkelawala@gmail.com>
2024-04-15 18:37:46 +0530

    solved sum-root-to-leaf-numbers

--- DSA/sum-root-to-leaf-numbers.js
+++ DSA/sum-root-to-leaf-numbers.js
@@ -7,13 +7,46 @@
  * }
  */
 /**
+ * Tree | pre-order-traversal
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    
+  let total = 0;
+  const dfs = (node, num) => {
+      if(!node.left && !node.right) {
+          num = num+node.val;
+          total += +num;
+          return;
+      }
+
+      node.left && dfs(node.left, num+node.val);
+      node.right && dfs(node.right, num+node.val);
+  }
+
+  dfs(root,"");
+  return total;
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * 
  * Pre Order Traversal
  * Time O(n) | Space O(n)
  * @param {TreeNode} root
  * @return {number}
  */
-var sumNumbers = function(root) {
+var sumNumbers0 = function(root) {
 
 
     const digits = [];
