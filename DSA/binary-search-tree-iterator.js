bfc9bdc8ac058906d8c76f50e75a0edefc07242d
aadil42 <adilkelawala@gmail.com>
2024-04-18 18:28:48 +0530

    add solution that runs in avarage O(n) time and space.

--- DSA/binary-search-tree-iterator.js
+++ DSA/binary-search-tree-iterator.js
@@ -7,12 +7,68 @@
  * }
  */
 /**
+ * Tree | in-orderTraversal | Stack.
+ * Time O(1) | Space O(h) (h is height of the tree) (this is the avarage time/space complexity)
+ * https://leetcode.com/problems/binary-search-tree-iterator/
+ * @param {TreeNode} root
+ */
+var BSTIterator = function(root) {
+    this.orderStack = [];
+    this.inOrderTraverse(root);
+};
+
+
+/**
+ * @return {null}
+ */
+BSTIterator.prototype.inOrderTraverse = function(root) {
+    const dfs = (node) => {
+        if(!node) return;
+        this.orderStack.push(node);
+        dfs(node.left);
+    }
+    dfs(root);
+}
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+    const nextNode = this.orderStack.pop();
+    this.inOrderTraverse(nextNode.right);
+    return nextNode.val;
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+    return this.orderStack.length > 0;
+};
+
+/** 
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
+/////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * Tree | InOrder-traversal 
  * Time O(1) | Space O(n)
  * https://leetcode.com/problems/binary-search-tree-iterator/
  * @param {TreeNode} root
  */
-var BSTIterator = function(root) {
+var BSTIterator0 = function(root) {
     
     this.inOrderArr = [];
 
@@ -32,14 +88,14 @@ var BSTIterator = function(root) {
 /**
  * @return {number}
  */
-BSTIterator.prototype.next = function() {
+BSTIterator0.prototype.next = function() {
     return this.inOrderArr.pop();
 };
 
 /**
  * @return {boolean}
  */
-BSTIterator.prototype.hasNext = function() {
+BSTIterator0.prototype.hasNext = function() {
     if(this.inOrderArr.length) return true;
     return false;
 };
@@ -64,12 +120,12 @@ BSTIterator.prototype.hasNext = function() {
 /**
  * @param {TreeNode} root
  */
-var BSTIterator0 = function(root) {
+var BSTIterator1 = function(root) {
     this.stack = [];
     this.addToStack(root);
 };
 
-BSTIterator0.prototype.addToStack = function(root) {
+BSTIterator1.prototype.addToStack = function(root) {
 
     let currentRoot = root;
     while(currentRoot) {
@@ -81,7 +137,7 @@ BSTIterator0.prototype.addToStack = function(root) {
 /**
  * @return {number}
  */
-BSTIterator0.prototype.next = function() {
+BSTIterator1.prototype.next = function() {
     // console.log(this.stack,  this.stack[this.stack.length - 1]);
     
     const next = this.stack.pop();
@@ -95,7 +151,7 @@ BSTIterator0.prototype.next = function() {
 /**
  * @return {boolean}
  */
-BSTIterator0.prototype.hasNext = function() {
+BSTIterator1.prototype.hasNext = function() {
     return this.stack.length ? true : false;
 };
 
