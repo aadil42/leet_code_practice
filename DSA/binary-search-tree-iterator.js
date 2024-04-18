ac5205260c9eaf0ea882dcf34e45bf3062cddb34
aadil42 <adilkelawala@gmail.com>
2024-04-18 17:59:36 +0530

    solved binary-search-tree-iterator

--- DSA/binary-search-tree-iterator.js
+++ DSA/binary-search-tree-iterator.js
@@ -1,4 +1,57 @@
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Tree | InOrder-traversal 
+ * Time O(1) | Space O(n)
+ * https://leetcode.com/problems/binary-search-tree-iterator/
+ * @param {TreeNode} root
+ */
+var BSTIterator = function(root) {
+    
+    this.inOrderArr = [];
+
+    const inOrderTraverse = (node) => {
+        if(!node) return;
+
+        inOrderTraverse(node.left);
+        this.inOrderArr.push(node.val);
+        inOrderTraverse(node.right);
+    }
+    
+    inOrderTraverse(root);
+
+    this.inOrderArr.reverse();
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+    return this.inOrderArr.pop();
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+    if(this.inOrderArr.length) return true;
+    return false;
+};
+
+/** 
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
+////////////////////////////////////////////////////////////////////////
+/**
  * Iterator traversal.
  * Time O(1) | Space O(h). h = height of the tree.
  * Definition for a binary tree node.
@@ -11,12 +64,12 @@
 /**
  * @param {TreeNode} root
  */
-var BSTIterator = function(root) {
+var BSTIterator0 = function(root) {
     this.stack = [];
     this.addToStack(root);
 };
 
-BSTIterator.prototype.addToStack = function(root) {
+BSTIterator0.prototype.addToStack = function(root) {
 
     let currentRoot = root;
     while(currentRoot) {
@@ -28,7 +81,7 @@ BSTIterator.prototype.addToStack = function(root) {
 /**
  * @return {number}
  */
-BSTIterator.prototype.next = function() {
+BSTIterator0.prototype.next = function() {
     // console.log(this.stack,  this.stack[this.stack.length - 1]);
     
     const next = this.stack.pop();
@@ -42,7 +95,7 @@ BSTIterator.prototype.next = function() {
 /**
  * @return {boolean}
  */
-BSTIterator.prototype.hasNext = function() {
+BSTIterator0.prototype.hasNext = function() {
     return this.stack.length ? true : false;
 };
 
