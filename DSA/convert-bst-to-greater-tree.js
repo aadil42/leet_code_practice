90da2bc0a0bab3939b2a1e68c6dcc63974e04e71
aadil42 <adilkelawala@gmail.com>
2024-04-18 17:40:25 +0530

    solved convert-bst-to-greater-tree

--- DSA/convert-bst-to-greater-tree.js
+++ DSA/convert-bst-to-greater-tree.js
@@ -1,4 +1,33 @@
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Tree | reverse pre-order-traversal 
+ * Time O(n) | Space O(n)
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function(root) {
+
+    const dfs = (node, max) =>  {
+        if(!node) return max;
+
+        const result = dfs(node.right, max);
+        node.val = result + node.val;
+        const result1 = dfs(node.left, node.val);
+        return result1;
+    }
+
+    dfs(root, 0);
+    return root;
+};
+
+/**
  * ReversePreOrder Traversal
  * Time O(n) | Space O(h). h = height of the tree.
  * Definition for a binary tree node.
@@ -12,7 +41,7 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var convertBST = function(root) {
+var convertBST0 = function(root) {
     
     function reversePreOrder(root, currentSum) {
         if(!root) return currentSum;
