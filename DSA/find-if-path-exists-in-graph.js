48f278f8da1d3f97584bb04c85c1c1c4e98948e5
aadil42 <adilkelawala@gmail.com>
2024-04-22 01:23:22 +0530

    solved find-if-path-exists-in-graph again. add time/space complexity

--- DSA/find-if-path-exists-in-graph.js
+++ DSA/find-if-path-exists-in-graph.js
@@ -1,3 +1,59 @@
+/**
+ * Graph | DFS
+ * Time O(n) | Space O(n^2)
+ * https://leetcode.com/problems/find-if-path-exists-in-graph
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+
+    const makeGraph = (edges) => {
+
+        const graph = {};
+        for(let i = 0; i < edges.length; i++) {
+            const from = edges[i][0];
+            const to = edges[i][1];
+
+            if(!graph[from]) {
+                graph[from] = [];
+            }
+            if(!graph[to]) {
+                graph[to] = [];
+            }
+
+            graph[from].push(to);
+            graph[to].push(from);
+        }
+
+        return graph;
+    }
+
+
+    const graph = makeGraph(edges);
+    const visited = new Set();
+    
+    const dfs = (node) => {
+        if(visited.has(node)) return false;
+
+        if(node === destination) return true;
+
+        visited.add(node);
+        const neighbors = graph[node];
+
+        for(let i = 0; i < neighbors.length; i++) {
+            const nextNode = neighbors[i];
+            if(dfs(nextNode)) return true;
+        }
+
+        return false;
+    }
+
+    return dfs(source);
+};
+
 function makeGraph(edges) {
     const graph = new Map();
 
@@ -48,7 +104,7 @@ function dfs(graph, source, destination) {
     return false;
 }
 
-var validPath = function(n, edges, source, destination) {
+var validPath0 = function(n, edges, source, destination) {
         
    if(source == destination) return true;
    const graph = makeGraph(edges); 
