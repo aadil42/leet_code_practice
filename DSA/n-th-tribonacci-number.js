bed92c1f2555cfb0c1c6fdf831aa824096c02a8a
aadil42 <adilkelawala@gmail.com>
2024-04-25 02:14:38 +0530

    solved n-th-tribonacci-number

--- DSA/n-th-tribonacci-number.js
+++ DSA/n-th-tribonacci-number.js
@@ -1,12 +1,36 @@
 /**
  * Recursion | DP
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/n-th-tribonacci-number
+ * 
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+
+  const hash = {};
+
+  const dfs = (n) => {
+      if(hash[n]) return hash[n];
+      if(n === 0) return 0;
+      if(n === 1 || n === 2) return 1;
+
+      hash[n] = dfs(n-1) + dfs(n-2) + dfs(n-3);
+      return hash[n];
+  }
+
+  return dfs(n);
+};
+
+/**
+ * Recursion | DP
  * https://leetcode.com/problems/n-th-tribonacci-number
  * 
  * Time O(n) | Space O(n)
  * @param {number} n
  * @return {number}
  */
-var tribonacci = function(n) {
+var tribonacci0 = function(n) {
 
     const cache = new Map();
     if(n === 1 || n === 2) return 1;
