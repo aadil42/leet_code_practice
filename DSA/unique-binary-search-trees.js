a39da6194714eae1629bf440ffc03882e4052646
aadil42 <adilkelawala@gmail.com>
2024-04-26 11:45:35 +0530

    solved unique-binary-search-trees

--- DSA/unique-binary-search-trees.js
+++ DSA/unique-binary-search-trees.js
@@ -1,10 +1,36 @@
 /**
+ * DFS | DP | Recursion | Tree
+ * Time O(n^2) | Space O(n) | n^2 because of the inner loop which runs from 0 to n on each call.
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+    
+    const cache = {};
+
+    const dfs = (n) => {
+        if(n <= 1) return 1;
+        if(cache[n]) return cache[n];
+
+        let total = 0;
+        for(let i = 0; i < n; i++) {
+            total += dfs(i) * dfs(n-1-i);
+        }
+
+        cache[n] = total;
+        return total;
+    }
+
+    return dfs(n);
+};
+
+/**
  * recursion
  * Time O(n^2) | Space O(n)
  * @param {number} n
  * @return {number}
  */
-var numTrees = function(n) {
+var numTrees0 = function(n) {
     
 
     function countRecursion(n,map) {
