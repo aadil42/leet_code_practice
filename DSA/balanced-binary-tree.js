18c82df28bdca2817fbcf33cbfa0444e96375aba
aadil42 <adilkelawala@gmail.com>
2024-05-11 22:29:31 +0530

    solved balanced-binary-tree

--- DSA/balanced-binary-tree.js
+++ DSA/balanced-binary-tree.js
@@ -1,4 +1,38 @@
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * DFS | Tree-traversal
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    
+    let b = true;
+    const dfs = (node) => {
+        if(!node) return 0;
+
+        const h1 = 1 + dfs(node.left);
+        const h2 = 1 + dfs(node.right);
+        if(Math.abs(h1 - h2) > 1) {
+            b = false;
+        }
+        
+        return Math.max(h1, h2);
+    }
+
+    dfs(root);
+    return b;
+};
+
+/**
  * postOrder Traversal
  * Time O(n) | Space O(h). h = height of the tree.
  * Definition for a binary tree node.
@@ -12,7 +46,7 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function(root) {
+var isBalanced0 = function(root) {
     
     let balanced = true;
 
