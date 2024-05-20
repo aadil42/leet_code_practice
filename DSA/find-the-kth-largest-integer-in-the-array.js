c0c9adcaf9fbe4786f017ccda52df975c4f90b39
aadil42 <adilkelawala@gmail.com>
2024-05-20 13:47:57 +0530

    solved find-the-kth-largest-integer-in-the-array second time with slightly different approch

--- DSA/find-the-kth-largest-integer-in-the-array.js
+++ DSA/find-the-kth-largest-integer-in-the-array.js
@@ -1,4 +1,18 @@
 /**
+ * @param {string[]} nums
+ * @param {number} k
+ * @return {string}
+ */
+var kthLargestNumber = function(nums, k) {
+    // sort it string wise.
+    nums.sort((a,b) => {
+        if(a.length !== b.length) return b.length - a.length;
+        return b.localeCompare(a);
+    });
+    return nums[k-1];
+};
+
+/**
  * 
  * Sort
  * 
@@ -7,7 +21,7 @@
  * @param {number} k
  * @return {string}
  */
-var kthLargestNumber = function(nums, k) { 
+var kthLargestNumber0 = function(nums, k) { 
     // just convert the string with BigInt.
 return nums.map((x) => BigInt(x)).sort((a, b) => (a < b ? 1 : -1))[k - 1].toString();
 };
