963deaeee3c8d5ab35ef8317473c305f95f68029
aadil42 <adilkelawala@gmail.com>
2024-05-21 10:42:21 +0530

    solved subsets

--- DSA/subsets.js
+++ DSA/subsets.js
@@ -1,10 +1,34 @@
 /**
+ * Backtracking | recursion | DFS
+ * Time O(2^n * n) | Space O(2^n * n)
+ * https://leetcode.com/problems/subsets
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    
+    const allSubs = [[]];
+
+    const dfs = (i, arr) => {
+        if(i === nums.length) return;
+        arr.push(nums[i]);
+        allSubs.push(arr.slice());
+        dfs(i+1, arr);
+        arr.pop();
+        dfs(i+1, arr);
+    }
+
+    dfs(0, []);
+    return allSubs;
+};
+
+/**
  * Recursion
  * Time O(n*2^n) | Space O(2^n)
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsets = function(nums) {
+var subsets0 = function(nums) {
     
     const result = [];
     function dfs(i, numArr) {
