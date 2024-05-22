86778924548ac5b568455d6098b1cf87565288b2
aadil42 <adilkelawala@gmail.com>
2024-05-22 09:00:14 +0530

    solved palindrome-partitioning. confused on it's time-space-complexity

--- DSA/palindrome-partitioning.js
+++ DSA/palindrome-partitioning.js
@@ -1,9 +1,44 @@
 /**
- * 
+ * Time O(n^n) | Space O(2^n) | (I was confused between n^n and 2^n for time-complexity. But I think it's n^n)
+ * Recursion | Backtracking
+ * https://leetcode.com/problems/palindrome-partitioning
  * @param {string} s
  * @return {string[][]}
  */
 var partition = function(s) {
+
+    const isPalindrome = (start, end) =>  {
+        while(start < end) {
+            if(s[start] !== s[end]) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }   
+    
+    const allSubs = [];
+    const dfs = (start, palindromes) => {
+        if(start === s.length) allSubs.push(palindromes.slice(0));
+
+        for(let i = start; i < s.length; i++) {
+            if(isPalindrome(start, i)) {
+                palindromes.push(s.slice(start, i+1));
+                dfs(i+1, palindromes);
+                palindromes.pop();
+            }
+        }
+    }
+    
+    dfs(0,[]);
+    return allSubs;
+};
+
+/**
+ * 
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition0 = function(s) {
     
     const palindromeSubStrings = [];
     const singlePalindrome = [];
