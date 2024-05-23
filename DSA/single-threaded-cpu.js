3b355059003e6fe3da72ccbe943892dafcdac19a
aadil42 <adilkelawala@gmail.com>
2024-05-23 14:18:42 +0530

    solved single-threaded-cpu second time

--- DSA/single-threaded-cpu.js
+++ DSA/single-threaded-cpu.js
@@ -1,4 +1,43 @@
 /**
+ * minQueue 
+ * Time O(n*log(n)) | Space O(n)
+ * https://leetcode.com/problems/single-threaded-cpu/
+ * @param {number[][]} tasks
+ * @return {number[]}
+ */
+var getOrder = function(tasks) {
+    
+  const minQ = new MinPriorityQueue({
+      compare: (a,b) => {
+          if(a[1] === b[1]) return a[2] - b[2];
+          return a[1] - b[1];
+      }
+  });
+
+  tasks = tasks.map((task, idx) => [...task, idx]);
+
+  let i = 0;
+  let t = tasks[0][0];
+  const order = [];
+
+  while(i < tasks.length || !minQ.isEmpty()) {
+      while(i < tasks.length && tasks[i][0] <= t) {
+          minQ.enqueue(tasks[i]);
+          i++;
+      }
+      if(!minQ.isEmpty()) {
+          const task = minQ.dequeue();
+          order.push(task[2]);
+          t += task[1];
+          continue;
+      }
+      t = tasks[i][0];
+  }
+
+  return order;
+};
+
+/**
  * 
  * MinHeap 
  * 
@@ -6,7 +45,7 @@
  * @param {number[][]} tasks
  * @return {number[]}
  */
-var getOrder = function(tasks) {
+var getOrder0 = function(tasks) {
     for(let i = 0; i < tasks.length; i++) tasks[i].push(i);
     tasks.sort((a, b) => a[0] - b[0]);
     const pq = new MinPriorityQueue({
