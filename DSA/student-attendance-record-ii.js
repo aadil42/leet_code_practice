86ff1268d0ecca5ae10877ae621f2aeceb3b25a4
aadil42 <adilkelawala@gmail.com>
2024-05-26 16:20:59 +0530

    solved student-attendance-record-ii with O(n) time and space complexity

--- DSA/student-attendance-record-ii.js
+++ DSA/student-attendance-record-ii.js
@@ -5,7 +5,7 @@
  * @param {number} n
  * @return {number}
  */
-var checkRecord = function(n) {
+var checkRecord0 = function(n) {
     
     const mod = 10**9 + 7;
 
@@ -41,3 +41,40 @@ var checkRecord = function(n) {
     dfs(0, [], 0);
     return total;
 };
+
+
+// optimized O(n) solution. But for some stupid reason it doesn't get submitted.
+/**
+ * DP | Recursion | Memoization
+ * Time O(n*2*3) | Space O(n*2*3)
+ * https://leetcode.com/problems/student-attendance-record-ii
+ * @param {number} n
+ * @return {number}
+ */
+var checkRecord = function(n) {
+    
+    const mod = 10**9 + 7;
+
+    const cache = new Map();
+
+    const dfs = (i, lateCount, absCount) => {
+
+        const hash = `${i}-${lateCount}-${absCount}`;
+        if(cache.has(hash)) return cache.get(hash);
+
+        if(lateCount > 2) return 0;
+        if(absCount > 1) return 0;
+
+        if(i === n) return 1;
+
+        const A = dfs(i+1, 0, absCount+1);
+        const L = dfs(i+1, lateCount+1, absCount);
+        const P = dfs(i+1, 0, absCount);
+
+        cache.set(hash, (A + L + P) % mod);
+        return (A + L + P) % mod;
+    }
+
+    return dfs(0, 0, 0) % mod;
+};
+
