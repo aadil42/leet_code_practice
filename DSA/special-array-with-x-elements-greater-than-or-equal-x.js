a5993ef5b074ad63bda0ef71d7f90dc7521ef538
aadil42 <adilkelawala@gmail.com>
2024-05-27 09:09:25 +0530

    solved it with sorting with O(n*log(n))

--- DSA/special-array-with-x-elements-greater-than-or-equal-x.js
+++ DSA/special-array-with-x-elements-greater-than-or-equal-x.js
@@ -1,11 +1,34 @@
 /**
+ * Sorting 
+ * Time O(n*log(n)) | Space O(1)
+ * https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var specialArray = function(nums) {
+    nums.sort((a,b) => a-b);
+    let idx = 0;
+    for(let i = 0; i < 1001; i++) {
+        while(idx < nums.length && nums[idx] < i) {
+            idx++;
+        }
+        if(idx < nums.length && 
+           nums[idx] >= i && 
+           i === nums.length - idx) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+/**
  * BruteForce 
  * Time O(n^2) | Space O(1)
  * https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x
  * @param {number[]} nums
  * @return {number}
  */
-var specialArray = function(nums) {
+var specialArray0 = function(nums) {
     
     const checkValidity = (num) => {
         let count = 0;
