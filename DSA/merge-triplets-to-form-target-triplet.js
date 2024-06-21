e14bfcee1de12eb762b33a8e90e25bd654d73e64
aadil42 <adilkelawala@gmail.com>
2024-06-21 09:08:50 +0530

    add alternate solution with same approch

--- DSA/merge-triplets-to-form-target-triplet.js
+++ DSA/merge-triplets-to-form-target-triplet.js
@@ -1,4 +1,35 @@
 /**
+ * Greedy | Array
+ * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/merge-triplets-to-form-target-triplet/
+ * 
+ * @param {number[][]} triplets
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var mergeTriplets = function(triplets, target) {
+    
+    const goodSet = new Set();
+
+    const notValid = (arr) => {
+        if(arr[0] > target[0]) return true;
+        if(arr[1] > target[1]) return true;
+        if(arr[2] > target[2]) return true;
+        return false;
+    }
+
+    for(let i = 0; i < triplets.length; i++) {
+        if(notValid(triplets[i])) continue;
+        
+        for(let j = 0; j < 3; j++) {
+            if(triplets[i][j] === target[j]) goodSet.add(j);
+        }
+    }
+
+    return goodSet.size === 3;
+};
+
+/**
  * 
  * Greedy 
  * Time O(n) | Space O(1);
@@ -7,12 +38,31 @@
  * @return {boolean}
  */
 
+// optimal approch
+var mergeTriplets0 = function(triplets, target) {
+    
+    const goodSet = new Set();
+
+    for(let i = 0; i < triplets.length; i++) {
+        if(triplets[i][0] > target[0] ||
+           triplets[i][1] > target[1] ||
+           triplets[i][2] > target[2] 
+        ) continue;
+
+        for(let j = 0; j < 3; j++) {
+            console.log(triplets[i][j], target[j]);
+            if(triplets[i][j] === target[j])  goodSet.add(j);
+        }
+    }
+    return goodSet.size === 3;
+};
+
 // brute force for some reason it's not working for this parameter
 // triplets =
 // [[2,5,3],[2,3,4],[1,2,5],[5,2,3]]
 // target =
 // [5,5,5]
-var mergeTriplets = function(triplets, target) {
+var mergeTriplets1 = function(triplets, target) {
     
 
     for(let i = 0; i < triplets.length; i++) {
@@ -36,22 +86,3 @@ var mergeTriplets = function(triplets, target) {
     return false;
 };
 
-
-// optimal approch
-var mergeTriplets = function(triplets, target) {
-    
-    const goodSet = new Set();
-
-    for(let i = 0; i < triplets.length; i++) {
-        if(triplets[i][0] > target[0] ||
-           triplets[i][1] > target[1] ||
-           triplets[i][2] > target[2] 
-        ) continue;
-
-        for(let j = 0; j < 3; j++) {
-            console.log(triplets[i][j], target[j]);
-            if(triplets[i][j] === target[j])  goodSet.add(j);
-        }
-    }
-    return goodSet.size === 3;
-};
