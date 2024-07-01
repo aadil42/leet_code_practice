fdc426eb51d0e122adf408a9f1ad5a871d3d2880
aadil42 <adilkelawala@gmail.com>
2024-07-01 11:58:06 +0530

    solved it with dp

--- DSA/jump-game-ii.js
+++ DSA/jump-game-ii.js
@@ -1,10 +1,40 @@
 /**
+ * DP | DFS
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/jump-game-ii/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    
+    const cache = {};
+
+    const dfs = (idx) => {
+        if(cache[idx]) return cache[idx];
+        if(idx >= nums.length) return Infinity;
+        if(idx === nums.length - 1) return 0;
+
+        let min = Infinity;
+        for(let i = idx+1; i <= idx+nums[idx]; i++) {
+            min = Math.min(min, 1+dfs(i));
+        }
+
+        cache[idx] = min;
+        return min;
+    }
+
+    return dfs(0);
+};
+
+/**
+ * Two Pointers
  * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/jump-game-ii/
  * @param {number[]} nums
  * @return {number}
  */
 
-var jump = function(nums) {
+var jump0 = function(nums) {
 
     let left = 0;
     let right = 0;
