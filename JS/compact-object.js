1999b326869bcd469a2f7fda8e8fdf80955f789a
aadil42 <adilkelawala@gmail.com>
2024-07-07 20:38:51 +0530

    added leaner code

--- JS/compact-object.js
+++ JS/compact-object.js
@@ -1,10 +1,45 @@
 /**
  * Recursion | DFS
- * https://leetcode.com/problems/compact-object/
  * @param {Object|Array} obj
  * @return {Object|Array}
  */
 var compactObject = function(obj) {
+    
+
+    const dfs = (objArr) => {
+
+        if(Array.isArray(objArr)) {
+            for(let i = objArr.length - 1; i > -1; i--) {
+                if(!objArr[i]) {
+                    objArr.splice(i,1);
+                    continue;
+                }
+                if(typeof objArr[i] === "object") dfs(objArr[i]);
+            }
+            return objArr;
+        }
+
+        for(const key in objArr) {
+            if(!objArr[key]) {
+                delete objArr[key];
+                continue;
+            }
+            if(typeof objArr[key] === "object") dfs(objArr[key]);
+        }
+        return objArr;
+
+    }
+
+    return dfs(obj);
+};
+
+/**
+ * Recursion | DFS
+ * https://leetcode.com/problems/compact-object/
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject0 = function(obj) {
 
 
     const checkIfObj = (obj) => {
