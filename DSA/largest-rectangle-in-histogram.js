675791f88963d62e7524ebf6cd3d44c2b3143efe
aadil42 <adilkelawala@gmail.com>
2024-07-12 15:28:34 +0530

    solved largest-rectangle-in-histogram

--- DSA/largest-rectangle-in-histogram.js
+++ DSA/largest-rectangle-in-histogram.js
@@ -5,7 +5,7 @@
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function(heights) {
+var largestRectangleArea1 = function(heights) {
     
     largestArea = 0;
     for(let i = 0; i < heights.length; i++) {
@@ -31,6 +31,95 @@ var largestRectangleArea = function(heights) {
     return largestArea;
 };
 
+
+/**
+ * Monotonic Stack 
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/largest-rectangle-in-histogram/
+ * Tip: imaging the current element as the smallest height and find out 
+ * /// untill how long we can extend it in the left and right direction getting
+ * // the largest possible width with that element as the smallest element.
+ * 
+ * @param {number[]} heights
+ * @return {number}
+ */
+
+var Stack = function() {
+    this.stack = [];
+}
+
+Stack.prototype.pop = function() {
+    return this.stack.pop();
+}
+
+Stack.prototype.push = function(el) {
+    this.stack.push(el);
+}
+
+Stack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+}
+
+Stack.prototype.isEmpty = function() {
+    return this.stack.length === 0;
+}
+
+Stack.prototype.clear = function() {
+    this.stack = [];
+}
+
+var largestRectangleArea = function(heights) {
+
+    const stack = new Stack();
+
+    const leftMinRange = [];
+    const rightMinRange = [];
+
+    for(let i = 0; i < heights.length; i++) {
+
+        while(!stack.isEmpty() && heights[i] <= heights[stack.top()]) {
+            stack.pop();
+        }
+
+        if(stack.isEmpty()) {
+            leftMinRange.push(-1);
+        } else {
+            leftMinRange.push(stack.top());
+        }
+
+        stack.push(i);
+    }
+
+    stack.clear();
+
+    for(let i = heights.length; i > -1; i--) {
+
+        while(!stack.isEmpty() && heights[i] <= heights[stack.top()]) {
+            stack.pop();
+        }
+
+        if(stack.isEmpty()) {
+            rightMinRange.push(heights.length);
+        } else {
+            rightMinRange.push(stack.top());
+        }
+
+        stack.push(i);
+    }
+
+    rightMinRange.reverse();
+
+    let maxArea = 0;
+
+    for(let i = 0; i < heights.length; i++) {
+        const width = (i - leftMinRange[i] - 1) + (rightMinRange[i] - i - 1) + 1;
+        const height = heights[i]
+        maxArea = Math.max(maxArea, width*height);
+    }
+    
+    return maxArea;
+};
+
 // efficient way
 
 /**
@@ -40,7 +129,7 @@ var largestRectangleArea = function(heights) {
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function(heights) {
+var largestRectangleArea0 = function(heights) {
     
     let stack = [];
     const leftDirectionLimit = [];
