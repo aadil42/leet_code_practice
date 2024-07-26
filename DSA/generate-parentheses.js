71e3557b7770885745fe7884adb7e80b83097c5b
aadil42 <adilkelawala@gmail.com>
2024-07-26 22:29:42 +0530

    solved generate-parentheses again

--- DSA/generate-parentheses.js
+++ DSA/generate-parentheses.js
@@ -1,10 +1,50 @@
 /**
+ * Recursion | Brute-force.
+ * Time O(2^n) | Space O(2^n)
+ * https://leetcode.com/problems/generate-parentheses/
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+
+    const ans = [];
+    const dfs = (open, close, currStr) => {
+  
+      if(open === 0 && close === 0) {
+          ans.push(currStr);
+          return;
+      }
+  
+      // All of the open parenthesis's are finish now we must close.
+      if(open === 0) {
+          dfs(open, close-1, currStr+")");
+          return;
+      }
+  
+      // We can eigher open a parenthises or close it. 
+      if(close > open) {
+          dfs(open-1, close, currStr+"(");
+          dfs(open, close-1, currStr+")");
+          return;
+      }
+  
+      // all parenthesis are perfectly formed until now. So we must start by opening a parenthises.
+      dfs(open-1, close, currStr+"(");
+    }
+    
+    dfs(n,n,"");
+  
+    return ans;  
+};
+
+
+/**
  * Recursion
  * Time O(2^n) | Space O(n)
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
+var generateParenthesis0 = function(n) {
     const result = [];
     const openL = n;
     const closeL = n;
