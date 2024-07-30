76b7d019d9d86d723fcb38f02d2f536057e81394
aadil42 <adilkelawala@gmail.com>
2024-07-30 14:14:14 +0530

    leetcode added a testcase that made your solution fail. So I fixed it.

--- DSA/validate-binary-tree-nodes.js
+++ DSA/validate-binary-tree-nodes.js
@@ -8,24 +8,23 @@
  * @param {number[]} rightChild
  * @return {boolean}
  */
-/**
- * @param {number} n
- * @param {number[]} leftChild
- * @param {number[]} rightChild
- * @return {boolean}
- */
 var validateBinaryTreeNodes = function(n, leftChild, rightChild) {
     
   const visited = new Set();
 
   const findRoot = () => {
-    let root = 0;
-    for(let i = 0; i < leftChild.length; i++) {
-      const left = leftChild[i];
-      const right = rightChild[i];
-      if(left ===  root || right === root) root = i;
+    
+    const childrenSet = new Set();
+    for(let i = 0; i < n; i++) {
+      childrenSet.add(i);
     }
-    return root;
+    
+    for(let i = 0; i < n; i++) {
+      childrenSet.delete(leftChild[i]);
+      childrenSet.delete(rightChild[i]);
+    }
+
+    return [...childrenSet][0];
   }
 
   const dfs = (i) => {
@@ -36,39 +35,9 @@ var validateBinaryTreeNodes = function(n, leftChild, rightChild) {
     const left = leftChild[i];
     const right = rightChild[i];
     visited.add(i);
-
     return dfs(left) && dfs(right);
   }
 
   const root = findRoot();
   return dfs(root) && visited.size === n;
-};
-
-var validateBinaryTreeNodes1 = function(n, leftChild, rightChild) {
-    
-    const visited = new Set();
-
-    const findRoot = () => {
-      let root = 0;
-      for(let i = 0; i < leftChild.length; i++) {
-        const left = leftChild[i];
-        const right = rightChild[i];
-        if(left ===  root || right === root) root = i;
-      }
-      return root;
-    }
-
-    const dfs = (i) => {
-
-      if(visited.has(i)) return false;
-      visited.add(i);
-      const left = leftChild[i];
-      const right = rightChild[i];
-      if(left !== -1 && !dfs(left)) return false;
-      if(right !== -1 && !dfs(right)) return false;
-      return true;
-    }
-
-    const root = findRoot();
-    return dfs(root) && visited.size === n;
 };
