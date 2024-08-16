350ef5c1027b3688307359e0fc827a4deaf3530a
aadil42 <adilkelawala@gmail.com>
2024-08-16 23:44:06 +0530

    add another solution for n-queens-ii

--- DSA/n-queens-ii.js
+++ DSA/n-queens-ii.js
@@ -1,9 +1,49 @@
+/**
+ * Time O(n^n) | Space O(n)
+ * BackTracking | Recursion
+ * https://leetcode.com/problems/n-queens-ii/
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function(n) {
+    
+    const colSet = new Set();
+    const posDiognal = new Set();
+    const negetiveDiognal = new Set();
+    let totalWays = 0;
+    const dfs = (row) => {
+
+        if(row === n) {
+            totalWays++;
+            return;
+        }
+
+        for(let col = 0; col < n; col++) {
+            if(colSet.has(col)) continue;
+            if(posDiognal.has(row+col)) continue;
+            if(negetiveDiognal.has(row-col)) continue;
+
+            colSet.add(col);
+            posDiognal.add(row+col);
+            negetiveDiognal.add(row-col);
+
+            dfs(row+1);
+
+            colSet.delete(col);
+            posDiognal.delete(row+col);
+            negetiveDiognal.delete(row-col);
+        }
+    }
+    dfs(0);
+    return totalWays;
+};
+
 // Same fucking problem as https://leetcode.com/problems/n-queens/
 /**
  * @param {number} n
  * @return {number}
  */
-var totalNQueens = function(n) {
+var totalNQueens0 = function(n) {
     let result = 0;    
     const board = [];
     for(let i = 0; i < n; i++) {
