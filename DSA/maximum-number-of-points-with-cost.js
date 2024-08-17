095b39ddd92007185203eb5917bb3fc2b46b9caf
aadil42 <adilkelawala@gmail.com>
2024-08-17 23:31:48 +0530

    add another brute force. better brute force

--- DSA/maximum-number-of-points-with-cost.js
+++ DSA/maximum-number-of-points-with-cost.js
@@ -1,11 +1,37 @@
 /**
+ * better Brute Force
+ * Time O(m*n*2) | Space O(1)
+ * https://leetcode.com/problems/maximum-number-of-points-with-cost
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxPoints = function(points) {
+    
+    const ROW = points.length;
+    const COL = points[0].length;
+
+    for(let row = 1; row < ROW; row++) {
+        for(let outerCol = 0; outerCol < COL; outerCol++) {
+            let currMax = 0;
+            for(let col = 0; col < COL; col++) {
+                currMax = Math.max(currMax, points[row][outerCol] + points[row-1][col] - Math.abs(col-outerCol));
+            }
+            points[row][outerCol] = currMax;
+        }
+    }
+
+    return Math.max(...points[ROW-1]);
+};
+
+
+/**
  * Brute Force | BackTracking
  * Time O(m^n) | Space O(m)
  * https://leetcode.com/problems/maximum-number-of-points-with-cost
  * @param {number[][]} points
  * @return {number}
  */
-var maxPoints = function(points) {
+var maxPoints0 = function(points) {
     
     let max = 0;
     const ROW = points.length;
