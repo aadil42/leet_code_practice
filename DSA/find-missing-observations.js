7021b08a6ecacbaf6ee9ec5c6fd310665dbfb0c7
aadil42 <adilkelawala@gmail.com>
2024-09-05 22:27:04 +0530

    solved find-missing-observations second time

--- DSA/find-missing-observations.js
+++ DSA/find-missing-observations.js
@@ -26,3 +26,39 @@ var missingRolls = function(rolls, mean, n) {
 
     return result;
 };
+
+/**
+ * Time O(n) | Space O(1)
+ * Math | Simulation | Array
+ * https://leetcode.com/problems/find-missing-observations/
+ * @param {number[]} rolls
+ * @param {number} mean
+ * @param {number} n
+ * @return {number[]}
+ */
+var missingRolls1 = function(rolls, mean, n) {
+    const m = rolls.length;
+    const mTotal = rolls.reduce((acc,curr) => acc+curr, 0);
+    const totalOfMissingObservation = (mean * (m+n)) - mTotal;
+    let remainder = totalOfMissingObservation % n
+    const divisions = Math.floor(totalOfMissingObservation/n);
+
+    if(divisions > 6) return []; 
+    if(divisions < 1) return [];
+
+    const missingObservationArr = [];
+    
+    for(let i = 0; i < n; i++) {
+        missingObservationArr.push(divisions);
+    }
+
+    let i = 0;
+    while(remainder) {
+        if(missingObservationArr[i] + 1 > 6) return [];
+        missingObservationArr[i] += 1;
+        i++;
+        remainder--;
+    }
+
+    return missingObservationArr;
+};
