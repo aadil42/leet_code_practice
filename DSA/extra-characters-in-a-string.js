c6c39e044ab10b34dfeefff2c08c92d08ffee30c
aadil42 <adilkelawala@gmail.com>
2024-09-23 15:16:13 +0530

    solved extra-characters-in-a-string again.

--- DSA/extra-characters-in-a-string.js
+++ DSA/extra-characters-in-a-string.js
@@ -1,4 +1,34 @@
 /**
+ * DP | Recurison
+ * Time O(n*m) | Space O(n) 
+ * https://leetcode.com/problems/extra-characters-in-a-string
+ * @param {string} s
+ * @param {string[]} dictionary
+ * @return {number}
+ */
+var minExtraChar = function(s, dictionary) {
+  return dfs(0, new Set(dictionary), s, new Map());
+};
+
+const dfs = (start, set, str, cache) => {
+
+  if(cache.has(start)) return cache.get(start);
+  if(start === str.length) return 0;
+
+  let min = Infinity;
+  for(let i = start; i < str.length; i++) {
+      const subStr = str.slice(start, i+1);
+      if(set.has(subStr)) {
+          min = Math.min(min, dfs(i+1, set, str, cache));
+      }
+  }
+
+  min = Math.min(min, dfs(start+1, set, str, cache) + 1);
+  cache.set(start, min);
+  return min;
+}
+
+/**
  * DP | Recursion
  * https://leetcode.com/problems/extra-characters-in-a-string/
  * 
@@ -8,7 +38,7 @@
  * @param {string[]} dictionary
  * @return {number}
  */
-var minExtraChar = function(s, dictionary) {
+var minExtraChar0 = function(s, dictionary) {
 
   const dictionarySet = new Set([...dictionary]);
 
@@ -30,3 +60,4 @@ var minExtraChar = function(s, dictionary) {
 
   return dfs(0);
 };
+
