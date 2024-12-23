e80c388675e9c68928236fe249347d5ede481dcd
aadil42 <adilkelawala@gmail.com>
2024-12-23 16:02:02 +0530

    add simpler solution

--- DSA/splitting-a-string-into-descending-consecutive-values.js
+++ DSA/splitting-a-string-into-descending-consecutive-values.js
@@ -1,11 +1,41 @@
 /**
+ * BackTracking | DFS
+ * Time O(n^2) | Space O(n) 
+ * https://leetcode.com/problems/splitting-a-string-into-descending-consecutive-values/
+ * @param {string} s
+ * @return {boolean}
+ */
+var splitString = function(s) {
+
+    const dfs = (start, pre) => {
+        if(start === s.length) return true;
+        for(let i = start; i < s.length;  i++) {
+            const subStr = s.slice(start, i+1);
+            if((+pre) - (+subStr) === 1) {
+                if(dfs(i+1, subStr)) return true;
+            }
+        }
+
+        return false;
+    }
+
+    for(let i = 0; i < s.length; i++) {
+        const subStr = s.slice(0, i+1);
+        if(i+1 === s.length) continue; // this means we took the entire string without spliting.
+        if(dfs(i+1, subStr)) return true;
+    }
+
+    return false;
+};
+
+/**
  * Backtracking/Recursion
  * 
  * Time O(n^n) | Space O(n)
  * @param {string} s
  * @return {boolean}
  */
-var splitString = function(s) {
+var splitString0 = function(s) {
     
     function dfs(i, subString) {
     if(i === s.length) return true;    
