4d78487463c2839cd8cdda5c6911b63637c479a5
aadil42 <adilkelawala@gmail.com>
2024-12-25 10:29:35 +0530

    solved find-largest-value-in-each-tree-row

--- DSA/find-largest-value-in-each-tree-row.js
+++ DSA/find-largest-value-in-each-tree-row.js
@@ -7,12 +7,55 @@
  * }
  */
 /**
+ * LevelOrderTraversal
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/find-largest-value-in-each-tree-row
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var largestValues = function(root) {
+
+    const q = new Queue();
+
+    const result = [];
+
+    if(!root) return result;
+
+    q.enqueue(root);
+    while (!q.isEmpty()) {
+        
+        let size = q.size();
+        let max = -Infinity;
+
+        while (size) {
+            const node = q.dequeue();
+            max = Math.max(max, node.val);
+            if (node.left) q.enqueue(node.left);
+            if (node.right) q.enqueue(node.right);
+            size--;
+        }
+
+        result.push(max);
+    }
+
+    return result;
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * BFS | Level order traversal
  * Time O(n^2) | Space O(n) The reason it's n^2 is because the shift method on array. It's O(n). You can easily use real queue data structure with the same code.
  * @param {TreeNode} root
  * @return {number[]}
  */
-var largestValues = function(root) {
+var largestValues0 = function(root) {
     
     if(!root) return [];
     const result = [];
