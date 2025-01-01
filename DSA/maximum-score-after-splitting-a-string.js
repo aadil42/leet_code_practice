66ef9923569c4c9cce7c518808d39555b2bdc1ee
aadil42 <adilkelawala@gmail.com>
2025-01-01 09:01:30 +0530

    solved maximum-score-after-splitting-a-string again

--- DSA/maximum-score-after-splitting-a-string.js
+++ DSA/maximum-score-after-splitting-a-string.js
@@ -1,11 +1,33 @@
 /**
+ * Array | Counting
+ * Time O(n) | Space O(n) (because  we're making array when counting ones freq)
+ * https://leetcode.com/problems/maximum-score-after-splitting-a-string
+ * @param {string} s
+ * @return {number}
+ */
+var maxScore = function(s) {
+    
+    let zeroCount = 0;
+    let oneCount = s.split("").filter((c) => c === "1").length;
+
+    let maxSum = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] === "0") zeroCount++;;
+        if (s[i] === "1") oneCount--;
+        maxSum = Math.max(maxSum, zeroCount+oneCount);
+    }
+
+    return maxSum;
+};
+
+/**
  * Array
  * Time O(n) | Space O(1)
  * https://leetcode.com/problems/maximum-score-after-splitting-a-string
  * @param {string} s
  * @return {number}
  */
-var maxScore = function(s) {
+var maxScore0 = function(s) {
     
     let ones = s.split('').reduce((acc, bit) => {
         if(bit === "1") return acc+1;
@@ -26,3 +48,4 @@ var maxScore = function(s) {
 
     return maxScore;
 };
+
