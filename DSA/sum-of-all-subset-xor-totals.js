7e5a4c0e556c3036419a6bb8db7e86082beddff2
aadil42 <adilkelawala@gmail.com>
2025-01-04 16:15:14 +0530

    solved sum-of-all-subset-xor-totals

--- DSA/sum-of-all-subset-xor-totals.js
+++ DSA/sum-of-all-subset-xor-totals.js
@@ -22,3 +22,34 @@ var subsetXORSum = function(nums) {
 
     return resultXor;
 };
+
+/**
+ * BruteForce | BackTracking | Recursion
+ * Time O(2^n) | Space O(2^n)
+ * https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subsetXORSum0 = function(nums) {
+    
+    const dfs = (idx, currSubSet, allSubSets) => {
+        
+        if (idx === nums.length) return allSubSets;
+
+        currSubSet.push(nums[idx]);
+        allSubSets.push([...currSubSet]);
+        dfs(idx+1, currSubSet, allSubSets);
+        currSubSet.pop();
+        dfs(idx+1, currSubSet, allSubSets);
+
+        return allSubSets;
+    }
+
+    const result = dfs(0,[],[]);
+    return result.map((subSet) => getXor(subSet)).reduce((acc, num) => acc+num, 0);
+};
+
+
+const getXor = (arr) => {
+    return arr.reduce((acc, num) => acc^num, 0);
+}
