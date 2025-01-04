01a9dd946de681b6da05baccb3569b0a18d76f00
aadil42 <adilkelawala@gmail.com>
2025-01-04 14:50:42 +0530

    solved combination-sum again

--- DSA/combination-sum.js
+++ DSA/combination-sum.js
@@ -28,3 +28,43 @@ var combinationSum = function(candidates, target) {
 
 const candidates = [2,3,6,7], target = 7;
 console.log(combinationSum(candidates, target));
+
+
+/**
+ * BruteForce | BackTarcking | Recursion
+ * Time O(n^target) | Space (n^target) (Not sure about the space complexity)
+ * https://leetcode.com/problems/combination-sum/
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum0 = function(candidates, target) {
+
+    const dfs = (sum, combination, allCombinations) => {
+        if (sum > target) return allCombinations;
+        if (sum === target) {
+            allCombinations.push(combination.slice(0));
+            return allCombinations;
+        }
+
+        for (let i = 0; i < candidates.length; i++) {
+            const candidate = candidates[i];
+            combination.push(candidate);
+            dfs(sum+candidate, combination, allCombinations);
+            combination.pop();
+        }
+
+        return allCombinations;
+    }
+
+    const combinations = dfs(0,[],[]);
+    return makeUnique(combinations);
+};
+
+const makeUnique = (combinations) => {
+    combinations = combinations.map((combination) => combination.sort((a,b) => a-b));
+    combinations = combinations.map((combination) => combination.join("#"));
+    combinations = new Set(combinations);
+    combinations = [...combinations].map((combination) => combination.split("#"));
+    return combinations.map((combination) => combination.map((num) => +num));
+}
