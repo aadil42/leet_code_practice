32f6b46024b4425b1d1cc7610224976caa427b91
aadil42 <adilkelawala@gmail.com>
2025-01-06 13:11:15 +0530

    solved minimum-interval-to-include-each-query again

--- DSA/minimum-interval-to-include-each-query.js
+++ DSA/minimum-interval-to-include-each-query.js
@@ -24,6 +24,56 @@ var minIntervalBF = function(intervals, queries) {
     return result;
 };
 
+/**
+ * PriorityQueue | Sorting
+ * Time O(n*log(n)) | Space O(n)
+ * https://leetcode.com/problems/minimum-interval-to-include-each-query/
+ * @param {number[][]} intervals
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+var minInterval = function(intervals, queries) {
+
+  queries = queries.map((q, idx) => [idx, q]).sort((a,b) => a[1]-b[1]);
+
+  intervals = intervals.sort((a,b) => a[0]-b[0]);
+
+  const minQ = new MinPriorityQueue({
+      compare: (a,b) => {
+          return a[0] - b[0];
+      }
+  });
+
+  let intervalIdx = 0;
+  const ans = [];
+
+  for (let i = 0; i < queries.length; i++) {
+      
+      const [idx, query] = queries[i];
+
+      while (intervalIdx < intervals.length && intervals[intervalIdx][0] <= query) {
+          
+          const [start, end] = intervals[intervalIdx];
+          const len = end - start + 1;
+          minQ.enqueue([len, end]);
+          intervalIdx++;
+      }
+
+      while (!minQ.isEmpty() && minQ.front()[1] < query) {
+          minQ.dequeue();
+      }
+
+      if (!minQ.isEmpty()) {
+          ans[idx] = minQ.front()[0];
+      } 
+      
+      if (minQ.isEmpty()) {
+          ans[idx] = -1;
+      }
+  }    
+
+  return ans;
+};
 
 /**
  * MinHeap
@@ -33,125 +83,125 @@ var minIntervalBF = function(intervals, queries) {
  * @param {number[]} queries
  * @return {number[]}
  */
-var minInterval = function(intervals, queries) {
+var minInterval0 = function(intervals, queries) {
     
-    // add indecis to queries
-    for(let i = 0; i < queries.length; i++) {
-        queries[i] = [queries[i], i];
-    }
+  // add indecis to queries
+  for(let i = 0; i < queries.length; i++) {
+      queries[i] = [queries[i], i];
+  }
 
-    // sort queries
-    queries.sort((a,b) => {
-       return a[0] - b[0];
-    });
+  // sort queries
+  queries.sort((a,b) => {
+     return a[0] - b[0];
+  });
 
-    // sort intervals based on left bound
-    intervals.sort((a,b) => {
-       return a[0] - b[0];
-    });
+  // sort intervals based on left bound
+  intervals.sort((a,b) => {
+     return a[0] - b[0];
+  });
 
-    const intervalMinHeap = new MinHeap();
+  const intervalMinHeap = new MinHeap();
 
-    let intervalIndex = 0;
+  let intervalIndex = 0;
 
-    const result = [];
-    for(let i = 0; i < queries.length; i++) {
-        const currentQueriy = queries[i][0];
-        // add intervals while the querie is in bound
-        while(intervals[intervalIndex] && intervals[intervalIndex][0] <= currentQueriy) {
-            const intervalLen = intervals[intervalIndex][1] - intervals[intervalIndex][0] + 1;
-            intervalMinHeap.insert([intervalLen, intervals[intervalIndex][1]]);
-            intervalIndex++;
-        }
+  const result = [];
+  for(let i = 0; i < queries.length; i++) {
+      const currentQueriy = queries[i][0];
+      // add intervals while the querie is in bound
+      while(intervals[intervalIndex] && intervals[intervalIndex][0] <= currentQueriy) {
+          const intervalLen = intervals[intervalIndex][1] - intervals[intervalIndex][0] + 1;
+          intervalMinHeap.insert([intervalLen, intervals[intervalIndex][1]]);
+          intervalIndex++;
+      }
 
-        // get rid of out of bound intervals
-        while(intervalMinHeap.peek() && intervalMinHeap.peek()[1] < currentQueriy) {
-            intervalMinHeap.extractMin();
-        }
+      // get rid of out of bound intervals
+      while(intervalMinHeap.peek() && intervalMinHeap.peek()[1] < currentQueriy) {
+          intervalMinHeap.extractMin();
+      }
 
-        // finally adding smallest interval len to result
-        if(intervalMinHeap.peek()) {
-            result[queries[i][1]] = intervalMinHeap.peek()[0];
-        } else {
-            result[queries[i][1]] = -1;
-        }
-    }
+      // finally adding smallest interval len to result
+      if(intervalMinHeap.peek()) {
+          result[queries[i][1]] = intervalMinHeap.peek()[0];
+      } else {
+          result[queries[i][1]] = -1;
+      }
+  }
 
-    return result;
+  return result;
 };
 
 
 class MinHeap {
-  constructor() {
-    this.heap = [];
+constructor() {
+  this.heap = [];
+}
+
+compare(a, b) {
+  return a[0] - b[0];
+}
+
+getParentIndex(index) {
+  return Math.floor((index - 1) / 2);
+}
+
+getLeftChildIndex(index) {
+  return (index * 2) + 1;
+}
+
+getRightChildIndex(index) {
+  return (index * 2) + 2;
+}
+
+insert(value) {
+  this.heap.push(value);
+  let currentIndex = this.heap.length - 1;
+  let parentIndex = this.getParentIndex(currentIndex);
+
+  while (currentIndex > 0 && this.compare(this.heap[currentIndex], this.heap[parentIndex]) < 0) {
+    [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
+    currentIndex = parentIndex;
+    parentIndex = this.getParentIndex(currentIndex);
+  }
+}
+
+extractMin() {
+  if (this.heap.length === 0) {
+    return null;
   }
 
-  compare(a, b) {
-    return a[0] - b[0];
+  if (this.heap.length === 1) {
+    return this.heap.pop();
   }
 
-  getParentIndex(index) {
-    return Math.floor((index - 1) / 2);
-  }
+  const min = this.heap[0];
+  this.heap[0] = this.heap.pop();
+  let currentIndex = 0;
+  let leftChildIndex = this.getLeftChildIndex(currentIndex);
+  let rightChildIndex = this.getRightChildIndex(currentIndex);
 
-  getLeftChildIndex(index) {
-    return (index * 2) + 1;
-  }
+  while (leftChildIndex < this.heap.length) {
+    let smallestChildIndex = rightChildIndex < this.heap.length && this.compare(this.heap[rightChildIndex], this.heap[leftChildIndex]) < 0
+      ? rightChildIndex
+      : leftChildIndex;
 
-  getRightChildIndex(index) {
-    return (index * 2) + 2;
-  }
-
-  insert(value) {
-    this.heap.push(value);
-    let currentIndex = this.heap.length - 1;
-    let parentIndex = this.getParentIndex(currentIndex);
-
-    while (currentIndex > 0 && this.compare(this.heap[currentIndex], this.heap[parentIndex]) < 0) {
-      [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
-      currentIndex = parentIndex;
-      parentIndex = this.getParentIndex(currentIndex);
-    }
-  }
-
-  extractMin() {
-    if (this.heap.length === 0) {
-      return null;
-    }
-
-    if (this.heap.length === 1) {
-      return this.heap.pop();
-    }
-
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    let currentIndex = 0;
-    let leftChildIndex = this.getLeftChildIndex(currentIndex);
-    let rightChildIndex = this.getRightChildIndex(currentIndex);
-
-    while (leftChildIndex < this.heap.length) {
-      let smallestChildIndex = rightChildIndex < this.heap.length && this.compare(this.heap[rightChildIndex], this.heap[leftChildIndex]) < 0
-        ? rightChildIndex
-        : leftChildIndex;
-
-      if (this.compare(this.heap[currentIndex], this.heap[smallestChildIndex]) <= 0) {
-        break;
-      }
-
-      [this.heap[currentIndex], this.heap[smallestChildIndex]] = [this.heap[smallestChildIndex], this.heap[currentIndex]];
-      currentIndex = smallestChildIndex;
-      leftChildIndex = this.getLeftChildIndex(currentIndex);
-      rightChildIndex = this.getRightChildIndex(currentIndex);
+    if (this.compare(this.heap[currentIndex], this.heap[smallestChildIndex]) <= 0) {
+      break;
     }
 
-    return min;
+    [this.heap[currentIndex], this.heap[smallestChildIndex]] = [this.heap[smallestChildIndex], this.heap[currentIndex]];
+    currentIndex = smallestChildIndex;
+    leftChildIndex = this.getLeftChildIndex(currentIndex);
+    rightChildIndex = this.getRightChildIndex(currentIndex);
   }
 
-  peek() {
-    return this.heap.length > 0 ? this.heap[0] : null;
-  }
+  return min;
+}
 
-  isEmpty() {
-    return this.heap.length === 0;
-  }
+peek() {
+  return this.heap.length > 0 ? this.heap[0] : null;
+}
+
+isEmpty() {
+  return this.heap.length === 0;
+}
 }
