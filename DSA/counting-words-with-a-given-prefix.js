e668bde49fcd12ee7fedd873e7169f394410383a
aadil42 <adilkelawala@gmail.com>
2025-01-09 11:53:12 +0530

    solved it with optimal approch (Trie)

--- DSA/counting-words-with-a-given-prefix.js
+++ DSA/counting-words-with-a-given-prefix.js
@@ -1,4 +1,65 @@
 /**
+ * Trie
+ * Time O(n*m) | Space O(n) | (m is max length of each word)
+ * https://leetcode.com/problems/counting-words-with-a-given-prefix
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+var prefixCount = function(words, pref) {
+    
+    const trie = createPreFixTrieOfWords(words);
+    return getPrefixCount(pref, trie);
+};
+
+const getPrefixCount = (pref, trie) => {
+    
+    const dfs = (idx, node) => {
+        if (idx === pref.length) return node.count;
+        
+        const latter = pref[idx];
+
+        if (!node[latter]) return 0;
+
+        return dfs(idx+1, node[latter]);
+    }
+
+    return dfs(0, trie);
+}
+
+const createPreFixTrieOfWords = (words) => {
+
+    const trie = {};
+
+    const addWord = (word) => {
+        // console.log(word)
+        const dfs = (idx, node) => {
+            if (idx === word.length) return;
+
+            const letter = word[idx];
+            // console.log(letter);
+            if(node[letter]) {
+                node[letter]["count"] += 1;
+            } else {
+                node[letter] = {};
+                node[letter]["count"] = 1;
+            }
+
+            dfs(idx+1, node[letter]);
+        }
+
+        dfs(0,trie);
+    }
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        addWord(word);
+    }
+
+    return trie
+}
+
+/**
  * BruteForce 
  * Time O(n^2 * m) | Space O(n) | (m is max length of each word)
  * https://leetcode.com/problems/counting-words-with-a-given-prefix
@@ -6,7 +67,7 @@
  * @param {string} pref
  * @return {number}
  */
-var prefixCount = function(words, pref) {
+var prefixCountBF = function(words, pref) {
     
     let count = 0;
     for (let i = 0; i < words.length; i++) {
