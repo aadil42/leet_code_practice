c1b6ec9518148bdf995bd2cf0a3f587245908201
aadil42 <adilkelawala@gmail.com>
2025-01-17 10:26:48 +0530

    add simpler oneline solution

--- DSA/neighboring-bitwise-xor.js
+++ DSA/neighboring-bitwise-xor.js
@@ -1,10 +1,21 @@
 /**
+ * Bit-manupilation
+ * Time O(n) |  Space O(1)
+ * https://leetcode.com/problems/neighboring-bitwise-xor/
+ * @param {number[]} derived
+ * @return {boolean}
+ */
+var doesValidArrayExist = function(derived) {
+    return derived.reduce((acc, bit) => acc^bit, 0) === 0;
+};
+
+/**
  * 
  * Very easy you need to grasp the intuition
  * @param {number[]} derived
  * @return {boolean}
  */
-var doesValidArrayExist = function(derived) {
+var doesValidArrayExist1 = function(derived) {
     let ones = 0;
     derived.forEach((bit) => {
         if(bit) ones++;
