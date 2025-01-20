4ccd189de0447629c7bb87ddda2d783bfc54b358
aadil42 <adilkelawala@gmail.com>
2025-01-20 10:01:13 +0530

    solved non-overlapping-intervals

--- DSA/non-overlapping-intervals.js
+++ DSA/non-overlapping-intervals.js
@@ -1,12 +1,41 @@
 /**
  * Greedy | Sorting
+ * Time O(n*log(n)) | Space O(1)
+ * https://leetcode.com/problems/non-overlapping-intervals/
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals2nd = function(intervals) {
+    
+    intervals.sort((a,b) => a[0]-b[0]);
+
+    let minRemoves = 0;
+    let preEnd = intervals[0][1];
+
+    for (let i = 1; i < intervals.length; i++) {
+        const [start, end] = intervals[i];
+
+        if (start < preEnd) {
+            preEnd = Math.min(preEnd, end);
+            minRemoves++;
+            continue;
+        }
+
+        preEnd = end;
+    }
+
+    return minRemoves
+};
+
+/**
+ * Greedy | Sorting
  * https://leetcode.com/problems/non-overlapping-intervals/
  * 
  * Time O(n*log(n)) | Space O(1)
  * @param {number[][]} intervals
  * @return {number}
  */
-var eraseOverlapIntervals = function(intervals) {
+var eraseOverlapIntervals1st = function(intervals) {
     
     intervals = intervals.sort((a, b) => a[0] - b[0]);
 
@@ -23,6 +52,3 @@ var eraseOverlapIntervals = function(intervals) {
     }
     return result;
 };
-
-intervals = [[1,2],[1,2],[1,2]];
-console.log(eraseOverlapIntervals(intervals));
