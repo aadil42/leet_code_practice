b7921374a11aeda086c430750595d8e2a8939fef
aadil42 <adilkelawala@gmail.com>
2025-02-11 13:16:18 +0530

    add recursive apporch. but it's still brute force

--- DSA/remove-all-occurrences-of-a-substring.js
+++ DSA/remove-all-occurrences-of-a-substring.js
@@ -1,5 +1,5 @@
 /** 
- * BruteForce | String
+ * BruteForce | String | Recursion
  * Time O(n^2) | Space O(1)
  * https://leetcode.com/problems/remove-all-occurrences-of-a-substring
  * @param {string} s
@@ -7,6 +7,22 @@
  * @return {string}
  */
 var removeOccurrences = function(s, part) {
+    const rec = (s, part) => {
+        if (s.includes(part)) return rec(s.replace(part, ""), part);
+        return s;
+    }
+    return rec(s, part);
+};
+
+/** 
+ * BruteForce | String
+ * Time O(n^2) | Space O(1)
+ * https://leetcode.com/problems/remove-all-occurrences-of-a-substring
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+var removeOccurrences0 = function(s, part) {
     
     while(s.includes(part)) {
         const idx = s.indexOf(part);
