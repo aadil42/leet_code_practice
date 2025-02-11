20c285f3397b1289aed55a612b193031d5303c29
aadil42 <adilkelawala@gmail.com>
2025-02-11 13:58:21 +0530

    add stack solution that is in O(n)

--- DSA/remove-all-occurrences-of-a-substring.js
+++ DSA/remove-all-occurrences-of-a-substring.js
@@ -1,4 +1,36 @@
 /** 
+ * Stack
+ * Time O(n) | Space O(n)
+ * https://leetcode.com/problems/remove-all-occurrences-of-a-substring
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+var removeOccurrences = function(s, part) {
+    
+    let stack = [];
+    
+    let i = 0;
+
+    while (i < s.length) {
+        
+        stack.push(s[i]);
+
+        if (stack.length >= part.length &&
+            stack[stack.length - 1] === part[part.length - 1]) {
+            const doesMatch = stack.slice(0).reverse().slice(0, part.length).reverse().join("") === part;
+                              //     copy     reverse    slice                  reverse  string 
+            if(doesMatch) {
+                stack.splice(stack.length - part.length);
+            }
+        }   
+        i++;
+    }
+
+    return stack.join("");
+};
+
+/** 
  * BruteForce | String | Recursion
  * Time O(n^2) | Space O(1)
  * https://leetcode.com/problems/remove-all-occurrences-of-a-substring
@@ -6,7 +38,7 @@
  * @param {string} part
  * @return {string}
  */
-var removeOccurrences = function(s, part) {
+var removeOccurrences1 = function(s, part) {
     const rec = (s, part) => {
         if (s.includes(part)) return rec(s.replace(part, ""), part);
         return s;
