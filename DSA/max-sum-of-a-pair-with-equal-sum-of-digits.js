79565eee2b9923ed703ec6d0cc5600fb20acb1ac
aadil42 <adilkelawala@gmail.com>
2025-02-12 20:19:20 +0530

    solved it with hash-map and sorting

--- DSA/max-sum-of-a-pair-with-equal-sum-of-digits.js
+++ DSA/max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -34,3 +34,38 @@ const getDigitsSum = (num) => {
           .map((n) => +n)
           .reduce((acc, curr) => acc+curr, 0);
 }
+
+/**
+ * HashMap | Sorting
+ * Time O(n*(k*log(k))) | Space O(n) (k = each array in hashmap)
+ * https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumSum0 = function(nums) {
+    
+    const digitSumToNum = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const digitSum = getDigitsSum(nums[i]);
+        if (digitSumToNum[digitSum]) {
+            digitSumToNum[digitSum].push(nums[i]);
+        } else {
+            digitSumToNum[digitSum] = [nums[i]];
+        }
+    }
+
+    let max = 0;
+
+    for (const key in digitSumToNum) {
+        if (digitSumToNum[key].length > 1) {
+            const arrOfPairs = digitSumToNum[key];
+            
+            const sorted = arrOfPairs.slice(0).sort((a,b) => b-a);
+
+            max = Math.max(max, sorted[0]+sorted[1]);
+        }
+    }
+
+    return max || -1;
+};
