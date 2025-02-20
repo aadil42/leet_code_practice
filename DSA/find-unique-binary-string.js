3f30c229ad0381afd2b9356ca68d4cf8604e3646
aadil42 <adilkelawala@gmail.com>
2025-02-20 11:30:39 +0530

    solved find-unique-binary-string

--- DSA/find-unique-binary-string.js
+++ DSA/find-unique-binary-string.js
@@ -106,3 +106,40 @@ const dfs = (idx, binaryArr, set) => {
     if(dfs(idx+1, binaryArr, set)) return binaryArr.join("");
     return false;
 }
+
+/**
+ * 4th time
+ * BackTracking | Recursion | BruteForce
+ * Time O(2^n) | Space O(n)
+ * https://leetcode.com/problems/find-unique-binary-string
+ * @param {string[]} nums
+ * @return {string}
+ */
+var findDifferentBinaryString = function(nums) {
+    
+    let isFound = false;
+    let missing = "";
+    const present = new Set(nums);
+    const n = nums[0].length;
+
+    const dfs = (binaryStr) => {
+
+        if (isFound) return;
+
+        if (binaryStr.length === n) {
+            if (!present.has(binaryStr)) {
+                isFound = true;
+                missing = binaryStr;
+                return;
+            }
+
+            return;
+        } 
+
+        dfs(binaryStr+"0");
+        dfs(binaryStr+"1");
+    }
+
+    dfs("");
+    return missing;
+};
