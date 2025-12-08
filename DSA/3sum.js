6d2d1b3472e6648ea356bd521a78eff7d0394685
aadil42 <adilkelawala@gmail.com>
2025-12-08 18:41:56 +0530

    solved 3sum again

--- DSA/3sum.js
+++ DSA/3sum.js
@@ -68,3 +68,65 @@ var threeSumR = function(nums) {
 
 console.log(threeSumR(nums));
 
+
+/**
+ * Two pointers | Sorting
+ * Time O(n^2) | Space O(n)
+ * https://leetcode.com/problems/3sum/submissions/1850153819/
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+//  for revision purposes.
+const sortO1 = (arr) => {
+
+    if (arr[0] > arr[1]) [arr[0], arr[1]] = [arr[1], arr[0]];
+    if (arr[0] > arr[2]) [arr[0], arr[2]] = [arr[2], arr[0]];
+    if (arr[1] > arr[2]) [arr[1], arr[2]] = [arr[2], arr[1]];
+
+    return arr;
+}
+const getTwoSum = (start, end, arr, target, ans, num) => {
+
+
+    while (start < end) {
+        const total = arr[start] + arr[end] 
+        if (total === target) {
+            ans.push(sortO1([num, arr[start], arr[end]]));
+            end--;
+            start++;
+        }
+        
+        if (total > target) end--;
+        if (total < target) start++;
+    }
+
+    return ans;
+}
+var threeSum = function(nums) {
+    
+    nums = nums.sort((a,b) => a-b);
+
+    // everything is 0 so no point executing further.
+    if (nums[0] === 0 && nums[nums.length - 1] === 0) return [[0,0,0]];
+
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        const target = nums[i] * -1;
+
+        const threeSumPairs = getTwoSum(i+1, nums.length - 1, nums, target, [], nums[i]);
+        threeSumPairs.forEach((pairs) => result.push(pairs));
+    }
+
+    const uniqueResults = new Set(
+        result.map((r) => {
+            return r.join(',');
+        })
+    );
+
+    return [...uniqueResults].map((r) => {
+        return r.split(',').map((r1) => {
+            return Number(r1);
+        });
+    });
+
+};
