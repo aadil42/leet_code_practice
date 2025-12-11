e1149c5dc54e3c894598ecdae7bf0548b7213276
aadil42 <adilkelawala@gmail.com>
2025-12-11 08:14:26 +0530

    solved longest-substring-without-repeating-characters again for revision

--- DSA/longest-substring-without-repeating-characters.js
+++ DSA/longest-substring-without-repeating-characters.js
@@ -30,3 +30,35 @@ var lengthOfLongestSubstring = function(s) {
 
     return max;
 };
+
+/**
+ * Sliding Window
+ * Time O(n) | Space O(1)
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1852493193/
+ * @param {string} s
+ * @return {number}
+ */
+//  for revision purpose
+var lengthOfLongestSubstringR = function(s) {
+    
+    let max = 0;
+    let uniqueSet = new Set();
+
+    let left = 0;
+    let right = 0;
+
+    while (right < s.length) {
+
+        while (uniqueSet.has(s[right])) {
+            uniqueSet.delete(s[left]);
+            left++;
+        }
+
+        uniqueSet.add(s[right]);
+
+        right = right + 1;
+        max = Math.max(max, right - left);
+    }
+
+    return max; 
+};
