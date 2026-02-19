2b68091ab7fe7e0efbe8edcd38925334e18b5936
aadil42 <adilkelawala@gmail.com>
2026-02-19 22:53:08 +0530

    solved search-in-rotated-sorted-array again

--- DSA/search-in-rotated-sorted-array.js
+++ DSA/search-in-rotated-sorted-array.js
@@ -46,3 +46,53 @@ var search = function(nums, target) {
      return -1;
  };
  
+ /**
+ * binary search (also did little bit of combinotorics) (it is difficult to grasp compared to previouse solution)
+ * Time O(log(n)) | Space O(1)
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1924628516/
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search0 = function(nums, target) {
+    
+    let left = 0;
+    let right = nums.length - 1;
+
+    const isIncreasing = (left, right, nums) => {
+        if (nums[left] <= nums[right]) return true;
+        return false;
+    }
+
+    while (left <= right) {
+
+        const mid = left + Math.floor((right - left)/2);
+
+        if (nums[mid] === target) return mid;
+
+        if (isIncreasing(left, mid, nums)) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+};
+
+
+// left half is increasing 
+   // target is between 
+   // target is not between
+
+// left half is not increasing
+   // target is between 
+   // target is not between
