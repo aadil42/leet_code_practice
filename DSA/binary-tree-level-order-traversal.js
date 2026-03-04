e5868671e7e0bd6a96df0c20116616b37906459e
aadil42 <adilkelawala@gmail.com>
2026-03-04 20:21:07 +0530

    solved binary-tree-level-order-traversal third time

--- DSA/binary-tree-level-order-traversal.js
+++ DSA/binary-tree-level-order-traversal.js
@@ -7,18 +7,18 @@ var heightOfTree = function(root) {
         goRecursive(root.left, height + 1);
         goRecursive(root.right, height + 1);
     }
-    
+
     return maxHeight;
 }
 var levelOrder = function(root) {
     if(!root) return [];
    const height = heightOfTree(root);
-    
-    const levelOrderArr = []; 
+
+    const levelOrderArr = [];
     for(let i = 0; i < height + 1; i++) {
         levelOrderArr.push(goRecursive(root, i, []));
     }
-    
+
     function goRecursive(root, level, levelArr) {
         if(!root) return levelArr;
         if(level === 0) {
@@ -30,11 +30,11 @@ var levelOrder = function(root) {
         }
             return levelArr;
     }
-    
+
     return levelOrderArr;
 };
 
-var levelOrder = function(root) {
+var levelOrder2 = function(root) {
     if(!root) return [];
     const treeHeight = heightOfTree(root);
 
@@ -59,4 +59,43 @@ var levelOrder = function(root) {
 
     return levelOrderArr;
 }
-Console
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * BFS traversal
+ * Time O(n^2) | Space O(n) (time is exponential because we're using array as a queue, with normal queue it would be n)
+ *  https://leetcode.com/problems/binary-tree-level-order-traversal/submissions/1937804715/
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder3 = function(root) {
+
+    if (!root) return [];
+
+    const queue = [root];
+    const bfsTraversal = [];
+
+    while (queue.length) {
+
+        let len = queue.length;
+        const level = [];
+        while (len) {
+            const node = queue.shift();
+            level.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+            len--;
+        }
+
+        bfsTraversal.push(level);
+    }
+
+    return bfsTraversal;
+};
