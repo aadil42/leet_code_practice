531098ba4b820c5f2e379718cfcfc0a8362bfb2b
aadil42 <adilkelawala@gmail.com>
2026-04-14 17:26:05 +0530

    add DP recursion solution. time complexity is same

--- DSA/maximum-product-subarray.js
+++ DSA/maximum-product-subarray.js
@@ -16,3 +16,36 @@ var maxProduct = function(nums) {
     return Max;
 };
 
+/**
+ * DP | Recursion | DFS
+ * Time O(n^2) | Space O(n^2)
+ * https://leetcode.com/problems/maximum-product-subarray/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDP = function(nums) {
+    
+    const cache = new Map();
+
+    const dfs = (idx, currProduct) => {
+
+        const hash = `${idx}-${currProduct}`;
+        if (idx === nums.length) return currProduct;
+        if (cache.has(hash)) return cache.get(hash);
+
+
+        const keepGoingWithCurrProductToNextIdx = dfs(idx+1, currProduct*nums[idx]);
+        const stopAndStartNewProductCalcFromHere = dfs(idx+1, nums[idx]);
+        
+        const max = Math.max(
+            keepGoingWithCurrProductToNextIdx,
+            stopAndStartNewProductCalcFromHere,
+            currProduct
+        );
+
+        cache.set(hash, max);
+        return max;
+    }
+
+    return dfs(1, nums[0]);
+};
